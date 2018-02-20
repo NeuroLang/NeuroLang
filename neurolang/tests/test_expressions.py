f8ce3058f1432fa01aa1f3ab0982429c7477100c
Demian Wassermann <demian.wassermann@inria.fr>
2018-02-20 23:09:31 +0100

    Worked on the expressions

--- neurolang/tests/test_expressions.py
+++ neurolang/tests/test_expressions.py
@@ -1,5 +1,6 @@
 from ..expressions import (
-    Symbol, SymbolApplication,
+    Symbol, Constant,
+    SymbolApplication,
     evaluate
 )
 
@@ -7,10 +8,15 @@ import operator as op
 import inspect
 
 
-def test_free_variable_application():
+def test_symbol_application():
     a = Symbol('a')
     b = Symbol('b')
-    assert SymbolApplication(op.add)(2, 3) == 5
+
+    oadd = SymbolApplication(op.add)
+    c = oadd(2, 3)
+    assert c.__wrapped__ == oadd
+    assert c.args == (2, 3)
+    assert evaluate(c) == 5
 
     fva = SymbolApplication(op.add)(a, 3)
     fvb = SymbolApplication(op.sub)(fva, 10)
@@ -18,11 +24,11 @@ def test_free_variable_application():
     fvd = SymbolApplication(a, kwargs={'c': b})
     fve = SymbolApplication(a, kwargs={'c': op.add(b, 2)})
 
-    assert a in fva._free_variables and (len(fva._free_variables) == 1)
+    assert a in fva._symbols and (len(fva._symbols) == 1)
     assert evaluate(fva, a=2) == 5
-    assert a in fvb._free_variables and (len(fvb._free_variables) == 1)
+    assert a in fvb._symbols and (len(fvb._symbols) == 1)
     assert evaluate(fvb, a=2) == -5
-    assert b in fvc._free_variables and (len(fvc._free_variables) == 2)
+    assert b in fvc._symbols and (len(fvc._symbols) == 2)
     assert isinstance(evaluate(fvc, b=2), SymbolApplication)
     assert evaluate(evaluate(fvc, b=3), a=2) == evaluate(fvc, a=2, b=3) == -15
     assert evaluate(
@@ -37,16 +43,16 @@ def test_free_variable_application():
         evaluate(fve, b=2),
         a=lambda *args, **kwargs: kwargs['c']
     ) == 4
-    assert SymbolApplication(op.add)(2, 3) == 5
 
 
-def test_free_variable_method_and_operator():
+def test_symbol_method_and_operator():
     a = Symbol('a')
     fva = a.__len__()
     fvb = a - 4
     fvc = 4 - a
     fve = a[2]
 
+    assert evaluate(a, a=1) == 1
     assert evaluate(fva, a={1}) == 1
     assert evaluate(fvb, a=1) == -3
     assert evaluate(fvc, a=1) == 3
@@ -55,7 +61,21 @@ def test_free_variable_method_and_operator():
     assert evaluate(fve, a=[0, 1, 2]) == 2
 
 
-def test_free_variable_wrapping():
+def test_constant_method_and_operator():
+    a = Constant(1, int)
+    fva = a + 1
+    b = Constant({1}, int)
+    fvb = b.__len__()
+    fbc = b.union({1, 2})
+
+    assert a == 1
+    assert evaluate(a) == 1
+    assert evaluate(fva) == 2
+    assert evaluate(fvb) == 1
+    assert evaluate(fbc) == {1, 2}
+
+
+def test_symbol_wrapping():
     def f(a: int)->float:
         '''
         test help
