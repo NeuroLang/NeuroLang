f7acecae12865bc56aeb0cc94486c419460ec5a8
Demian Wassermann <demian.wassermann@inria.fr>
2018-04-04 09:33:42 +0200

    Testing style

--- neurolang/tests/test_neurolang_interpreter.py
+++ neurolang/tests/test_neurolang_interpreter.py
@@ -1,9 +1,8 @@
-from pytest import raises, mark
+from pytest import raises
 
 from .. import neurolang as nl
 from .. import solver
-from typing import Set, Tuple, AbstractSet
-import operator as op
+from typing import Tuple, AbstractSet
 
 
 def test_assignment_values():
@@ -93,16 +92,12 @@ class FourIntsSetSolver(solver.SetBasedSolver):
         self.symbol_table[res.symbol] = res
         return res
 
-    #@nl.add_match(nl.FunctionApplication(op
-
     def predicate_equal_to(self, value: int)->FourInts:
-        return FourInts(value)
+        return nl.Constant(FourInts(value))
 
-    def predicate_singleton_set(self, value: int)->Set[FourInts]:
+    def predicate_singleton_set(self, value: int)->AbstractSet[FourInts]:
         return solver.FiniteDomainSet(
-            [FourInts(value)],
-            type_=FourInts,
-            typed_symbol_table=self.symbol_table
+            [nl.Constant(FourInts(value))],
         )
 
 
@@ -129,15 +124,19 @@ def test_queries():
     ast = nl.parser(script)
     nli.compile(ast)
 
-    assert nli.symbol_table['one'].value.value == 1
-    assert nli.symbol_table['one'].value.type == FourInts
-    assert nli.symbol_table['two'].value.value == 2
-    assert nli.symbol_table['three'].value.value == 3
-    assert nli.symbol_table['oneset'].value.value == {1}
-    assert nli.symbol_table['oneset_'].value.value == {1}
-    assert nli.symbol_table['onetwo'].value.value == {1, 2}
-    assert nli.symbol_table['twoset'].value.value == {2}
-    assert nli.symbol_table['twothree'].value.value == {2, 3}
+    assert nli.symbol_table['one'].value == 1
+    assert nli.symbol_table['one'].type == FourInts
+    assert nli.symbol_table['two'].value == 2
+    assert nli.symbol_table['three'].value == 3
+    assert 1 in nli.symbol_table['oneset'].value
+    assert nli.symbol_table['oneset_'].value \
+        == frozenset((nli.symbol_table['one'],))
+    assert nli.symbol_table['onetwo'].value \
+        == frozenset((nli.symbol_table['one'], nli.symbol_table['two']))
+    assert nli.symbol_table['twoset'].value \
+        == frozenset((nli.symbol_table['two'],))
+    assert nli.symbol_table['twothree'].value \
+        == frozenset((nli.symbol_table['two'], nli.symbol_table['three']))
 
 
 def test_error_messages():
