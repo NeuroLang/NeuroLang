6f7831ae287b05b3672451694f423f4ba52c0dd0
matischmit <matias.schmit@gmail.com>
2018-04-06 14:08:54 +0200

    example for querying north regions

--- examples/plot_set_type_example.py
+++ examples/plot_set_type_example.py
@@ -15,6 +15,7 @@ from neurolang.solver import SetBasedSolver
 import typing
 import operator
 from neurolang.regions import *
+from neurolang.RCD_relations import *
 
 
 ###################################################################
@@ -53,11 +54,35 @@ class RegionsSetType(SetBasedSolver):
     @nl.add_match(nl.Predicate(nl.Symbol('singleton'), (nl.Constant[typing.Tuple[int, int, int, int]],)))
     def singleton(self, expression):
         a = expression.args[0].value
-        r = Region(a[:2], a[2:])
+        r = Region((a[0].value, a[1].value), (a[2].value, a[3].value))
         res = nl.Constant[typing.AbstractSet[self.type]](
             frozenset((r,))
         )
         return res
+
+
+    @nl.add_match(
+        nl.Predicate(
+            nl.Symbol('north_of'),
+            (nl.Constant[typing.Tuple[int, int, int, int]],)
+        )
+    )
+    def north_of(self, expression):
+        a = expression.args[0].value
+        r2 = Region((a[0].value, a[1].value), (a[2].value, a[3].value))     #todo region as arg, dont create new one from tuple
+
+        result, visited = frozenset(), frozenset()
+        for symb in self.symbol_table.symbols_by_type(typing.AbstractSet[Region]).values():
+            elem = symb.value
+            if not elem <= visited:
+                for k in elem:
+                    dir = direction_matrix(k, r2)
+                    if dir[0, 1] == 1:
+                        result = result.union(elem)
+                visited = visited.union(elem)
+
+        return self.walk(nl.Constant[typing.AbstractSet[Region]](result))
+
 
     @nl.add_match(nl.FunctionApplication(nl.Constant(operator.invert), (nl.Constant[typing.AbstractSet],)))
     def rewrite_finite_domain_inversion(self, expression):
@@ -77,7 +102,6 @@ class RegionsSetType(SetBasedSolver):
         result = all_regions - set_value
         return self.walk(nl.Constant[set_type](result))
 
-
 ##################################################################
 class NeuroLangCompiler(
     RegionsSetType,
@@ -94,24 +118,17 @@ nlc = NeuroLangCompiler()
 #####################
 # Run three queries
 nlc.compile('''
-    origin are Regions singleton (0, 0, 0, 0)
     unit_region are Regions singleton (0, 0, 1, 1)
-    both are Regions in origin or in unit_region
+    box5 are Regions singleton (0, 0, 5, 5)
+    negative_unit are Regions singleton (-1, -1, 0, 0)
+    both are Regions in box5 or in unit_region
     test are Regions not in unit_region
+    northern_regions are Regions north_of (2, 1, 3, 2)
+    foo are Regions north_of (-5, -5, 4, -4)
+    bar are Regions north_of (0, -8, 4, -7)
 ''')
-
 
 #####################################
 # Print the resulting symbol table
 for k, v in nlc.symbol_table.items():
-    print(k, ':', v)
-
-
-#######################################
-# Print the intermediate representation
-# of a two statements
-ir = nlc.get_intermediate_representation('''
-    one are Regions in both
-''')
-print(ir[0])
-
+   print(k, ':', v)
