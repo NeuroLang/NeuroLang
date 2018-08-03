3d79ed571b560213973328f737e9f49c3c192f2a
Matías Schmit <matias.schmit@gmail.com>
2018-08-03 13:47:42 +0200

    'Anatomically correct' operations in solver (#44)
    * anatomical correct functions for anterior,posterior, inferior, superior as functions of the region solver
    
    *  avoid multiple function nesting in anatomical_direction functions of class RegionSolver

--- neurolang/frontend/tests/test_frontend.py
+++ neurolang/frontend/tests/test_frontend.py
@@ -2,6 +2,7 @@ from neurolang import frontend
 from ...regions import Region
 import pytest
 
+
 def test_add_regions_and_query():
     neurolang = frontend.RegionFrontend()
 
@@ -21,3 +22,31 @@ def test_neurosynth_query():
     neurolang = frontend.RegionFrontend()
     result = neurolang.neurosynth_term_to_region_set('emotion')
     assert len(result.value) != 0
+
+
+def test_anatomical_superior_of_query():
+    neurolang = frontend.RegionFrontend()
+
+    inferior = Region((2, 2, 2), (5, 5, 5))
+    central = Region((2, 0, 0), (5, 3, 8))
+    superior = Region((2, 2, 6), (5, 5, 8))
+
+    neurolang.add_region(inferior, result_symbol_name='inferior_region')
+    neurolang.add_region(central, result_symbol_name='central_region')
+    neurolang.add_region(superior, result_symbol_name='superior_region')
+
+    x = neurolang.new_region_symbol(symbol_name='x')
+    query = neurolang.query(
+        x, neurolang.symbols.superior_of(x, neurolang.symbols.inferior_region)
+    )
+    query_result = query.do(result_symbol_name='result_of_test_query')
+
+    assert len(query_result.value) == 2
+
+    query = neurolang.query(
+        x, neurolang.symbols.anatomical_superior_of(x, neurolang.symbols.inferior_region)
+    )
+    query_result = query.do(result_symbol_name='result_of_test_query')
+
+    assert len(query_result.value) == 1
+    assert next(iter(query_result.value)).name == 'superior_region'
