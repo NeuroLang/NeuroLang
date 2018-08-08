4878f5aaec91a7f4bc2c86f7cf5c5fdd297af2b0
Valentin Iovene <valentin@too.gy>
2018-08-08 15:37:14 +0200

    Cleaning (#47)
    * Cleaning test_solvers.py
    
    * Relative import fix
    
    * Using C and S shortcuts + removing useless imports
    
    * Style improvement of test_expression_iterator
    
    * Use C_, S_, F_ instead of C, S, F
    
    * Use C_ instead of solver.Constant
    
    * Use C_/S_/F_ in all tests and fix style
    
    * Use expressions.Constant in isinstance for clarity
    
    * Don't use shortcuts in isinstance asserts
    
    * No shortcut in add_match

--- neurolang/tests/test_algebraic_structures.py
+++ neurolang/tests/test_algebraic_structures.py
@@ -1,121 +1,79 @@
-from .. import neurolang as nl
 import operator
+
+from .. import neurolang as nl
+from .. import expressions
+
+C_ = expressions.Constant
+S_ = expressions.Symbol
+F_ = expressions.FunctionApplication
 
 
 def associative_command():
-    element1 = nl.Symbol('element1')
-    element2 = nl.Symbol('element2')
-    element3 = nl.Symbol('element3')
-    op = nl.Symbol('op')
+    element1 = S_('element1')
+    element2 = S_('element2')
+    element3 = S_('element3')
+    op = S_('op')
 
-    a = nl.FunctionApplication(
-            op,
-            (
-                nl.FunctionApplication(op, (element1, element2)),
-                element3
-            )
-        )
+    a = F_(op, (F_(op, (element1, element2)), element3))
 
-    b = nl.FunctionApplication(
-            op,
-            (
-                element1,
-                nl.FunctionApplication(op, (element2, element3))
-            )
-        )
+    b = F_(op, (element1, F_(op, (element2, element3))))
     return a, b
 
 
 def commutative_command():
-    element1 = nl.Symbol('element1')
-    element2 = nl.Symbol('element2')
-    op = nl.Symbol('op')
-    a = nl.FunctionApplication(op, (element1, element2))
-    b = nl.FunctionApplication(op, (element2, element1))
+    element1 = S_('element1')
+    element2 = S_('element2')
+    op = S_('op')
+    a = F_(op, (element1, element2))
+    b = F_(op, (element2, element1))
     return a, b
 
 
 def identity_command():
-    op = nl.Symbol('op')
-    element1 = nl.Symbol('element1')
-    null = nl.Symbol('null')
+    op = S_('op')
+    element1 = S_('element1')
+    null = S_('null')
     a = element1
-    b = nl.FunctionApplication(op, (element1, null))
+    b = F_(op, (element1, null))
     return a, b
 
 
 def inverse_command():
-    op = nl.Symbol('op')
-    element1 = nl.Symbol('element1')
-    null = nl.Symbol('null')
+    op = S_('op')
+    element1 = S_('element1')
+    null = S_('null')
 
     a = null
-    b = nl.FunctionApplication(op, (element1, element1))
+    b = F_(op, (element1, element1))
 
     return a, b
 
 
 def left_distributy_command():
-    op_dot = nl.Symbol('op_dot')
-    op_cross = nl.Symbol('op_cross')
-    element1 = nl.Symbol('element1')
-    element2 = nl.Symbol('element2')
-    element3 = nl.Symbol('element3')
-    a = nl.FunctionApplication(
-            op_dot,
-            (
-                element1,
-                nl.FunctionApplication(
-                    op_cross,
-                    (element2, element3)
-                )
-            )
-        )
-    b = nl.FunctionApplication(
-            op_cross,
-            (
-                nl.FunctionApplication(
-                    op_dot,
-                    (element1, element2)
-                ),
-                nl.FunctionApplication(
-                    op_dot,
-                    (element1, element3)
-                )
-            )
-        )
+    op_dot = S_('op_dot')
+    op_cross = S_('op_cross')
+    element1 = S_('element1')
+    element2 = S_('element2')
+    element3 = S_('element3')
+    a = F_(op_dot, (element1, F_(op_cross, (element2, element3))))
+    b = F_(
+        op_cross,
+        (F_(op_dot, (element1, element2)), F_(op_dot, (element1, element3)))
+    )
     return a, b
 
 
 def right_distributy_command():
-    op_dot = nl.Symbol('op_dot')
-    op_cross = nl.Symbol('op_cross')
-    element1 = nl.Symbol('element1')
-    element2 = nl.Symbol('element2')
-    element3 = nl.Symbol('element3')
-    a = nl.FunctionApplication(
-            op_dot,
-            (
-                nl.FunctionApplication(
-                    op_cross,
-                    (element2, element3)
-                ),
-                element1
-            )
-        )
-    b = nl.FunctionApplication(
-            op_cross,
-            (
-                nl.FunctionApplication(
-                    op_dot,
-                    (element2, element1)
-                ),
-                nl.FunctionApplication(
-                    op_dot,
-                    (element3, element1)
-                )
-            )
-        )
+    op_dot = S_('op_dot')
+    op_cross = S_('op_cross')
+    element1 = S_('element1')
+    element2 = S_('element2')
+    element3 = S_('element3')
+    a = F_(op_dot, (F_(op_cross, (element2, element3)), element1))
+    b = F_(
+        op_cross,
+        (F_(op_dot, (element2, element1)), F_(op_dot, (element3, element1)))
+    )
     return a, b
 
 
@@ -129,11 +87,11 @@ def check_command(command, nli):
 def check_is_abelian_group(op, inv_op, nli, null=None):
     nli.push_scope()
     if null is not None:
-        nli.symbol_table['null'] = nl.Constant(null)
+        nli.symbol_table['null'] = C_(null)
     check_is_monoid(operation=op, nli=nli, null=null)
-    nli.symbol_table['op'] = nl.Constant(op)
+    nli.symbol_table['op'] = C_(op)
     check_command(commutative_command, nli)
-    nli.symbol_table['op'] = nl.Constant(inv_op)
+    nli.symbol_table['op'] = C_(inv_op)
     check_command(inverse_command, nli)
 
     nli.pop_scope()
@@ -143,8 +101,8 @@ def check_is_monoid(operation, nli, null=None):
     nli.push_scope()
 
     if null is not None:
-        nli.symbol_table['null'] = nl.Constant(null)
-    nli.symbol_table['op'] = nl.Constant(operation)
+        nli.symbol_table['null'] = C_(null)
+    nli.symbol_table['op'] = C_(operation)
     check_command(associative_command, nli)
     check_command(identity_command, nli)
 
@@ -153,8 +111,8 @@ def check_is_monoid(operation, nli, null=None):
 
 def check_cross_op_is_distributive_with_respect_to_dot_op(cross, dot, nli):
     nli.push_scope()
-    nli.symbol_table['op_dot'] = nl.Constant(dot)
-    nli.symbol_table['op_cross'] = nl.Constant(cross)
+    nli.symbol_table['op_dot'] = C_(dot)
+    nli.symbol_table['op_cross'] = C_(cross)
 
     check_command(left_distributy_command, nli)
     check_command(right_distributy_command, nli)
@@ -163,37 +121,37 @@ def check_cross_op_is_distributive_with_respect_to_dot_op(cross, dot, nli):
 
 def check_algebraic_structure_is_a_ring(
     nli,
-    op_add=operator.add, op_inv_add=operator.sub,
-    op_mul=operator.mul, op_inv_mul=operator.truediv
+    op_add=operator.add,
+    op_inv_add=operator.sub,
+    op_mul=operator.mul,
+    op_inv_mul=operator.truediv
 ):
     check_is_abelian_group(op=op_add, inv_op=op_inv_add, nli=nli, null=0)
     check_is_monoid(operation=op_mul, nli=nli, null=1)
-    check_cross_op_is_distributive_with_respect_to_dot_op(cross=op_add,
-                                                          dot=op_mul,
-                                                          nli=nli)
+    check_cross_op_is_distributive_with_respect_to_dot_op(
+        cross=op_add, dot=op_mul, nli=nli
+    )
 
 
 def test_algebraic_structure_of_naturals():
     elements = (1, 2, 3)
     null_element = 0
     symbols = {
-        nl.Symbol[int]('element{}'.format(i + 1)): nl.Constant[int](e)
+        S_[int]('element{}'.format(i + 1)): C_[int](e)
         for i, e in enumerate(elements)
     }
-    symbols[nl.Symbol[int]('null')] = nl.Constant[int](null_element)
+    symbols[S_[int]('null')] = C_[int](null_element)
 
-    class TheSolver(
-        nl.NumericOperationsSolver[int],
-        nl.GenericSolver
-    ):
+    class TheSolver(nl.NumericOperationsSolver[int], nl.GenericSolver):
         pass
 
     nli = nl.NeuroLangIntermediateRepresentationCompiler(
-        solver=TheSolver(),
-        symbols=symbols
+        solver=TheSolver(), symbols=symbols
     )
     check_algebraic_structure_is_a_ring(
-        op_add=operator.add, op_inv_add=operator.sub,
-        op_mul=operator.mul, op_inv_mul=operator.truediv,
+        op_add=operator.add,
+        op_inv_add=operator.sub,
+        op_mul=operator.mul,
+        op_inv_mul=operator.truediv,
         nli=nli
     )
--- neurolang/tests/test_expression_iterator.py
+++ neurolang/tests/test_expression_iterator.py
@@ -1,79 +1,76 @@
 from .. import expression_walker
 from .. import expressions
 
+C_ = expressions.Constant
+S_ = expressions.Symbol
+F_ = expressions.FunctionApplication
+
 
 def test_leaves():
-    for expression in (
-            expressions.Constant(1),
-            expressions.Symbol('a')
-    ):
+    for expression in (C_(1), S_('a')):
         walk = list(expression_walker.expression_iterator(expression))
         assert walk == [(None, expression)]
 
-    for expression in (
-            expressions.Constant(1),
-            expressions.Symbol('a')
-    ):
+    for expression in (C_(1), S_('a')):
         for dfs in (True, False):
-            walk = list(expression_walker.expression_iterator(
-                expression, include_level=True, dfs=dfs
-            ))
+            walk = list(
+                expression_walker.expression_iterator(
+                    expression, include_level=True, dfs=dfs
+                )
+            )
             assert walk == [(None, expression, 0)]
 
 
 def test_tuple():
-    expression = expressions.Constant((expressions.Symbol('b'),  expressions.Symbol('c')))
+    expression = C_((S_('b'), S_('c')))
 
     for dfs in (True, False):
         walk = list(expression_walker.expression_iterator(expression))
         res = [
             (None, expression, 0),
-            (None, expression.value[0], 1), (None, expression.value[1], 1)
-        ]
+            (None, expression.value[0], 1),
+            (None, expression.value[1], 1)
+        ]  # yapf: disable
         assert walk == [r[:2] for r in res]
 
-        walk = list(expression_walker.expression_iterator(
-            expression, include_level=True, dfs=dfs
-        ))
+        walk = list(
+            expression_walker.expression_iterator(
+                expression, include_level=True, dfs=dfs
+            )
+        )
         assert walk == res
 
 
 def test_function_application():
-    expression = expressions.FunctionApplication(
-            expressions.Symbol('a'),
-            (expressions.Symbol('b'),  expressions.Symbol('c'))
-    )
+    expression = F_(S_('a'), (S_('b'), S_('c')))
 
     for dfs in (True, False):
         walk = list(expression_walker.expression_iterator(expression))
         res = [
             (None, expression, 0),
-            ('functor', expression.functor, 1), ('args', expression.args, 1),
-            (None, expression.args[0], 2), (None, expression.args[1], 2)
-        ]
+            ('functor', expression.functor, 1),
+            ('args', expression.args, 1),
+            (None, expression.args[0], 2),
+            (None, expression.args[1], 2)
+        ]  # yapf: disable
         assert walk == [r[:2] for r in res]
 
-        walk = list(expression_walker.expression_iterator(
-            expression, include_level=True, dfs=dfs
-        ))
+        walk = list(
+            expression_walker.expression_iterator(
+                expression, include_level=True, dfs=dfs
+            )
+        )
         assert walk == res
 
 
 def test_function_application_nested_dfs():
-    expression = expressions.FunctionApplication(
-            expressions.Symbol('a'),
-            (
-                expressions.FunctionApplication(
-                    expressions.Symbol('c'), tuple()
-                ),
-                expressions.Symbol('b')
-            )
-    )
+    expression = F_(S_('a'), (F_(S_('c'), tuple()), S_('b')))
 
     walk = list(expression_walker.expression_iterator(expression))
     res = [
         (None, expression, 0),
-        ('functor', expression.functor, 1), ('args', expression.args, 1),
+        ('functor', expression.functor, 1),
+        ('args', expression.args, 1),
         (None, expression.args[0], 2),
         ('functor', expression.args[0].functor, 3),
         ('args', expression.args[0].args, 3),
@@ -81,35 +78,31 @@ def test_function_application_nested_dfs():
     ]
 
     assert walk == [r[:2] for r in res]
-    walk = list(expression_walker.expression_iterator(
-        expression, include_level=True
-    ))
+    walk = list(
+        expression_walker.expression_iterator(expression, include_level=True)
+    )
     assert walk == res
 
 
 def test_function_application_nested_bfs():
-    expression = expressions.FunctionApplication(
-            expressions.Symbol('a'),
-            (
-                expressions.FunctionApplication(
-                    expressions.Symbol('c'), tuple()
-                ),
-                expressions.Symbol('b')
-            )
-    )
+    expression = F_(S_('a'), (F_(S_('c'), tuple()), S_('b')))
 
     walk = list(expression_walker.expression_iterator(expression, dfs=False))
     res = [
         (None, expression, 0),
-        ('functor', expression.functor, 1), ('args', expression.args, 1),
-        (None, expression.args[0], 2), (None, expression.args[1], 2),
+        ('functor', expression.functor, 1),
+        ('args', expression.args, 1),
+        (None, expression.args[0], 2),
+        (None, expression.args[1], 2),
         ('functor', expression.args[0].functor, 3),
         ('args', expression.args[0].args, 3),
     ]
 
     assert walk == [r[:2] for r in res]
 
-    walk = list(expression_walker.expression_iterator(
-        expression, dfs=False, include_level=True
-    ))
+    walk = list(
+        expression_walker.expression_iterator(
+            expression, dfs=False, include_level=True
+        )
+    )
     assert walk == res
