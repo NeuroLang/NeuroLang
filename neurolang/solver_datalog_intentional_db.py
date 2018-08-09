899a685b7f7e9c5adc9bf152fb8c33fa0bc2ec6d
Demian Wassermann <demian.wassermann@inria.fr>
2018-08-09 18:00:07 +0200

    Added docs

--- neurolang/solver_datalog_intentional_db.py
+++ neurolang/solver_datalog_intentional_db.py
@@ -15,10 +15,21 @@ __all__ = ['IntentionalDatabaseSolver']
 
 
 class IntentionalDatabaseSolver(PatternWalker):
+    '''Mixin to add sets as intentional databases on the DatalogSolver'''
     @add_match(
         FunctionApplication(Constant[AbstractSet], ...),
     )
     def functionapplication_abstract_set(self, expression):
+        '''
+        This pattern enables using intermediate representation objects
+        as relations in a datalog-compatible syntax. This means that
+        the set `R = {(1, 2), (3, 4)}` will behave such that
+        `R(1, 2)` (i.e. `FunctionApplication[bool](functor, args)`
+        with `functor = Constant[AbstractSet[Tuple[int, int]]](R)` and
+        `args = (Constant[int](1), Constant[int](2))`)
+        and `R(3, 4)` are `True` and any other tuple
+        given to `R` used as a function will be `False`.
+        '''
         if len(expression.args) == 1:
             element = expression.args[0]
         elif isinstance(expression.args, Constant):
@@ -36,6 +47,7 @@ class IntentionalDatabaseSolver(PatternWalker):
         return Constant[bool](ret)
 
     def function_isin(self, element: Any, set: AbstractSet) -> bool:
+        '''Function for checking that an element is in a set'''
         return element in set
 
     def function_join(
@@ -43,6 +55,7 @@ class IntentionalDatabaseSolver(PatternWalker):
         set1: AbstractSet, elem1: int,
         set2: AbstractSet, elem2: int
     ) -> AbstractSet[Tuple]:
+        '''function to join two sets based on one item element'''
 
         if len(set1) > 1 and not isinstance(next(iter(set1)), tuple):
             set1 = ((e,) for e in set1)
