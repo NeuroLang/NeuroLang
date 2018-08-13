b75b513f4172b7188f4066beb3048f85b870c09c
Demian Wassermann <demian.wassermann@inria.fr>
2018-08-13 16:48:35 +0200

    Improved code and testing

--- neurolang/tests/test_datalog_walkers.py
+++ neurolang/tests/test_datalog_walkers.py
@@ -1,5 +1,8 @@
 import pytest
 
+from operator import or_, and_, invert
+import typing
+
 from .. import datalog_walkers as dw
 from ..neurolang import (
     Constant, Symbol, FunctionApplication,
@@ -12,10 +15,18 @@ F_ = FunctionApplication
 E_ = ExistentialPredicate
 A_ = UniversalPredicate
 
+and_binary_functor = C_[typing.Callable[[bool, bool], bool]](and_)
+or_binary_functor = C_[typing.Callable[[bool, bool], bool]](or_)
+invert_functor = C_[typing.Callable[[bool], bool]](invert)
+
 
 def test_undefined():
     a = set()
 
+    assert (a + dw.undefined) is dw.undefined
+    assert (dw.undefined + a) is dw.undefined
+    assert (a - dw.undefined) is dw.undefined
+    assert (dw.undefined - a) is dw.undefined
     assert (a | dw.undefined) is dw.undefined
     assert (dw.undefined | a) is dw.undefined
     assert (a & dw.undefined) is dw.undefined
@@ -61,6 +72,16 @@ def test_conjunction():
         c: dw.Intersection({f})
     }
 
+    e = f & E_[bool](S_('x'), g)
+    restrictors = srv.walk(e)
+
+    assert restrictors is dw.undefined
+
+    e = E_[bool](S_('x'), g) & f
+    restrictors = srv.walk(e)
+
+    assert restrictors is dw.undefined
+
 
 def test_disjunction():
     srv = dw.SafeRangeVariablesWalker()
@@ -68,15 +89,41 @@ def test_disjunction():
     a = C_(sum)
     b = S_('b')
     c = S_('c')
-    d = C_(lambda x: x % 2 == 0)
+    d = C_(any)
+    e = C_(all)
 
     f = F_[bool](a, (b, c, C_(1)))
     g = F_[bool](d, (b,))
-    e = f | g
+
+    exp = f | g
+    restrictors = srv.walk(exp)
+    res = {b: dw.Union((f, g))}
+
+    assert restrictors == res
+
+    h = F_[bool](e, (b,))
+    exp = (f & g) | h
+    restrictors = srv.walk(exp)
+    res = {b: dw.Union((dw.Intersection((f, g)), h))}
+
+    assert restrictors == res
+
+    h = F_[bool](e, (b,))
+    exp = f | (g & h)
+    restrictors = srv.walk(exp)
+    res = {b: dw.Union((dw.Intersection((h, g)), f))}
+
+    assert restrictors == res
+
+    e = f | E_[bool](S_('x'), g)
     restrictors = srv.walk(e)
-    assert restrictors == {b: {
-        dw.Union((dw.Intersection({f}), dw.Intersection({g})))
-    }}
+
+    assert restrictors is dw.undefined
+
+    e = E_[bool](S_('x'), g) | f
+    restrictors = srv.walk(e)
+
+    assert restrictors is dw.undefined
 
 
 def test_inversion():
@@ -152,3 +199,74 @@ def test_replace_variables():
     assert exp_new.args[1].head == expected_result.args[1].head
     assert exp_new.args[1].body.functor == expected_result.args[1].body.functor
     assert exp_new.args[1].body.args == expected_result.args[1].body.args
+
+    vsw = dw.VariableSubstitutionWalker()
+
+    exp_new_2 = vsw.walk(exp_new)
+
+    assert exp_new is exp_new_2
+
+
+def test_push_neg_double_neg():
+    csnrf = dw.ConvertToSNRFWalker()
+
+    a = S_('a')
+    e = (~(~a).cast(bool)).cast(bool)
+
+    res = csnrf.walk(e)
+
+    assert res == a
+
+
+def test_push_neg_and():
+    csnrf = dw.ConvertToSNRFWalker()
+
+    a = S_[bool]('a')
+    b = S_[bool]('b')
+    e = (~(a & b).cast(bool)).cast(bool)
+
+    res = csnrf.walk(e)
+    assert res.functor == C_(or_)
+    assert len(res.args) == 2
+    assert res.args[0].functor == C_(invert)
+    assert res.args[0].args[0] == a
+    assert res.args[1].functor == C_(invert)
+    assert res.args[1].args[0] == b
+
+
+def test_push_neg_or():
+    csnrf = dw.ConvertToSNRFWalker()
+
+    a = S_[bool]('a')
+    b = S_[bool]('b')
+    e = (~(a | b).cast(bool)).cast(bool)
+
+    res = csnrf.walk(e)
+    assert res.functor == C_(and_)
+    assert len(res.args) == 2
+    assert res.args[0].functor == C_(invert)
+    assert res.args[0].args[0] == a
+    assert res.args[1].functor == C_(invert)
+    assert res.args[1].args[0] == b
+
+
+def test_univ_to_ex():
+    csnrf = dw.ConvertToSNRFWalker()
+
+    a = S_[typing.Callable[[bool], bool]]('a')
+    b = S_[bool]('b')
+    e = A_[bool](b, a(b))
+
+    exp_res = invert_functor(E_[bool](b, invert_functor(a(b))))
+
+    res = csnrf.walk(e)
+
+    assert res.functor == exp_res.functor
+    assert len(res.args) == len(exp_res.args) == 1
+    assert res.args[0].head == exp_res.args[0].head
+    assert res.args[0].body.functor == exp_res.args[0].body.functor
+    assert len(res.args[0].body.args) == len(exp_res.args[0].body.args) == 1
+    res_in_f = res.args[0].body.args[0]
+    exp_res_in_f = exp_res.args[0].body.args[0]
+    assert res_in_f.functor == exp_res_in_f.functor
+    assert res_in_f.args[0] == exp_res_in_f.args[0]
