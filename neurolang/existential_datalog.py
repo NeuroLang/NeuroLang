7f9a9536c123df1b9956c967715e3d29af08de0e
Valentin Iovene <valentin.iovene@inria.fr>
2018-08-22 13:05:23 +0200

    Use ExistentialPredicate instead of ExistentialRule

--- neurolang/existential_datalog.py
+++ neurolang/existential_datalog.py
@@ -1,25 +1,12 @@
-from .expressions import Expression, Symbol, NeuroLangException
+from .expressions import Statement, ExistentialPredicate, NeuroLangException
+from .solver_datalog_naive import NaiveDatalog
+from .expression_pattern_matching import add_match
 
-
-class ExistentialRule(Expression):
-    def __init__(self, eq_variables, statement):
-        # if only one variable given, make a tuple
-        if isinstance(eq_variables, Symbol): eq_variables = (eq_variables, )
-        if not all(isinstance(v, Symbol) for v in eq_variables):
-            raise NeuroLangException('E-Quantified variables must be symbols')
-        if any(v in statement.rhs._symbols for v in eq_variables):
+class ExistentialDatalog(NaiveDatalog):
+    @add_match(Statement(ExistentialPredicate, ...))
+    def existential_predicate_in_head(self, expression):
+        eq_variable = expression.lhs.head
+        if eq_variable in expression.rhs._symbols:
             raise NeuroLangException(
-                'E-Quantified variables cannot occur in rhs'
+                '\u2203-quantified variable cannot occur in rhs'
             )
-        if not all(v in statement.lhs._symbols for v in eq_variables):
-            raise NeuroLangException(
-                'All E-Quantified variables must occur in lhs'
-            )
-        self.eq_variables = eq_variables
-        self.statement = statement
-
-    def __repr__(self):
-        eq_var_str = '\u2203 {}'.format(
-            ', '.join([v.name for v in self.eq_variables])
-        )
-        return f'ExistentialRule({eq_var_repr}, {statement_repr})'
--- neurolang/tests/test_existential_datalog.py
+++ neurolang/tests/test_existential_datalog.py
@@ -1,18 +1,25 @@
 import pytest
 
 from .. import expressions
-from ..existential_datalog import ExistentialRule
+from ..existential_datalog import ExistentialDatalog
 
 S_ = expressions.Symbol
 F_ = expressions.FunctionApplication
 St_ = expressions.Statement
+EP_ = expressions.ExistentialPredicate
 
 
 def test_existential_rule_constructor():
+
+    solver = ExistentialDatalog()
+
     x, y = S_('x'), S_('y')
     P, Q = S_('P'), S_('Q')
+
     with pytest.raises(expressions.NeuroLangException):
-        rule = ExistentialRule(y, St_(F_(P, (x, y)), F_(Q, (y, ))))
+        exp = St_(EP_(y, P(x, y)), F_(Q, (x, y)))
+        solver.walk(exp)
+
     with pytest.raises(expressions.NeuroLangException):
-        rule = ExistentialRule(y, St_(F_(P, (x, )), F_(Q, (x, ))))
-    rule = ExistentialRule(y, St_(F_(P, (x, y)), F_(Q, (x, ))))
+        exp = St_(EP_(y, P(x)), F_(Q, (x, )))
+        solver.walk(exp)
