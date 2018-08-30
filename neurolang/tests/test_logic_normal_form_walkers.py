e476a32502159304e9df82f5978fc222120ec346
Demian Wassermann <demian.wassermann@inria.fr>
2018-08-30 16:58:44 -0300

    More consitent naming

--- neurolang/tests/test_logic_normal_form_walkers.py
+++ neurolang/tests/test_logic_normal_form_walkers.py
@@ -3,7 +3,7 @@ import pytest
 from operator import or_, and_, invert, eq
 import typing
 
-from .. import datalog_walkers as dw
+from .. import logic_normal_form_walkers as nfw
 from ..neurolang import (
     Constant, Symbol, FunctionApplication,
     ExistentialPredicate, UniversalPredicate
@@ -23,19 +23,19 @@ invert_functor = C_[typing.Callable[[bool], bool]](invert)
 def test_undefined():
     a = set()
 
-    assert (a + dw.undefined) is dw.undefined
-    assert (dw.undefined + a) is dw.undefined
-    assert (a - dw.undefined) is dw.undefined
-    assert (dw.undefined - a) is dw.undefined
-    assert (a | dw.undefined) is dw.undefined
-    assert (dw.undefined | a) is dw.undefined
-    assert (a & dw.undefined) is dw.undefined
-    assert (dw.undefined & a) is dw.undefined
-    assert (~dw.undefined) is dw.undefined
+    assert (a + nfw.undefined) is nfw.undefined
+    assert (nfw.undefined + a) is nfw.undefined
+    assert (a - nfw.undefined) is nfw.undefined
+    assert (nfw.undefined - a) is nfw.undefined
+    assert (a | nfw.undefined) is nfw.undefined
+    assert (nfw.undefined | a) is nfw.undefined
+    assert (a & nfw.undefined) is nfw.undefined
+    assert (nfw.undefined & a) is nfw.undefined
+    assert (~nfw.undefined) is nfw.undefined
 
 
 def test_atom():
-    srv = dw.SafeRangeVariablesWalker()
+    srv = nfw.SafeRangeVariablesWalker()
 
     a = C_(sum)
     x = S_('x')
@@ -50,13 +50,13 @@ def test_atom():
 
     restrictors = srv.walk(f)
     assert restrictors == {
-        x: dw.Intersection({f}),
-        y: dw.Intersection({f})
+        x: nfw.Intersection({f}),
+        y: nfw.Intersection({f})
     }
 
 
 def test_conjunction():
-    srv = dw.SafeRangeVariablesWalker()
+    srv = nfw.SafeRangeVariablesWalker()
 
     a = C_(sum)
     x = S_('x')
@@ -69,35 +69,35 @@ def test_conjunction():
     e = f & g
     restrictors = srv.walk(e)
     assert restrictors == {
-        x: dw.Intersection({f, g}),
-        y: dw.Intersection({f})
+        x: nfw.Intersection({f, g}),
+        y: nfw.Intersection({f})
     }
 
     e = f & g & F_[bool](C_(eq), (x, c))
     restrictors = srv.walk(e)
     assert restrictors == {
-        x: dw.Intersection({f, g, c}),
-        y: dw.Intersection({f})
+        x: nfw.Intersection({f, g, c}),
+        y: nfw.Intersection({f})
     }
 
     z = S_('z')
     e = f & g & F_[bool](C_(eq), (x, z))
-    with pytest.raises(dw.NeuroLangException):
+    with pytest.raises(nfw.NeuroLangException):
         restrictors = srv.walk(e)
 
     e = f & EP_[bool](S_('z'), g)
     restrictors = srv.walk(e)
 
-    assert restrictors is dw.undefined
+    assert restrictors is nfw.undefined
 
     e = EP_[bool](S_('z'), g) & f
     restrictors = srv.walk(e)
 
-    assert restrictors is dw.undefined
+    assert restrictors is nfw.undefined
 
 
 def test_disjunction():
-    srv = dw.SafeRangeVariablesWalker()
+    srv = nfw.SafeRangeVariablesWalker()
 
     a = C_(sum)
     x = S_('x')
@@ -110,37 +110,37 @@ def test_disjunction():
 
     exp = f | g
     restrictors = srv.walk(exp)
-    res = {x: dw.Union((f, g))}
+    res = {x: nfw.Union((f, g))}
 
     assert restrictors == res
 
     h = F_[bool](e, (x,))
     exp = (f & g) | h
     restrictors = srv.walk(exp)
-    res = {x: dw.Union((dw.Intersection((f, g)), h))}
+    res = {x: nfw.Union((nfw.Intersection((f, g)), h))}
 
     assert restrictors == res
 
     h = F_[bool](e, (x,))
     exp = f | (g & h)
     restrictors = srv.walk(exp)
-    res = {x: dw.Union((dw.Intersection((h, g)), f))}
+    res = {x: nfw.Union((nfw.Intersection((h, g)), f))}
 
     assert restrictors == res
 
     e = f | EP_[bool](S_('z'), g)
     restrictors = srv.walk(e)
 
-    assert restrictors is dw.undefined
+    assert restrictors is nfw.undefined
 
     e = EP_[bool](S_('z'), g) | f
     restrictors = srv.walk(e)
 
-    assert restrictors is dw.undefined
+    assert restrictors is nfw.undefined
 
 
 def test_inversion():
-    srv = dw.SafeRangeVariablesWalker()
+    srv = nfw.SafeRangeVariablesWalker()
 
     a = C_(sum)
     x = S_('x')
@@ -155,7 +155,7 @@ def test_inversion():
 
 
 def test_existential():
-    srv = dw.SafeRangeVariablesWalker()
+    srv = nfw.SafeRangeVariablesWalker()
 
     a = C_(sum)
     x = S_('x')
@@ -165,16 +165,16 @@ def test_existential():
     e = EP_[bool](x, f)
 
     restrictors = srv.walk(e)
-    assert restrictors == {y: dw.Intersection({f})}
+    assert restrictors == {y: nfw.Intersection({f})}
 
     e = EP_[bool](S_('z'), f)
 
     restrictors = srv.walk(e)
-    assert restrictors is dw.undefined
+    assert restrictors is nfw.undefined
 
 
 def test_not_srnf():
-    srv = dw.SafeRangeVariablesWalker()
+    srv = nfw.SafeRangeVariablesWalker()
 
     a = C_(sum)
     x = S_('x')
@@ -183,17 +183,17 @@ def test_not_srnf():
     f = F_[bool](a, (x, y, C_(1)))
     e = UP_[bool](x, f)
 
-    with pytest.raises(dw.NeuroLangException):
+    with pytest.raises(nfw.NeuroLangException):
         srv.walk(e)
 
     e2 = ~(f & EP_[bool](x, f))
 
-    with pytest.raises(dw.NeuroLangException):
+    with pytest.raises(nfw.NeuroLangException):
         srv.walk(e2)
 
 
 def test_replace_variables():
-    vsw = dw.VariableSubstitutionWalker()
+    vsw = nfw.VariableSubstitutionWalker()
 
     a = C_(sum)
     x = S_('x')
@@ -209,7 +209,7 @@ def test_replace_variables():
 
     assert exp_new == expected_result
 
-    vsw = dw.VariableSubstitutionWalker()
+    vsw = nfw.VariableSubstitutionWalker()
 
     exp_new_2 = vsw.walk(exp_new)
 
@@ -217,7 +217,7 @@ def test_replace_variables():
 
 
 def test_push_neg_double_neg():
-    csnrf = dw.ConvertToSNRFWalker()
+    csnrf = nfw.ConvertToSNRFWalker()
 
     a = S_('a')
     e = (~(~a).cast(bool)).cast(bool)
@@ -228,7 +228,7 @@ def test_push_neg_double_neg():
 
 
 def test_push_neg_and():
-    csnrf = dw.ConvertToSNRFWalker()
+    csnrf = nfw.ConvertToSNRFWalker()
 
     a = S_[bool]('a')
     b = S_[bool]('b')
@@ -239,7 +239,7 @@ def test_push_neg_and():
 
 
 def test_push_neg_or():
-    csnrf = dw.ConvertToSNRFWalker()
+    csnrf = nfw.ConvertToSNRFWalker()
 
     a = S_[bool]('a')
     b = S_[bool]('b')
@@ -250,7 +250,7 @@ def test_push_neg_or():
 
 
 def test_univ_to_ex():
-    csnrf = dw.ConvertToSNRFWalker()
+    csnrf = nfw.ConvertToSNRFWalker()
 
     a = S_[typing.Callable[[bool], bool]]('a')
     b = S_[bool]('b')
@@ -264,7 +264,7 @@ def test_univ_to_ex():
 
 
 def test_flatten_and():
-    fw = dw.FlattenMultipleLogicalOperators()
+    fw = nfw.FlattenMultipleLogicalOperators()
 
     args = [S_[bool](f'a{i}') for i in range(5)]
 
@@ -286,7 +286,7 @@ def test_flatten_and():
 
 
 def test_flatten_or():
-    fw = dw.FlattenMultipleLogicalOperators()
+    fw = nfw.FlattenMultipleLogicalOperators()
 
     args = [S_[bool](f'a{i}') for i in range(5)]
 
@@ -316,12 +316,12 @@ def test_SNRF_and_range():
 
     exp = R(x)
 
-    res, r = dw.expression_to_SRNF_and_range(exp)
-    assert r == {x: dw.Intersection((exp,))}
+    res, r = nfw.expression_to_SRNF_and_range(exp)
+    assert r == {x: nfw.Intersection((exp,))}
     assert res is exp
 
     exp = UP_[bool](x, invert_functor(Q(x, y)))
-    res, r = dw.expression_to_SRNF_and_range(exp)
+    res, r = nfw.expression_to_SRNF_and_range(exp)
     exp_res = invert_functor(EP_[bool](x, Q(x, y)))
     assert r == {}
     assert res == exp_res
