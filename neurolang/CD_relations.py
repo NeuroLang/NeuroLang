df8243ff16ca25ad31021ea37bf9cf671642de65
Demian Wassermann <demian.wassermann@inria.fr>
2018-09-05 20:59:11 -0300

    Tune-ups

--- neurolang/CD_relations.py
+++ neurolang/CD_relations.py
@@ -1,6 +1,7 @@
 from .interval_algebra import v_before, v_overlaps, v_during, v_meets, v_starts, v_finishes, v_equals
 from .regions import ExplicitVBR
 import numpy as np
+from scipy.linalg import kron
 
 
 __all__ = ['cardinal_relation']
@@ -55,7 +56,6 @@ def overlap_resolution(region, reference_region, stop_at=None):
                 current_reference_region_level = current_reference_region_next_level
             else:
                 max_depth_reached_ref = True
-
         mat = direction_matrix(
             [reg.box for reg in current_region_level],
             [reg.box for reg in current_reference_region_level]
@@ -105,6 +105,6 @@ def direction_matrix(region_bbs, another_region_bbs):
             rp_vector = relation_vectors(bb.limits, another_region_bb.limits)
             tensor = rp_vector[0].reshape(1, 3)
             for i in range(1, len(rp_vector)):
-                tensor = np.kron(rp_vector[i].reshape((3,) + (1,) * i), tensor)
+                tensor = kron(rp_vector[i].reshape((3,) + (1,) * i), tensor).squeeze()
             res += tensor.astype(bool, copy=False)  # np.logical_or(res, tensor)
     return res.astype(int, copy=False)
