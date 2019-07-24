5b47d360f2f3869ed3daccb436a0b19d1244ab12
Demian Wassermann <demianw@users.noreply.github.com>
2019-07-24 14:45:09 +0200

    Change array equality syntax (#131)

--- neurolang/regions.py
+++ neurolang/regions.py
@@ -218,8 +218,8 @@ class ExplicitVBR(VolumetricBrainRegion):
         return out
 
     def __eq__(self, other):
-        return (np.all(self.affine == other.affine) and
-                np.all(self.voxels == other.voxels))
+        return (np.array_equiv(self.affine, other.affine) and
+                np.array_equiv(self.voxels, other.voxels))
 
     def __repr__(self):
         return f'Region(VBR= affine:{self.affine}, voxels:{self.voxels})'
@@ -397,7 +397,7 @@ class SphericalVolume(ImplicitVBR):
         return hash(self.bounding_box.limits.tobytes())
 
     def __eq__(self, other):
-        return (np.all(self._center == other._center) and
+        return (np.array_equiv(self._center, other._center) and
                 self._radius == other._radius)
 
     def __repr__(self):
@@ -457,8 +457,8 @@ class PlanarVolume(ImplicitVBR):
         return hash(self.bounding_box.limits.tobytes())
 
     def __eq__(self, other):
-        return (np.all(self._origin == other._origin) and
-                np.all(self._vector == other._vector))
+        return (np.array_equiv(self._origin, other._origin) and
+                np.array_equiv(self._vector, other._vector))
 
     def __repr__(self):
         return (f'PlanarVolume(Origin={tuple(self._origin)},'
