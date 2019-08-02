b0ff95c3c814526a6577a39b4edd2910b7d71c9f
gzanitti <gzanitti@gmail.com>
2019-08-02 16:20:07 +0200

    Warded Datalog: check

--- neurolang/tests/test_warded_datalog.py
+++ neurolang/tests/test_warded_datalog.py
@@ -1,19 +1,31 @@
+import pytest
+
 from .. import expressions
+from .. import solver_datalog_naive as sdb
+from .. import solver_datalog_extensional_db
+from .. import datalog_chase as dc
+from .. import expression_walker as ew
 from ..solver_datalog_naive import (
     Implication, Fact
 )
+from ..warded_datalog import WardedDatalog, NeuroLangDataLogNonWarded
 
-from ..warded_datalog import WardedDatalog
+class Datalog(
+    sdb.DatalogBasic,
+    solver_datalog_extensional_db.ExtensionalDatabaseSolver,
+    ew.ExpressionBasicEvaluator
+):
+    def function_gt(self, x: int, y: int)->bool:
+        return x > y
 
 C_ = expressions.Constant
 S_ = expressions.Symbol
 Fa_ = expressions.FunctionApplication
 Ep_ = expressions.ExistentialPredicate
 Eb_ = expressions.ExpressionBlock
-I_ = Implication
-F_ = Fact
 
-def test_warded_walker():
+
+def test_warded_walker_1():
     x = S_('x')
     y = S_('y')
     z = S_('z')
@@ -21,18 +33,122 @@ def test_warded_walker():
     Q = S_('Q')
     R = S_('R')
 
-    P1 = F_(Q(x, z))
-    P2 = I_(R(y, z), P(x, y))
-    #P3 = F_(P(z, y))
-    P4 = I_(R(x, z), Q(x,z) & P(x))
-
-    program = Eb_((
-        P1,
-        P2,
-        #P3,
-        P4,
+    datalog_program = Eb_((
+        Fact(Q(x, z)),
+        Implication(R(y, z), P(x, y)),
+        Implication(R(x, z), Q(x,z) & P(x)),
     ))
 
     wd = WardedDatalog()
-    result = wd.walk(program)
-    print(result)
+    warded = wd.walk(datalog_program)
+
+    assert warded == True
+
+def test_warded_walker_2():
+    P = S_('P')
+    Q = S_('Q')
+    T = S_('T')
+    x = S_('x')
+    y = S_('y')
+    z = S_('z')
+
+    datalog_program = Eb_((
+        Implication(Q(z, x), P(x)),
+        Implication(T(x), Q(x, y) & P(y)),
+    ))
+
+    wd = WardedDatalog()
+    warded = wd.walk(datalog_program)
+
+    assert warded == True
+
+
+def test_variables_outside_ward():
+    P = S_('P')
+    Q = S_('Q')
+    T = S_('T')
+    x = S_('x')
+    y = S_('y')
+    z = S_('z')
+
+    datalog_program = Eb_((
+        Implication(Q(z, x), P(x)),
+        Implication(T(x), Q(x, y) & P(x)),
+    ))
+
+    wd = WardedDatalog()
+    with pytest.raises(
+        NeuroLangDataLogNonWarded,
+        match=r"The program is not warded: there are dangerous variables outside the ward.*"
+    ):
+        warded = wd.walk(datalog_program)
+
+
+
+def test_more_one_atom():
+    P = S_('P')
+    Q = S_('Q')
+    R = S_('R')
+    S = S_('S')
+    T = S_('T')
+    x = S_('x')
+    y = S_('y')
+    z = S_('z')
+
+    datalog_program = Eb_((
+        Implication(Q(z, x), P(x)),
+        Implication(R(y, x), S(x)),
+        Implication(T(x), Q(x, y) & P(y) & R(x, z) & S(z)),
+    ))
+
+    wd = WardedDatalog()
+    with pytest.raises(
+        NeuroLangDataLogNonWarded,
+        match=r"The program is not warded: there are dangerous variables that appear in more than one atom of the body.*"
+    ):
+        warded = wd.walk(datalog_program)
+
+
+def test_warded_chase():
+    hsbc = C_('HSBC')
+    hsb = C_('HSB')
+    iba = C_('IBA')
+
+    company = S_('Company')
+    controls = S_('Controls')
+    owns = S_('Owns')
+    stock = S_('Stock')
+    PSC = S_('PSC')
+    strongLink = S_('StrongLink')
+
+    x = S_('x')
+    p = S_('p')
+    s = S_('s')
+    y = S_('y')
+
+    datalog_program = Eb_((
+        Fact(company(hsbc)),
+        Fact(company(hsb)),
+        Fact(company(iba)),
+        Fact(controls(hsbc, hsb)),
+        Fact(controls(hsb, iba)),
+
+        Implication(owns(p, s, x), company(x)),
+        Implication(stock(x, s), owns(p, s, x)),
+        Implication(PSC(x, p), owns(p, s, x)),
+        Implication(owns(p, s, y), PSC(x, p) & controls(x, y)),
+        Implication(strongLink(x, y), PSC(x, p) & PSC(y, p)),
+        Implication(owns(p, s, x), strongLink(x, y)),
+        Implication(owns(p, s, y), strongLink(x, y)),
+        Implication(company(x), stock(x, s)),
+    ))
+
+    wd = WardedDatalog()
+    wd.walk(datalog_program)
+
+    #dl = Datalog()
+    #dl.walk(datalog_program)
+
+    #solution_instance = dc.build_chase_solution(dl)
+
+    #print(solution_instance)
--- neurolang/warded_datalog.py
+++ neurolang/warded_datalog.py
@@ -3,16 +3,21 @@ from .expressions import (
     FunctionApplication,
 )
 from .expression_walker import (
-    PatternWalker, add_match
+    PatternWalker, add_match, expression_iterator
 )
 from .solver_datalog_naive import (
     Implication, Fact
 )
 
+from .exceptions import NeuroLangException
+
+class NeuroLangDataLogNonWarded(NeuroLangException):
+    pass
+
 class WardedDatalog(PatternWalker):
 
     def __init__(self):
-        self.can_be_dangerous = []
+        self.can_be_dangerous = dict({})
 
 
     @add_match(ExpressionBlock)
@@ -20,18 +25,20 @@ class WardedDatalog(PatternWalker):
         for rule in expression.expressions:
             self.walk(rule)
 
-        cwd = CheckWardedDatalog(self.can_be_dangerous)
-        cwd.walk(expression)
+        cdv = CheckDangerousVariables(self.can_be_dangerous)
+        cdv.walk(expression)
+
+        return True
 
 
     @add_match(FunctionApplication(Constant, ...))
     def warded_function_constant(self, expression):
-        args = set()
+        symbols = set()
         for arg in expression.args:
             temp = self.walk(arg)
-            args = args.union(temp)
+            symbols = symbols.union(temp)
 
-        return args
+        return symbols
 
 
     @add_match(FunctionApplication)
@@ -39,19 +46,20 @@ class WardedDatalog(PatternWalker):
         symbols = set()
         for arg in expression.args:
             symbol = self.walk(arg)
-            symbols.add(symbol)
+            symbols = symbols.union(symbol)
 
         return symbols
 
 
     @add_match(Fact)
     def warded_fact(self, expression):
-        symbols = set()
-        for arg in expression.fact.args:
-            symbol = self.walk(arg)
-            symbols.add(symbol)
+        pass
+        #symbols = set()
+        #for arg in expression.fact.args:
+        #    symbol = self.walk(arg)
+        #    symbols = symbols.union(symbol)
 
-        return symbols
+        #return symbols
 
 
     @add_match(Implication)
@@ -63,12 +71,13 @@ class WardedDatalog(PatternWalker):
 
         for var in free_vars:
             if var in consequent:
-                self.can_be_dangerous.append(var)
+                position = self.calc_position(var, expression.consequent)
+                self.can_be_dangerous = self.merge_dicts(self.can_be_dangerous, position)
 
 
     @add_match(Symbol)
     def warded_symbol(self, expression):
-        return expression.name
+        return set(expression.name)
 
 
     @add_match(Constant)
@@ -76,17 +85,90 @@ class WardedDatalog(PatternWalker):
         pass
 
 
-class CheckWardedDatalog(PatternWalker):
+    def calc_position(self, var, expression):
+        for exp in expression_iterator(expression):
+            if var in exp[1].args:
+                return dict({exp[1].functor: [exp[1].args.index(var)]})
+
+
+    def merge_dicts(self, to_update_dic, new_dict):
+        for key, value in new_dict.items():
+            if key in to_update_dic:
+                old_values = to_update_dic[key]
+                old_values.append(value)
+                to_update_dic[key] = old_values
+            else:
+                to_update_dic[key] = [value]
+
+        return to_update_dic
+
+
+class CheckDangerousVariables(PatternWalker):
 
     def __init__(self, can_be_dangerous):
         self.can_be_dangerous = can_be_dangerous
+        self.dangerous_vars = {}
 
 
     @add_match(ExpressionBlock)
-    def warded_expression_block(self, expression):
+    def check_dangerous_block(self, expression):
         for rule in expression.expressions:
-            warded = self.walk(rule)
-            if not warded:
-                return False
-        else:
-            return True
+            self.walk(rule)
+
+
+    @add_match(Fact)
+    def check_dangerous_fact(self, expression):
+        pass
+
+
+    @add_match(Implication)
+    def check_dangerous_implication(self, expression):
+        antecedent = self.check_dangerous(expression.antecedent)
+        consequent = self.check_dangerous(expression.consequent)
+
+        dangerous_symbol = antecedent.intersection(consequent)
+        if len(dangerous_symbol) == 1 and next(iter(dangerous_symbol)) in expression.antecedent._symbols:
+            var = dangerous_symbol.pop()
+            dangerous_pos = self.can_be_dangerous[var].pop()
+
+            dangerous_var = self.get_name(expression.consequent, dangerous_pos)
+
+            single_body = self.check_var_single_body(dangerous_var, expression.antecedent)
+            if not single_body:
+                raise NeuroLangDataLogNonWarded(
+                    f'The program is not warded: there are dangerous variables outside the ward in {expression.antecedent}'
+                )
+
+
+    def check_dangerous(self, expression):
+        dangerous = set()
+        for key in self.can_be_dangerous.keys():
+            if key in expression.functor:
+                dangerous.add(key)
+
+        if len(dangerous) > 1:
+            raise NeuroLangDataLogNonWarded(
+                f'The program is not warded: there are dangerous variables that appear in more than one atom of the body in {expression}'
+            )
+
+        return dangerous
+
+    def get_name(self, expression, position):
+        names = [expression.args[index] for index in position]
+        #TODO Remove this conditional
+        if len(names) > 1:
+            raise NeuroLangException(
+                f'DEBUG: Unexpected length'
+            )
+        return names[0]
+
+    def check_var_single_body(self, var, expression):
+        founded = False
+        for exp in expression_iterator(expression):
+            if exp[0] is 'args':
+                if not founded and var in exp[1]:
+                    founded = True
+                elif founded and var in exp[1]:
+                    return False
+
+        return True
