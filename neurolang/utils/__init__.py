8f9735e115d4cec0dcd575be5754258feebc4bc7
Demian Wassermann <demian.wassermann@inria.fr>
2019-09-18 17:42:50 -0300

    Fixed merge

--- neurolang/utils/__init__.py
+++ neurolang/utils/__init__.py
@@ -1,22 +1,9 @@
 from .orderedset import OrderedSet
-<<<<<<< HEAD
 from .relational_algebra_set import (NamedRelationalAlgebraFrozenSet,
                                      RelationalAlgebraFrozenSet,
                                      RelationalAlgebraSet)
-||||||| merged common ancestors
-from .relational_algebra_set import RelationalAlgebraSet
-=======
-from .relational_algebra_set import (RelationalAlgebraFrozenSet,
-                                     RelationalAlgebraSet)
->>>>>>> master
 
-<<<<<<< HEAD
 __all__ = [
     'OrderedSet', 'RelationalAlgebraSet',
     'RelationalAlgebraFrozenSet', 'NamedRelationalAlgebraFrozenSet'
 ]
-||||||| merged common ancestors
-__all__ = ['OrderedSet', 'RelationalAlgebraSet']
-=======
-__all__ = ['OrderedSet', 'RelationalAlgebraSet', 'RelationalAlgebraFrozenSet']
->>>>>>> master
