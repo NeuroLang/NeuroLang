8af7ad1fc4e7d7ec2ff0a4496916d6996e30dd06
Demian Wassermann <demian.wassermann@inria.fr>
2019-10-12 14:39:46 +0200

    instance object works

--- neurolang/datalog/instance.py
+++ neurolang/datalog/instance.py
@@ -1,34 +1,34 @@
-from collections.abc import Set, Mapping, MutableSet
+from collections.abc import Set, Mapping, MutableSet, MutableMapping
 
-from ..solver_datalog_naive import Fact
+from ..expression_walker import ReplaceExpressionsByValues
 from ..exceptions import NeuroLangException
 
 
-def factset_as_dict(factset):
+def factset_as_dict(factset, set_type=frozenset):
     result = dict()
     for fact in factset:
         predicate = fact.consequent.functor
         if predicate not in result:
-            result[predicate] = set()
-        result[predicate].add(tuple(fact.consequent.args))
+            result[predicate] = []
+        result[predicate].append(tuple(fact.consequent.args))
     for predicate in result:
-        result[predicate] = frozenset(result[predicate])
+        result[predicate] = set_type(result[predicate])
     return result
 
 
-class Instance:
+class FrozenInstance:
+    _set_type = frozenset
+
     def __init__(self, elements=None):
         if elements is None:
             elements = dict()
         if isinstance(elements, Mapping):
-            if any(
-                not isinstance(tuple_set, Set) or
-                isinstance(tuple_set, MutableSet)
-                for tuple_set in elements.values()
-            ):
-                raise NeuroLangException('Expected immutable tuple sets')
+            elements = {
+                k: self._set_type(v)
+                for k, v in elements.items()
+            }
         else:
-            elements = factset_as_dict(elements)
+            elements = factset_as_dict(elements, set_type=self._set_type)
         self.elements = elements
         self.cached_hash = None
 
@@ -39,36 +39,29 @@ class Instance:
 
     def __or__(self, other):
         new_elements = dict()
-        for predicate in (
-            set(self.elements.keys()).union(set(other.elements.keys()))
-        ):
-            new_elements[predicate] = frozenset(
-                self.elements.get(predicate, frozenset()).union(
-                    other.elements.get(predicate, frozenset())
-                )
+        for predicate in (self.elements.keys() | other.elements.keys()):
+            new_elements[predicate] = self._set_type(
+                self.elements.get(predicate, self._set_type()) |
+                other.elements.get(predicate, self._set_type())
             )
-        return self.__class__(new_elements)
+
+        return type(self)(new_elements)
 
     def __sub__(self, other):
         new_elements = dict()
-        for predicate in self.elements.keys():
-            if predicate in other.elements:
-                tuples = self.elements[predicate] - other.elements[predicate]
-                if len(tuples) > 0:
-                    new_elements[predicate] = tuples
-            else:
-                new_elements[predicate] = self.elements[predicate]
-        return self.__class__(new_elements)
+        for predicate, tuple_set in self.elements.items():
+            new_set = tuple_set - other.elements.get(predicate, set())
+            if len(new_set) > 0:
+                new_elements[predicate] = new_set
+        return type(self)(new_elements)
 
     def __and__(self, other):
         new_elements = dict()
-        for predicate in (
-            set(self.elements.keys()) & set(other.elements.keys())
-        ):
-            new_elements[predicate] = (
-                self.elements[predicate] & other.elements[predicate]
-            )
-        return self.__class__(new_elements)
+        for predicate in (self.elements.keys() & other.elements.keys()):
+            new_set = self.elements[predicate] & other.elements[predicate]
+            if len(new_set) > 0:
+                new_elements[predicate] = new_set
+        return type(self)(new_elements)
 
     def copy(self):
         new_copy = type(self)()
@@ -77,7 +70,7 @@ class Instance:
         return new_copy
 
 
-class MapInstance(Instance, Mapping):
+class FrozenMapInstance(FrozenInstance, Mapping):
     def __getitem__(self, predicate):
         return self.elements[predicate]
 
@@ -88,16 +81,18 @@ class MapInstance(Instance, Mapping):
         return len(self.elements)
 
     def as_set(self):
-        return SetInstance(self.elements)
+        out = FrozenSetInstance()
+        out.elements = self.elements
+        return out
 
 
-class SetInstance(Instance, Set):
-    def __contains__(self, fact):
-        predicate = fact.consequent.functor
-        tuple_values = fact.consequent.args
+class FrozenSetInstance(FrozenInstance, Set):
+    def __contains__(self, predicate):
+        functor = predicate.functor
+        tuple_values = predicate.args
         return (
-            predicate in self.elements and
-            tuple_values in self.elements[predicate]
+            functor in self.elements and
+            tuple_values in self.elements[functor]
         )
 
     def __len__(self):
@@ -106,7 +101,78 @@ class SetInstance(Instance, Set):
     def __iter__(self):
         for predicate, tuples in self.elements.items():
             for t in tuples:
-                yield Fact(predicate(*t))
+                yield predicate(*t)
 
     def as_map(self):
-        return MapInstance(self.elements)
+        out = FrozenMapInstance()
+        out.elements = self.elements
+        return out
+
+
+class Instance(FrozenInstance):
+    _set_type = set
+
+    def __hash__(self):
+        raise TypeError('Instance objects are mutable and cannot be hashed')
+
+    def __ior__(self, other):
+        for predicate in (self.elements.keys() & other.elements.keys()):
+            self.elements[predicate] |= other.elements[predicate]
+        for predicate in (other.elements.keys() - self.elements.keys()):
+            self.elements[predicate] = other.elements[predicate]
+        return self
+
+    def __isub__(self, other):
+        for predicate in (self.elements.keys() & other.elements.keys()):
+            self.elements[predicate] -= other.elements[predicate]
+            if len(self.elements[predicate]) == 0:
+                del self.elements[predicate]
+        return self
+
+    def __iand__(self, other):
+        subs = self.elements.keys() - other.elements.keys()
+        for predicate in subs:
+            del self.elements[predicate]
+        for predicate in self.elements:
+            self.elements[predicate] &= other.elements[predicate]
+            if len(self.elements[predicate]) == 0:
+                del self.elements[predicate]
+        return self
+
+    def copy(self):
+        new_copy = type(self)()
+        new_copy.elements = self.elements.copy()
+        return new_copy
+
+
+class MapInstance(Instance, FrozenMapInstance, MutableMapping):
+    def __setitem__(self, predicate, value):
+        self.elements[predicate] = self._set_type(value)
+
+    def __delitem__(self, predicate):
+        del self.elements[predicate]
+
+    def as_set(self):
+        out = SetInstance()
+        out.elements = self.elements
+        return out
+
+
+class SetInstance(Instance, FrozenSetInstance, MutableSet):
+    _rebv = ReplaceExpressionsByValues(dict())
+
+    def add(self, fact):
+        if fact.functor not in self.elements:
+            self.elements[fact.functor] = self._set_type
+        self.elements[fact.functor].add(self._rebv(fact.args))
+
+    def discard(self, fact):
+        value = self._rebv(fact.args)
+        self.elements[fact.functor].discard(value)
+        if len(self.elements[fact.functor]) == 0:
+            del self.elements[fact.functor]
+
+    def as_map(self):
+        out = MapInstance()
+        out.elements = self.elements
+        return out
--- neurolang/tests/test_datalog_instance.py
+++ neurolang/tests/test_datalog_instance.py
@@ -1,6 +1,8 @@
+from ..datalog import Fact
+from ..datalog.instance import (
+    FrozenMapInstance, FrozenSetInstance, MapInstance, SetInstance
+)
 from ..expressions import Constant, Symbol
-from ..solver_datalog_naive import Fact
-from ..datalog.instance import SetInstance, MapInstance
 
 S_ = Symbol
 C_ = Constant
@@ -11,17 +13,18 @@ P = Symbol('P')
 
 
 def test_set_instance_contains_facts():
-    elements = {Q: frozenset({(C_(2), ), (C_(3), )})}
-    instance = SetInstance(elements)
-    assert F_(Q(C_(2))) in instance
-    assert F_(Q(C_(3))) in instance
-    assert F_(Q(C_(4))) not in instance
+    elements = {Q: ({(C_(2), ), (C_(3), )})}
+    instance = FrozenSetInstance(elements)
+    assert Q(C_(2)) in instance
+    assert Q(C_(3)) in instance
+    assert Q(C_(4)) not in instance
     assert hash(instance) is not None
+    assert set(iter(instance)) == {Q(C_(2)), Q(C_(3))}
 
 
 def test_map_instance_contains_facts():
-    elements = {Q: frozenset({(C_(2), ), (C_(3), )})}
-    instance = MapInstance(elements)
+    elements = {Q: ({(C_(2), ), (C_(3), )})}
+    instance = FrozenMapInstance(elements)
     assert Q in instance
     assert hash(instance) is not None
 
@@ -42,7 +45,7 @@ def test_convert_instance_to_set():
     elements = {
         Q: frozenset({(C_(2), ), (C_(3), )}),
     }
-    set_instance = SetInstance(elements)
+    set_instance = FrozenSetInstance(elements)
     map_instance = set_instance.as_map()
     assert set_instance.elements is map_instance.elements
 
@@ -65,10 +68,10 @@ def test_instance_union():
         P: frozenset({(C_(3), )}),
     }
     instance = SetInstance(elements1) | SetInstance(elements2)
-    assert F_(Q(C_(2))) in instance
-    assert F_(Q(C_(3))) in instance
-    assert F_(Q(C_(4))) in instance
-    assert F_(P(C_(3))) in instance
+    assert Q(C_(2)) in instance
+    assert Q(C_(3)) in instance
+    assert Q(C_(4)) in instance
+    assert P(C_(3)) in instance
 
 
 def test_instance_difference():
@@ -81,7 +84,7 @@ def test_instance_difference():
         P: frozenset({(C_(3), )}),
     }
     instance = SetInstance(elements2) - SetInstance(elements1)
-    assert F_(Q(C_(4))) in instance
+    assert Q(C_(4)) in instance
     assert len(instance) == 1
 
 
@@ -95,6 +98,53 @@ def test_instance_intersection():
         P: frozenset({(C_(3), )}),
     }
     instance = SetInstance(elements2) & SetInstance(elements1)
-    assert F_(Q(C_(3))) in instance
-    assert F_(P(C_(3))) in instance
+    assert Q(C_(3)) in instance
+    assert P(C_(3)) in instance
+    assert len(instance) == 2
+
+
+def test_instance_union_update():
+    elements1 = {
+        Q: frozenset({(C_(2), ), (C_(3), )}),
+    }
+    elements2 = {
+        Q: frozenset({(C_(4), ), (C_(3), )}),
+        P: frozenset({(C_(3), )}),
+    }
+    instance = SetInstance(elements1)
+    instance |= SetInstance(elements2)
+    assert Q(C_(2)) in instance
+    assert Q(C_(3)) in instance
+    assert Q(C_(4)) in instance
+    assert P(C_(3)) in instance
+
+
+def test_instance_difference_update():
+    elements1 = {
+        Q: frozenset({(C_(2), ), (C_(3), )}),
+        P: frozenset({(C_(3), )})
+    }
+    elements2 = {
+        Q: frozenset({(C_(4), )}),
+        P: frozenset({(C_(3), )}),
+    }
+    instance = SetInstance(elements2)
+    instance -= SetInstance(elements1)
+    assert Q(C_(4)) in instance
+    assert len(instance) == 1
+
+
+def test_instance_intersection_update():
+    elements1 = {
+        Q: frozenset({(C_(2), ), (C_(3), )}),
+        P: frozenset({(C_(3), )})
+    }
+    elements2 = {
+        Q: frozenset({(C_(3), )}),
+        P: frozenset({(C_(3), )}),
+    }
+    instance = SetInstance(elements2)
+    instance &= SetInstance(elements1)
+    assert Q(C_(3)) in instance
+    assert P(C_(3)) in instance
     assert len(instance) == 2
