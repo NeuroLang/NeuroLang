121a0836d51be957fd639beff6e930becf13702a
Valentin Iovene <valentin@too.gy>
2019-10-18 15:39:42 +0200

    Prob(Data)Log to Bayesian Network (#195)

---
+++ neurolang/probabilistic/expressions.py
@@ -0,0 +1,30 @@
+from typing import Mapping
+
+from ..expressions import Definition, Constant, Symbol
+
+
+class Distribution(Definition):
+    def __init__(self, parameters):
+        self.parameters = parameters
+
+
+class DiscreteDistribution(Distribution):
+    pass
+
+
+class TableDistribution(DiscreteDistribution):
+    def __init__(self, table):
+        self.table = table
+        super().__init__(
+            Constant[Mapping]({Symbol[Constant[Mapping]]("table"): self.table})
+        )
+
+    def __repr__(self):
+        return "TableDistribution[\n{}\n]".format(
+            "\n".join(
+                [
+                    f"\t{value}:\t{prob}"
+                    for value, prob in self.table.value.items()
+                ]
+            )
+        )
