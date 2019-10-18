1016e574463d5b7c0523bec1383514d48fe14cae
Demian Wassermann <demian.wassermann@inria.fr>
2019-10-18 11:59:14 +0200

    Increased testing coverage

--- neurolang/datalog/tests/test_magic_sets.py
+++ neurolang/datalog/tests/test_magic_sets.py
@@ -1,11 +1,11 @@
 import operator
 
+from pytest import raises
+
 from ... import expression_walker, expressions
-from .. import Implication, Fact, DatalogProgram
-from .. import magic_sets
+from .. import DatalogProgram, Fact, Implication, magic_sets
 from ..chase import Chase
 from ..expressions import TranslateToLogic
-
 
 C_ = expressions.Constant
 S_ = expressions.Symbol
@@ -46,6 +46,9 @@ def test_adorned_expression():
     assert ax == ax_
     assert ax != ax__
 
+    with raises(NotImplementedError):
+        magic_sets.AdornedExpression(a, 'b', 1).name
+
 
 def test_resolution_works():
     x = S_('X')
@@ -82,6 +85,43 @@ def test_resolution_works():
 
     solution = Chase(dl).build_chase_solution()
     assert solution[goal].value == {C_((e,)) for e in (b, c, d)}
+
+
+def test_resolution_works_query_constant():
+    x = S_('X')
+    y = S_('Y')
+    z = S_('Z')
+    anc = S_('anc')
+    par = S_('par')
+    q = S_('q')
+    a = C_('a')
+    b = C_('b')
+    c = C_('c')
+    d = C_('d')
+
+    edb = Eb_([
+        F_(par(a, b)),
+        F_(par(b, c)),
+        F_(par(c, d)),
+    ])
+
+    code = Eb_([
+        Imp_(q(x, a), anc(a, x)),
+        Imp_(anc(x, y), par(x, y)),
+        Imp_(anc(x, y), anc(x, z) & par(z, y)),
+    ])
+
+    dl = Datalog()
+    dl.walk(code)
+    dl.walk(edb)
+    goal, mr = magic_sets.magic_rewrite(q(x), dl)
+
+    dl = Datalog()
+    dl.walk(mr)
+    dl.walk(edb)
+
+    solution = Chase(dl).build_chase_solution()
+    assert solution[goal].value == {C_((e, a)) for e in (b, c, d)}
 
 
 def test_resolution_works_builtin():
