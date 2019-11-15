35c72dc1d92bf06f72823cbd5830aa268c97e1cb
Kshitij Chawla (kchawla-pi) <kc.insight.pi@gmail.com>
2019-11-15 03:51:40 +0100

    Restored inadvertently changed example; Restored caching of venv

--- examples/plot_neurolang_datalog_example.py
+++ examples/plot_neurolang_datalog_example.py
@@ -14,84 +14,77 @@ import numpy as np
 
 from neurolang import frontend as fe
 
-from joblib import Memory
-location = './cachedir'
-memory = Memory(location)
-
 ###############################################################################
 # Load the Destrieux example from nilearn
 # ---------------------------------------
-def costly_compute():
-    destrieux_dataset = datasets.fetch_atlas_destrieux_2009()
-    destrieux_map = nib.load(destrieux_dataset['maps'])
+
+destrieux_dataset = datasets.fetch_atlas_destrieux_2009()
+destrieux_map = nib.load(destrieux_dataset['maps'])
 
 
-    ###############################################################################
-    # Initialize the NeuroLang instance and load Destrieux's cortical parcellation
-    # -----------------------------------------------------------------------------
+###############################################################################
+# Initialize the NeuroLang instance and load Destrieux's cortical parcellation
+# -----------------------------------------------------------------------------
 
 
-    nl = fe.NeurolangDL()
-    destrieux = nl.new_symbol(name='destrieux')
-    d = []
-    for label_number, name in destrieux_dataset['labels']:
-        if label_number == 0:
-            continue
-        name = name.decode()
-        region = nl.create_region(destrieux_map, label=label_number)
-        if region is None:
-            continue
-        name = name.replace('-', '_').replace(' ', '_')
-        d.append((name.lower(), region))
+nl = fe.NeurolangDL()
+destrieux = nl.new_symbol(name='destrieux')
+d = []
+for label_number, name in destrieux_dataset['labels']:
+    if label_number == 0:
+        continue
+    name = name.decode()
+    region = nl.create_region(destrieux_map, label=label_number)
+    if region is None:
+        continue
+    name = name.replace('-', '_').replace(' ', '_')
+    d.append((name.lower(), region))
 
-    destrieux = nl.add_tuple_set(d, name='destrieux')
-
-
-    ###############################################################################
-    # Add a function to measure a region's volume
-    # -----------------------------------------------------------------------------
-
-    @nl.add_symbol
-    def region_volume(region: fe.ExplicitVBR) -> float:
-        volume = (
-            len(region.voxels) *
-            float(np.product(np.abs(np.linalg.eigvals(region.affine[:-1, :-1]))))
-        )
-        return volume
+destrieux = nl.add_tuple_set(d, name='destrieux')
 
 
-    ###############################################################################
-    # Load all contiguous regions from Neurosynth that fit the term "supramarginal"
-    # -----------------------------------------------------------------------------
+###############################################################################
+# Add a function to measure a region's volume
+# -----------------------------------------------------------------------------
+
+@nl.add_symbol
+def region_volume(region: fe.ExplicitVBR) -> float:
+    volume = (
+        len(region.voxels) *
+        float(np.product(np.abs(np.linalg.eigvals(region.affine[:-1, :-1]))))
+    )
+    return volume
 
 
-    neurosynth_supramarginal = nl.load_neurosynth_term_regions(
-        'supramarginal',
-        name='neurosynth_supramarginal'
+###############################################################################
+# Load all contiguous regions from Neurosynth that fit the term "supramarginal"
+# -----------------------------------------------------------------------------
+
+
+neurosynth_supramarginal = nl.load_neurosynth_term_regions(
+    'supramarginal',
+    name='neurosynth_supramarginal'
+)
+
+
+########################################################################
+# Query all Destrieux regions that overlap with NeuroSynth supramarginal
+# region having volume larger than 2500mm3 with the environment
+# ----------------------------------------------------------------------
+
+
+with nl.environment as e:
+    res = nl.query(
+            e.query(e.name, e.region_1),
+            e.destrieux(e.name, e.region_1) &
+            neurosynth_supramarginal(e.region_2) &
+            (region_volume(e.region_2) > 2500) &
+            nl.symbols.overlapping(e.region_1, e.region_2)
     )
 
 
-    ########################################################################
-    # Query all Destrieux regions that overlap with NeuroSynth supramarginal
-    # region having volume larger than 2500mm3 with the environment
-    # ----------------------------------------------------------------------
-
-
-    with nl.environment as e:
-        res = nl.query(
-                e.query(e.name, e.region_1),
-                e.destrieux(e.name, e.region_1) &
-                neurosynth_supramarginal(e.region_2) &
-                (region_volume(e.region_2) > 2500) &
-                nl.symbols.overlapping(e.region_1, e.region_2)
-        )
-    return res
-# from examples.helper import cache_obj
-# res = cache_obj(res)
-costly_compute = memory.cache(costly_compute)
-res = costly_compute()
 for name, region in res.value:
-     plotting.plot_roi(region.spatial_image(), title=name)
+    plotting.plot_roi(region.spatial_image(), title=name)
 
 
 ########################################################################
@@ -100,18 +93,18 @@ for name, region in res.value:
 # ----------------------------------------------------------------------
 
 
-# region_1 = nl.new_symbol(name='region_1')
-# region_2 = nl.new_symbol(name='region_2')
-# query = nl.new_symbol(name='query')
-# name = nl.new_symbol(name='name')
-#
-# res = nl.query(
-#         query(name, region_1),
-#         destrieux(name, region_1) & neurosynth_supramarginal(region_2) &
-#         (region_volume(region_2) > 2500) &
-#         nl.symbols.overlapping(region_1, region_2)
-# )
-#
-#
-# for name, region in res.value:
-#     plotting.plot_roi(region.spatial_image(), title=name)
+region_1 = nl.new_symbol(name='region_1')
+region_2 = nl.new_symbol(name='region_2')
+query = nl.new_symbol(name='query')
+name = nl.new_symbol(name='name')
+
+res = nl.query(
+        query(name, region_1),
+        destrieux(name, region_1) & neurosynth_supramarginal(region_2) &
+        (region_volume(region_2) > 2500) &
+        nl.symbols.overlapping(region_1, region_2)
+)
+
+
+for name, region in res.value:
+    plotting.plot_roi(region.spatial_image(), title=name)
