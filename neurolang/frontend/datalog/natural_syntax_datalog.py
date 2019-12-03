7f538dd88527871f5e87978e1fbd2737cad22e0e
Demian Wassermann <demian.wassermann@inria.fr>
2019-12-03 18:02:00 +0100

    Reduced code duplication

--- neurolang/frontend/datalog/natural_syntax_datalog.py
+++ neurolang/frontend/datalog/natural_syntax_datalog.py
@@ -4,6 +4,8 @@ import tatsu
 
 from ...datalog import Conjunction, Fact, Implication, Negation, Union
 from ...expressions import Constant, Expression, Symbol
+from . import DatalogSemantics as DatalogClassicSemantics
+from . import ExternalSymbol
 
 GRAMMAR = u"""
     @@grammar::Datalog
@@ -123,42 +125,9 @@ OPERATOR = {
 COMPILED_GRAMMAR = tatsu.compile(GRAMMAR)
 
 
-class ExternalSymbol(Symbol):
-    def __repr__(self):
-        return "@S{{{}: {}}}".format(self.name, self.__type_repr__)
-
-
-class DatalogSemantics:
-    def __init__(self, locals=None, globals=None):
-        super().__init__()
-
-        if locals is None:
-            locals = {}
-        if globals is None:
-            globals = {}
-
-        self.locals = locals
-        self.globals = globals
-
-    def start(self, ast):
-        return ast
-
-    def expressions(self, ast):
-        if isinstance(ast, Expression):
-            ast = (ast,)
-        return Union(ast)
-
-    def fact(self, ast):
-        return Fact(ast)
-
+class DatalogSemantics(DatalogClassicSemantics):
     def constant_predicate(self, ast):
         return ast['predicate'](*ast['arguments'])
-
-    def rule(self, ast):
-        return Implication(ast[0], ast[2])
-
-    def body(self, ast):
-        return Conjunction(ast)
 
     def head_predicate(self, ast):
         if not isinstance(ast, Expression):
@@ -171,66 +140,6 @@ class DatalogSemantics:
     def predicate(self, ast):
         if not isinstance(ast, Expression):
             ast = ast['predicate'](*ast['arguments'])
-        return ast
-
-    def negated_predicate(self, ast):
-        return Negation(ast[1])
-
-    def comparison(self, ast):
-        operator = Constant(OPERATOR[ast[1]])
-        return operator(ast[0], ast[2])
-
-    def arguments(self, ast):
-        if isinstance(ast, Expression):
-            return (ast,)
-        return tuple(ast)
-
-    def ext_identifier(self, ast):
-        ast = ast[1]
-        return ExternalSymbol[ast.type](ast.name)
-
-    def arithmetic_operation(self, ast):
-        if isinstance(ast, Expression):
-            return ast
-
-        if len(ast) == 1:
-            return ast[0]
-
-        op = Constant(OPERATOR[ast[1]])
-
-        return op(*ast[0::2])
-
-    def term(self, ast):
-        if isinstance(ast, Expression):
-            return ast
-        elif len(ast) == 1:
-            return ast[0]
-
-        op = Constant(OPERATOR[ast[1]])
-
-        return op(ast[0], ast[2])
-
-    def factor(self, ast):
-        if isinstance(ast, Expression):
-            return ast
-        elif len(ast) == 1:
-            return ast[0]
-        else:
-            return Constant(pow)(ast[0], ast[2])
-
-    def function_application(self, ast):
-        return Symbol(ast[0])(*ast[2])
-
-    def identifier(self, ast):
-        return Symbol(ast)
-
-    def text(self, ast):
-        return Constant(ast[1])
-
-    def number(self, ast):
-        return Constant(float("".join(ast)))
-
-    def _default(self, ast):
         return ast
 
 
