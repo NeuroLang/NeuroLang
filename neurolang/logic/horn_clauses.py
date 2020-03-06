477bf7d3f75ea9253d84413162dd4861884c8024
Nikita Zdanovitch <nikita.zdanovitch@inria.fr>
2020-03-06 16:39:32 +0100

    Remove implications and move negations to atoms.

---
+++ neurolang/logic/horn_clauses.py
@@ -0,0 +1,52 @@
+
+from . import Implication, Conjunction, Disjunction, Negation, UniversalPredicate, ExistentialPredicate
+from ..expression_walker import IdentityWalker, add_match
+
+
+class EliminateImplications(IdentityWalker):
+    '''
+    Removes the implication ocurrences of an expression.
+
+    '''
+    @add_match(Implication(..., ...))
+    def remove_implication(self, implication):
+        c = self.walk(implication.consequent)
+        a = self.walk(implication.antecedent)
+        return Disjunction((c, Negation(a)))
+
+
+class MoveNegationsToAtoms(IdentityWalker):
+    '''
+    Moves the negations the furthest possible to the atoms.
+    Assumes that there are no implications in the expression.
+    '''
+    @add_match(Negation(UniversalPredicate(..., ...)))
+    def negated_universal(self, negation):
+        quantifier = negation.formula
+        x = quantifier.head
+        p = self.walk(Negation(quantifier.body))
+        return ExistentialPredicate(x, p)
+
+    @add_match(Negation(ExistentialPredicate(..., ...)))
+    def negated_existential(self, negation):
+        quantifier = negation.formula
+        x = quantifier.head
+        p = self.walk(Negation(quantifier.body))
+        return UniversalPredicate(x, p)
+
+    @add_match(Negation(Conjunction(...)))
+    def negated_conjunction(self, negation):
+        conj = negation.formula
+        l = lambda e: self.walk(Negation(e))
+        return Disjunction(tuple(map(l, conj.formulas)))
+
+    @add_match(Negation(Disjunction(...)))
+    def negated_disjunction(self, negation):
+        disj = negation.formula
+        l = lambda e: self.walk(Negation(e))
+        return Conjunction(tuple(map(l, disj.formulas)))
+
+    @add_match(Negation(Negation(...)))
+    def negated_negation(self, negation):
+        return negation.formula.formula
+    
---
+++ neurolang/tests/test_fol_to_datalog.py
@@ -0,0 +1,103 @@
+import typing
+
+from ..expressions import Symbol, Constant
+from ..logic import Implication, Disjunction, Conjunction, Negation, UniversalPredicate, ExistentialPredicate
+from ..logic.horn_clauses import EliminateImplications, MoveNegationsToAtoms
+
+
+
+def test_remove_implication():
+    A = Symbol('A')
+    B = Symbol('B')
+    exp = Implication(B, A)
+    res = EliminateImplications().walk(exp)
+    
+    assert isinstance(res, Disjunction)
+    assert res == Disjunction((B, Negation(A)))
+
+
+def test_remove_nested_implication():
+    A = Symbol('A')
+    B = Symbol('B')
+    exp = Implication(
+            Implication(Negation(A), Negation(B)),
+            Implication(B, A)
+          )
+    res = EliminateImplications().walk(exp)
+    
+    assert isinstance(res, Disjunction)
+    assert res == Disjunction((
+            Disjunction((Negation(A), Negation(Negation(B)))),
+            Negation(Disjunction((B, Negation(A))))
+        ))
+
+
+def test_remove_double_negation():
+    A = Symbol('A')
+    exp = Negation(Negation(A))
+    res = MoveNegationsToAtoms().walk(exp)
+    assert res == A
+
+
+def test_negated_universal():
+    X = Symbol('X')
+    P = Symbol('P')
+    exp = Negation(UniversalPredicate(X, P(X)))
+    res = MoveNegationsToAtoms().walk(exp)
+    assert res == ExistentialPredicate(X, Negation(P(X)))
+
+
+def test_negated_existensial():
+    X = Symbol('X')
+    P = Symbol('P')
+    exp = Negation(ExistentialPredicate(X, P(X)))
+    res = MoveNegationsToAtoms().walk(exp)
+    assert res == UniversalPredicate(X, Negation(P(X)))
+
+
+def test_negated_disjunction():
+    A = Symbol('A')
+    B = Symbol('B')
+    exp = Negation(Disjunction((A, B)))
+    res = MoveNegationsToAtoms().walk(exp)
+    assert res == Conjunction((Negation(A), Negation(B)))
+
+
+def test_negated_conjunction():
+    A = Symbol('A')
+    B = Symbol('B')
+    exp = Negation(Conjunction((A, B)))
+    res = MoveNegationsToAtoms().walk(exp)
+    assert res == Disjunction((Negation(A), Negation(B)))
+
+
+def test_long_negated_chain():
+    A = Symbol('A')
+    X = Symbol('X')
+    Y = Symbol('Y')
+    R = Symbol('R')
+    exp = Negation(
+            Disjunction((
+                A,
+                UniversalPredicate(
+                    X,
+                    ExistentialPredicate(
+                        Y,
+                        Negation(R(X, Y))
+                    )
+                )
+            ))
+        )
+    res = MoveNegationsToAtoms().walk(exp)
+    assert res == Conjunction((
+            Negation(A),
+            ExistentialPredicate(
+                X,
+                UniversalPredicate(
+                    Y,
+                    R(X, Y)
+                )
+            )
+        ))
+
+
