0593dd14c1ba094f2309279c5d7b1e28e62bf084
Nikita Zdanovitch <nikita.zdanovitch@inria.fr>
2020-03-09 18:16:49 +0100

    Skolemization.

--- neurolang/logic/horn_clauses.py
+++ neurolang/logic/horn_clauses.py
@@ -1,14 +1,21 @@
+from . import (
+    Implication,
+    Conjunction,
+    Disjunction,
+    Negation,
+    UniversalPredicate,
+    ExistentialPredicate,
+)
+from ..expressions import Constant, Symbol, FunctionApplication
+from ..expression_walker import ExpressionWalker, add_match
 
-from . import (Implication, Conjunction, Disjunction, Negation,
-               UniversalPredicate, ExistentialPredicate)
-from ..expression_walker import IdentityWalker, add_match
 
-
-class EliminateImplications(IdentityWalker):
-    '''
+class EliminateImplications(ExpressionWalker):
+    """
     Removes the implication ocurrences of an expression.
 
-    '''
+    """
+
     @add_match(Implication(..., ...))
     def remove_implication(self, implication):
         c = self.walk(implication.consequent)
@@ -16,11 +23,12 @@ class EliminateImplications(IdentityWalker):
         return Disjunction((c, Negation(a)))
 
 
-class MoveNegationsToAtoms(IdentityWalker):
-    '''
+class MoveNegationsToAtoms(ExpressionWalker):
+    """
     Moves the negations the furthest possible to the atoms.
     Assumes that there are no implications in the expression.
-    '''
+    """
+
     @add_match(Negation(UniversalPredicate(..., ...)))
     def negated_universal(self, negation):
         quantifier = negation.formula
@@ -38,19 +46,65 @@ class MoveNegationsToAtoms(IdentityWalker):
     @add_match(Negation(Conjunction(...)))
     def negated_conjunction(self, negation):
         conj = negation.formula
-        return Disjunction(tuple(map(
-            lambda e: self.walk(Negation(e)),
-            conj.formulas
-        )))
+        formulas = map(lambda e: self.walk(Negation(e)), conj.formulas)
+        return Disjunction(tuple(formulas))
 
     @add_match(Negation(Disjunction(...)))
     def negated_disjunction(self, negation):
         disj = negation.formula
-        return Conjunction(tuple(map(
-            lambda e: self.walk(Negation(e)),
-            disj.formulas
-        )))
+        formulas = map(lambda e: self.walk(Negation(e)), disj.formulas)
+        return Conjunction(tuple(formulas))
 
     @add_match(Negation(Negation(...)))
     def negated_negation(self, negation):
         return negation.formula.formula
+
+
+class Skolemize(ExpressionWalker):
+    """
+    Replaces the existential quantifiers and introduces
+    Skolem constants for quantified variables.
+    """
+
+    def __init__(self):
+        self.used_symbols = []
+        self.mapping = []
+        self.universally_quantified_variables = []
+
+    def fresh_skolem_constant(self):
+        c = Symbol.fresh()
+        c.skolem_constant = True
+        # Should check if the symbol is already present in the expression
+        # ... maybe not if using a stack for the existential variables
+        self.used_symbols.append(c)
+        return c
+
+    @add_match(UniversalPredicate)
+    def universal_quantifier(self, expression):
+        self.universally_quantified_variables.append(expression.head)
+        new_body = self.walk(expression.body)
+        self.universally_quantified_variables.remove(expression.head)
+        return UniversalPredicate(expression.head, new_body)
+
+    @add_match(ExistentialPredicate)
+    def existential_quantifier(self, expression):
+        c = self.fresh_skolem_constant()
+        if self.universally_quantified_variables:
+            c = c(*self.universally_quantified_variables)
+        self.mapping.append((expression.head, c))
+        new_body = self.walk(expression.body)
+        self.mapping.remove((expression.head, c))
+        return new_body
+
+    @add_match(Symbol)
+    def _match_symbol(self, symbol):
+        for s, r in reversed(self.mapping):
+            if s == symbol:
+                return r
+        return symbol
+
+    @add_match(
+        FunctionApplication, lambda e: hasattr(e.functor, "skolem_constant")
+    )
+    def _match_function_application(self, f):
+        return f
--- neurolang/tests/test_fol_to_datalog.py
+++ neurolang/tests/test_fol_to_datalog.py
@@ -1,101 +1,182 @@
 import typing
+from unittest.mock import patch
 
 from ..expressions import Symbol, Constant
-from ..logic import Implication, Disjunction, Conjunction, Negation, UniversalPredicate, ExistentialPredicate
-from ..logic.horn_clauses import EliminateImplications, MoveNegationsToAtoms
-
+from ..logic import (
+    Implication,
+    Disjunction,
+    Conjunction,
+    Negation,
+    UniversalPredicate,
+    ExistentialPredicate,
+)
+from ..logic.horn_clauses import (
+    EliminateImplications,
+    MoveNegationsToAtoms,
+    Skolemize,
+    Symbol,
+)
 
 
 def test_remove_implication():
-    A = Symbol('A')
-    B = Symbol('B')
+    A = Symbol("A")
+    B = Symbol("B")
     exp = Implication(B, A)
     res = EliminateImplications().walk(exp)
-    
+
     assert isinstance(res, Disjunction)
     assert res == Disjunction((B, Negation(A)))
 
 
 def test_remove_nested_implication():
-    A = Symbol('A')
-    B = Symbol('B')
-    exp = Implication(
-            Implication(Negation(A), Negation(B)),
-            Implication(B, A)
-          )
+    A = Symbol("A")
+    B = Symbol("B")
+    exp = Implication(Implication(Negation(A), Negation(B)), Implication(B, A))
     res = EliminateImplications().walk(exp)
-    
+
     assert isinstance(res, Disjunction)
-    assert res == Disjunction((
+    assert res == Disjunction(
+        (
             Disjunction((Negation(A), Negation(Negation(B)))),
-            Negation(Disjunction((B, Negation(A))))
-        ))
+            Negation(Disjunction((B, Negation(A)))),
+        )
+    )
+
+
+def test_remove_nested_implication():
+    A = Symbol("A")
+    B = Symbol("B")
+    exp = Disjunction((A, Implication(B, A)))
+    res = EliminateImplications().walk(exp)
+    assert res == Disjunction((A, Disjunction((B, Negation(A)))))
 
 
 def test_remove_double_negation():
-    A = Symbol('A')
+    A = Symbol("A")
     exp = Negation(Negation(A))
     res = MoveNegationsToAtoms().walk(exp)
     assert res == A
 
 
 def test_negated_universal():
-    X = Symbol('X')
-    P = Symbol('P')
+    X = Symbol("X")
+    P = Symbol("P")
     exp = Negation(UniversalPredicate(X, P(X)))
     res = MoveNegationsToAtoms().walk(exp)
     assert res == ExistentialPredicate(X, Negation(P(X)))
 
 
 def test_negated_existensial():
-    X = Symbol('X')
-    P = Symbol('P')
+    X = Symbol("X")
+    P = Symbol("P")
     exp = Negation(ExistentialPredicate(X, P(X)))
     res = MoveNegationsToAtoms().walk(exp)
     assert res == UniversalPredicate(X, Negation(P(X)))
 
 
 def test_negated_disjunction():
-    A = Symbol('A')
-    B = Symbol('B')
+    A = Symbol("A")
+    B = Symbol("B")
     exp = Negation(Disjunction((A, B)))
     res = MoveNegationsToAtoms().walk(exp)
     assert res == Conjunction((Negation(A), Negation(B)))
 
 
 def test_negated_conjunction():
-    A = Symbol('A')
-    B = Symbol('B')
+    A = Symbol("A")
+    B = Symbol("B")
     exp = Negation(Conjunction((A, B)))
     res = MoveNegationsToAtoms().walk(exp)
     assert res == Disjunction((Negation(A), Negation(B)))
 
 
 def test_long_negated_chain():
-    A = Symbol('A')
-    X = Symbol('X')
-    Y = Symbol('Y')
-    R = Symbol('R')
+    A = Symbol("A")
+    X = Symbol("X")
+    Y = Symbol("Y")
+    R = Symbol("R")
     exp = Negation(
-            Disjunction((
+        Disjunction(
+            (
                 A,
                 UniversalPredicate(
-                    X,
-                    ExistentialPredicate(
-                        Y,
-                        Negation(R(X, Y))
-                    )
-                )
-            ))
-        )
-    res = MoveNegationsToAtoms().walk(exp)
-    assert res == Conjunction((
-            Negation(A),
-            ExistentialPredicate(
-                X,
-                UniversalPredicate(
-                    Y,
-                    R(X, Y)
-                )
+                    X, ExistentialPredicate(Y, Negation(R(X, Y)))
+                ),
             )
-        ))
+        )
+    )
+    res = MoveNegationsToAtoms().walk(exp)
+    assert res == Conjunction(
+        (Negation(A), ExistentialPredicate(X, UniversalPredicate(Y, R(X, Y))))
+    )
+
+
+def test_skolemize_without_existentials():
+    A = Symbol("A")
+    B = Symbol("B")
+    exp = Negation(Conjunction((A, B)))
+    res = Skolemize().walk(exp)
+    assert res == exp
+
+
+def test_skolemize_simple_expression():
+    P = Symbol("P")
+    X = Symbol("X")
+    exp = ExistentialPredicate(X, P(X))
+    sk = Skolemize()
+    res = sk.walk(exp)
+    c0 = sk.used_symbols[0]
+    assert res == P(c0)
+
+
+def test_skolemize_universally_quantified():
+    X = Symbol("X")
+    Y = Symbol("Y")
+    exp = UniversalPredicate(Y, ExistentialPredicate(X, Disjunction((Y, X))))
+    sk = Skolemize()
+    res = sk.walk(exp)
+    c0 = sk.used_symbols[0]
+    assert res == UniversalPredicate(Y, Disjunction((Y, c0(Y))))
+
+
+def test_skolemize_nested_universals():
+    X = Symbol("X")
+    Y = Symbol("Y")
+    Z = Symbol("Z")
+    exp = UniversalPredicate(
+        Z,
+        UniversalPredicate(Y, ExistentialPredicate(X, Disjunction((Z, Y, X)))),
+    )
+    sk = Skolemize()
+    res = sk.walk(exp)
+    c0 = sk.used_symbols[0]
+    assert res == UniversalPredicate(
+        Z, UniversalPredicate(Y, Disjunction((Z, Y, c0(Z, Y)))),
+    )
+
+
+def test_skolemize_multiple_existentials():
+    X = Symbol("X")
+    Y = Symbol("Y")
+    Z = Symbol("Z")
+    exp = ExistentialPredicate(
+        X,
+        UniversalPredicate(Y, ExistentialPredicate(Z, Conjunction((X, Y, Z)))),
+    )
+    sk = Skolemize()
+    res = sk.walk(exp)
+    c0, c1 = sk.used_symbols
+    assert res == UniversalPredicate(Y, Conjunction((c0, Y, c1(Y))))
+
+
+def test_skolemize_repeated_symbols():
+    X = Symbol("X")
+    Y = Symbol("Y")
+    P = Symbol("P")
+    exp = UniversalPredicate(
+        X, Disjunction((P(X), ExistentialPredicate(X, P(X))))
+    )
+    sk = Skolemize()
+    res = sk.walk(exp)
+    c0 = sk.used_symbols[0]
+    assert res == UniversalPredicate(X, Disjunction((P(X), P(c0(X)))))
