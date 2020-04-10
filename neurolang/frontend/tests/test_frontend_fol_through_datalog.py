96df645c67ad57cfdff7e0156d18d4150d646bd7
Nikita Zdanovitch <nikita.zdanovitch@inria.fr>
2020-04-10 11:25:23 +0200

    The region frontend and cloned/patched tests for it.

--- neurolang/frontend/tests/test_frontend_fol_through_datalog.py
+++ neurolang/frontend/tests/test_frontend_fol_through_datalog.py
@@ -1,25 +1,46 @@
 from unittest.mock import patch
-
-from . import test_frontend
 from .. import RegionFrontendFolThroughDatalog
+from . import test_frontend as tf
 
 
-current_module = __import__(__name__)
+patch_frontend = patch(
+    "neurolang.frontend.RegionFrontend", new=RegionFrontendFolThroughDatalog
+)
 
+
+@patch_frontend
+def test_tuple_symbol_multiple_types_query():
+    tf.test_tuple_symbol_multiple_types_query()
+
+
+@patch_frontend
+def test_quantifier_expressions():
+    tf.test_quantifier_expressions()
+
+
+#
+# current_module = __import__(__name__)
+#
 # Programmatically copy the tests from test_frontend using
 # RegionFrontendFolThroughDatalog instead of RegionFrontend
-for name in dir(test_frontend):
-    if not name.startswith("test_"):
-        continue
-    test = getattr(test_frontend, name)
-    if not callable(test):
-        continue
-
-    def test_wrapper():
-        with patch(
-            "neurolang.frontend.RegionFrontend",
-            new=RegionFrontendFolThroughDatalog,
-        ):
-            test()
-
-    setattr(current_module, name, test_wrapper)
+# def borrow_tests():
+#     from . import test_frontend
+#
+#     for name in dir(test_frontend):
+#         if not name.startswith("test_"):
+#             continue
+#         test = getattr(test_frontend, name)
+#         if not callable(test):
+#             continue
+#
+#         def test_wrapper():
+#             with patch(
+#                 "neurolang.frontend.RegionFrontend",
+#                 new=RegionFrontendFolThroughDatalog,
+#             ):
+#                 test()
+#
+#         setattr(current_module, name, test_wrapper)
+#
+# borrow_tests()
+#
