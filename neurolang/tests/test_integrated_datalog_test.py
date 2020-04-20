5fc265664fcd2ebacc5bcd228c1e47fb2b4a2aea
Demian Wassermann <demian.wassermann@inria.fr>
2020-04-20 20:18:26 +0200

    Add inital test prototype

---
+++ neurolang/tests/test_integrated_datalog_test.py
@@ -0,0 +1,39 @@
+from ..datalog import DatalogProgram
+from ..datalog.aggregation import (AggregationApplication,
+                                   DatalogWithAggregationMixin)
+from ..datalog.expressions import Fact
+from ..datalog.negation import DatalogProgramNegationMixin
+from ..expressions import Constant, Symbol
+from ..logic import Conjunction, Implication, Negation, Union
+from ..probabilistic.cplogic.program import CPLogicProgram
+from ..probabilistic.expressions import ProbabilisticPredicate
+from ..type_system import Unknown
+
+
+def test_all_composed():
+    class Datalog(
+        CPLogicProgram,
+        DatalogWithAggregationMixin,
+        DatalogProgramNegationMixin,
+        DatalogProgram
+    ):
+        def function_length(self, x: Unknown) -> int:
+            return len(x)
+
+    A = Symbol('A')
+    B = Symbol('B')
+    C = Symbol('C')
+    D = Symbol('D')
+    x = Symbol('x')
+
+    program = Union((
+        Fact(A(Constant(1))),
+        Fact(A(Constant(2))),
+        Fact(C(Constant(3))),
+        Fact(ProbabilisticPredicate(Constant(.5), B(Constant(2)))),
+        Implication(AggregationApplication(Symbol('length'), (x,)), A(x)),
+        Implication(D(x), Conjunction((A(x), Negation(D(x))))),
+    ))
+
+    dl = Datalog()
+    dl.walk(program)
