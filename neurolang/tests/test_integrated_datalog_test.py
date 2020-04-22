a9cc8f740b5737da558aa378d3680d0702777501
Demian Wassermann <demian.wassermann@inria.fr>
2020-04-22 08:34:46 +0200

    Add constraints to the tests

--- neurolang/tests/test_integrated_datalog_test.py
+++ neurolang/tests/test_integrated_datalog_test.py
@@ -3,6 +3,7 @@ from ..datalog.aggregation import (AggregationApplication,
                                    DatalogWithAggregationMixin)
 from ..datalog.expressions import Fact
 from ..datalog.negation import DatalogProgramNegationMixin
+from ..datalog.constraints_representation import RightImplication, DatalogConstraintsProgramMixin
 from ..expressions import Constant, Symbol
 from ..logic import Conjunction, Implication, Negation, Union
 from ..probabilistic.cplogic.program import CPLogicProgram
@@ -10,39 +11,42 @@ from ..probabilistic.expressions import ProbabilisticPredicate
 from ..type_system import Unknown
 
 
+A = Symbol('A')
+B = Symbol('B')
+C = Symbol('C')
+D = Symbol('D')
+E = Symbol('E')
+F = Symbol('F')
+
+
 def test_all_composed():
     class Datalog(
         CPLogicProgram,
         DatalogWithAggregationMixin,
         DatalogProgramNegationMixin,
+        DatalogConstraintsProgramMixin,
         DatalogProgram
     ):
         def function_length(self, x: Unknown) -> int:
             return len(x)
 
-    A = Symbol('A')
-    B = Symbol('B')
-    C = Symbol('C')
-    D = Symbol('D')
-    E = Symbol('E')
     x = Symbol('x')
-
     program = Union((
         Fact(A(Constant(1))),
         Fact(A(Constant(2))),
+        Fact(A(Constant(3))),
         Fact(C(Constant(3))),
         Fact(ProbabilisticPredicate(Constant(.5), B(Constant(2)))),
         Implication(D(AggregationApplication(Symbol('length'), (x,))), A(x)),
         Implication(E(x), Conjunction((A(x), Negation(D(x))))),
+        Implication(F(x), A(x)),
+        RightImplication(C(x), F(x))
     ))
 
     dl = Datalog()
     dl.walk(program)
 
-    assert len(dl.extensional_database()) == 2
-    assert len(dl.intensional_database()) == 2
-    assert len(dl.probabilistic_facts()) == 1
-
-    assert A in dl.extensional_database() and B in dl.extensional_database()
-    assert D in dl.intensional_database() and E in dl.intensional_database()
-    assert B in dl.probabilistic_facts()
+    assert {A, B, C} == set(dl.extensional_database())
+    assert {D, E} == set(dl.intensional_database())
+    assert {B} == set(dl.probabilistic_facts())
+    assert {F} == set(dl.constraints())
