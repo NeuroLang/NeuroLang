eec69e52d0aa8f7e1f3a51c36169447ef24e2e0f
Demian Wassermann <demian.wassermann@inria.fr>
2020-05-02 17:06:42 +0200

    Initial commit

---
+++ neurolang/utils/relational_algebra_set/__init__.py
@@ -0,0 +1,14 @@
+from .pandas import (
+    RelationalAlgebraStringExpression,
+    RelationalAlgebraFrozenSet,
+    RelationalAlgebraSet,
+    NamedRelationalAlgebraFrozenSet
+)
+
+
+__all__ = [
+    "RelationalAlgebraStringExpression",
+    "RelationalAlgebraFrozenSet",
+    "RelationalAlgebraSet",
+    "NamedRelationalAlgebraFrozenSet"
+]
