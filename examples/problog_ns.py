dd0672249102998b02f0987e0b96d2337fca59f6
Valentin Iovene <valentin@too.gy>
2020-06-23 01:10:22 +0200

    working small example

--- examples/problog_ns.py
+++ examples/problog_ns.py
@@ -1,4 +1,5 @@
 from typing import Iterable
+from uuid import uuid1
 
 import nibabel as nib
 import numpy as np
@@ -14,8 +15,12 @@ from neurolang.datalog.expression_processing import (
 )
 from neurolang.exceptions import NeuroLangFrontendException
 from neurolang.expression_walker import ExpressionBasicEvaluator
+from neurolang.expressions import Symbol
 from neurolang.frontend import QueryBuilderDatalog, RegionFrontendDatalogSolver
 from neurolang.frontend.neurosynth_utils import NeuroSynthHandler
+from neurolang.frontend.query_resolution_expressions import (
+    Symbol as FrontEndSymbol,
+)
 from neurolang.logic import Implication, Union
 from neurolang.probabilistic.cplogic import solve_succ_all
 from neurolang.probabilistic.cplogic.program import (
@@ -54,21 +59,36 @@ class ProbabilisticFrontend(QueryBuilderDatalog):
         return deterministic_solution
 
     def add_uniform_probabilistic_choice_over_set(self, iterable, name=None):
-        if isinstance(iterable, pd.DataFrame)
+        if name is None:
+            name = str(uuid1())
+        probability = 1 / len(iterable)
+        if isinstance(iterable, pd.DataFrame):
+            columns = iterable.columns
+            prob_col = Symbol.fresh().name
+            new_columns = (prob_col,) + tuple(columns)
+            iterable = iterable.copy()
+            iterable[prob_col] = probability
+            iterable = iterable[new_columns]
+        else:
+            iterable = [(probability,) + tuple(tupl) for tupl in iterable]
+        self.solver.add_probabilistic_choice_from_tuples(
+            Symbol(name), iterable
+        )
+        return FrontEndSymbol(self, name)
 
     def _make_probabilistic_program_from_deterministic_solution(
         self, deterministic_solution
     ):
         cpl = CPLogicProgram()
         for pred_symb, ra_set in deterministic_solution.items():
-            cpl.add_extensional_predicate_from_tuples(pred_symb, ra_set)
+            cpl.add_extensional_predicate_from_tuples(pred_symb, ra_set.value)
         for pred_symb in self.solver.pfact_pred_symbs:
             cpl.add_probabilistic_facts_from_tuples(
-                pred_symb, self.solver.symbol_table[pred_symb]
+                pred_symb, self.solver.symbol_table[pred_symb].value
             )
         for pred_symb in self.solver.pchoice_pred_symbs:
             cpl.add_probabilistic_choice_from_tuples(
-                pred_symb, self.solver.symbol_table[pred_symb]
+                pred_symb, self.solver.symbol_table[pred_symb].value
             )
         return cpl
 
@@ -80,20 +100,20 @@ class ProbabilisticFrontend(QueryBuilderDatalog):
         if prob_symbols is None:
             prob_symbols = set()
         if query_pred is None:
-            query_reachable_code = self.union_of_idb()
+            query_reachable_code = self._union_of_idb()
         else:
             query_reachable_code = reachable_code(query_pred, self.solver)
-        constraints_symbols = set(
-            ri.consequent.functor for ri in self.solver.constraints().formulas
-        )
         deterministic_symbols = (
             set(self.solver.extensional_database().keys())
             | set(det_symbols)
-            | constraints_symbols
-            | self.solver.builtins()
+            | set(self.solver.builtins().keys())
         )
         deterministic_program = list()
-        probabilistic_symbols = set() | set(prob_symbols)
+        probabilistic_symbols = (
+            self.solver.pfact_pred_symbs
+            | self.solver.pchoice_pred_symbs
+            | set(prob_symbols)
+        )
         probabilistic_program = list()
         unclassified_code = list(query_reachable_code.formulas)
         unclassified = 0
@@ -139,16 +159,31 @@ class ProbabilisticFrontend(QueryBuilderDatalog):
 nl = ProbabilisticFrontend()
 
 # loading neurosynth data into the program
-nsh = NeuroSynthHandler()
+# nsh = NeuroSynthHandler()
+# ns_term_in_study = nl.add_tuple_set(
+# nsh.ns_load_term_study_associations(threshold=1e-3),
+# name="ns_term_in_study",
+# )
+# ns_activation = nl.add_tuple_set(
+# nsh.ns_load_reported_activations(), name="ns_activation"
+# )
+# selected_study = nl.add_uniform_probabilistic_choice_over_set(
+# nsh.ns_load_all_study_ids(), name="selected_study"
+# )
 ns_term_in_study = nl.add_tuple_set(
-    nsh.ns_load_term_study_associations(threshold=1e-3),
+    {
+        ("59895", "insula"),
+        ("34984", "brain"),
+        ("34984", "auditory"),
+        ("59895", "brain"),
+    },
     name="ns_term_in_study",
 )
 ns_activation = nl.add_tuple_set(
-    nsh.ns_load_reported_activations(), name="ns_activation"
+    {("59895", 455), ("59895", 222), ("34984", 455),}, name="ns_activation"
 )
 selected_study = nl.add_uniform_probabilistic_choice_over_set(
-    nsh.ns_load_all_study_ids(), name="selected_study"
+    {("59895",), ("34984",)}, name="selected_study"
 )
 with nl.scope as e:
     e.term_association[e.term] = (
@@ -158,3 +193,5 @@ with nl.scope as e:
         ns_activation[e.study_id, e.voxel_id] & selected_study[e.study_id]
     )
     res = nl.solve_all()
+
+    print(res)
