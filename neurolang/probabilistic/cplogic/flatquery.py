3169e4466cbcf3be7cd8161663be800b9e60a25b
Valentin Iovene <valentin@too.gy>
2020-06-26 14:35:03 +0200

    probchoices=

--- neurolang/probabilistic/cplogic/flatquery.py
+++ neurolang/probabilistic/cplogic/flatquery.py
@@ -1,4 +1,7 @@
-from ...datalog.expression_processing import flatten_query, enforce_conjunction
+import collections
+import operator
+
+from ...datalog.expression_processing import enforce_conjunction, flatten_query
 from ...exceptions import ForbiddenDisjunctionError, ForbiddenExpressionError
 from ...expressions import Constant, FunctionApplication, Symbol
 from ...logic import Conjunction, Implication, Union
@@ -8,6 +11,8 @@ from ...relational_algebra import (
     Projection,
     RelationalAlgebraSolver,
     RenameColumns,
+    RenameColumn,
+    Selection,
     str2columnstr_constant,
 )
 from ...relational_algebra_provenance import (
@@ -21,6 +26,38 @@ from .grounding import (
     get_grounding_predicate,
     ground_cplogic_program,
 )
+
+EQUAL = Constant(operator.eq)
+
+
+def rename_columns_for_args_to_match(relation, src_args, dst_args):
+    """
+    Rename the columns of a relation so that they match the targeted args.
+
+    Parameters
+    ----------
+    relation : ProvenanceAlgebraSet or RelationalAlgebraOperation
+        The relation on which the renaming of the columns should happen.
+    src_args : tuple of Symbols
+        The predicate's arguments currently matching the columns.
+    dst_args : tuple of Symbols
+        New args that the naming of the columns should match.
+
+    Returns
+    -------
+    RelationalAlgebraOperation
+        The unsolved nested operations that apply the renaming scheme.
+
+    """
+    src_cols = list(str2columnstr_constant(arg.name) for arg in src_args)
+    dst_cols = list(str2columnstr_constant(arg.name) for arg in dst_args)
+    result = relation
+    for dst_col in set(dst_cols):
+        idxs = [i for i, c in enumerate(dst_cols) if c == dst_col]
+        result = RenameColumn(result, src_cols[idxs[0]], dst_col)
+        for idx in idxs[1:]:
+            result = Selection(result, EQUAL(src_cols[idx], dst_col))
+    return result
 
 
 def grounding_to_provset(grounding, dst_pred):
@@ -40,15 +77,29 @@ def grounding_to_provset(grounding, dst_pred):
         relation = ConcatenateConstantColumn(
             relation, prov_col, cst_one_probability
         )
-    renames = tuple(
-        (str2columnstr_constant(src.name), str2columnstr_constant(dst.name))
-        for src, dst in zip(src_pred.args, dst_pred.args)
+    relation = rename_columns_for_args_to_match(
+        relation, src_pred.args, dst_pred.args
     )
-    relation = RenameColumns(relation, renames)
     solver = RelationalAlgebraSolver()
     relation = solver.walk(relation)
     prov_set = ProvenanceAlgebraSet(relation.value, prov_col)
     return prov_set
+
+
+def get_probchoice_variable_equalities(conj_query, pchoice_pred_symbs):
+    grouped_pchoice_preds = collections.defaultdict(list)
+    for pred in conj_query.formulas:
+        if pred.functor in pchoice_pred_symbs:
+            grouped_pchoice_preds[pred.functor].append(pred)
+    eq_set = set()
+    for predicates in grouped_pchoice_preds.values():
+        arity = len(predicates[0].args)
+        for var_idx in range(arity):
+            for pred_idx in range(1, len(predicates)):
+                x = predicates[pred_idx - 1].args[var_idx]
+                y = predicates[pred_idx].args[var_idx]
+                eq_set.add((x, y))
+    return eq_set
 
 
 def solve_succ_query(query, cpl):
@@ -70,5 +121,14 @@ def solve_succ_query(query, cpl):
         for arg in set.union(*[set(pred.args) for pred in query.formulas])
     )
     relation = Projection(relation, proj_cols)
+    for x, y in get_probchoice_variable_equalities(
+        conj_query, cpl.pchoice_pred_symbs
+    ):
+        relation = Selection(
+            relation,
+            EQUAL(
+                str2columnstr_constant(x.name), str2columnstr_constant(y.name)
+            ),
+        )
     solver = RelationalAlgebraProvenanceCountingSolver()
     return solver.walk(relation)
