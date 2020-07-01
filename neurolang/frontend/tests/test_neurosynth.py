39c78a39aef4776f98117e9d52ff6f2165676f2e
Valentin Iovene <valentin@too.gy>
2020-07-01 13:26:12 +0200

    add test

--- neurolang/frontend/tests/test_neurosynth.py
+++ neurolang/frontend/tests/test_neurosynth.py
@@ -13,5 +13,22 @@ def test_load_study_ids(monkeypatch):
     monkeypatch.setattr(NeuroSynthHandler, "ns_study_ids", mock_study_ids)
     frontend = NeurolangDL()
     symbol = frontend.load_neurosynth_study_ids()
-    expected = {(StudyID(1),), (StudyID(2),)}
-    assert frontend[symbol.symbol_name].value == expected
+    assert frontend[symbol.symbol_name].value == mock_study_ids()
+
+
+def mock_reported_activations(*args, **kwargs):
+    return {
+        (StudyID(1), 42),
+        (StudyID(1), 21),
+        (StudyID(2), 42),
+        (StudyID(3), 21),
+    }
+
+
+def test_reported_activations(monkeypatch):
+    monkeypatch.setattr(
+        NeuroSynthHandler, "ns_reported_activations", mock_reported_activations
+    )
+    frontend = NeurolangDL()
+    symbol = frontend.load_neurosynth_reported_activations()
+    assert frontend[symbol.symbol_name].value == mock_reported_activations()
