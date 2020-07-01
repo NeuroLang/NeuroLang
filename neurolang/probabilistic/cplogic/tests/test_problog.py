a957dcc07f4b5c5e946db5e06957a42c831a1c36
Valentin Iovene <valentin@too.gy>
2020-07-01 13:45:13 +0200

    further testing

--- neurolang/probabilistic/cplogic/tests/test_problog.py
+++ neurolang/probabilistic/cplogic/tests/test_problog.py
@@ -4,10 +4,12 @@ import problog.sdd_formula
 
 from ....datalog.expressions import Fact
 from ....expressions import Constant, Symbol
+from ....logic import Implication
 from ..problog import cplogic_to_problog
 from ..program import CPLogicProgram
 
 P = Symbol("P")
+a = Constant("a")
 
 
 def test_convert_cpl_to_pl():
@@ -31,3 +33,13 @@ def test_zero_arity():
     cpl_program = CPLogicProgram()
     cpl_program.walk(Fact(P()))
     cplogic_to_problog(cpl_program)
+    cpl_program = CPLogicProgram()
+    cpl_program.walk(Fact(P(a)))
+    cpl_program.walk(Implication(Symbol("yes")(), P(a)))
+    pl = cplogic_to_problog(cpl_program)
+    query = problog.logic.Term("query")
+    query_pred = problog.logic.Term("yes")
+    pl += query(query_pred)
+    res = problog.core.ProbLog.convert(pl, problog.sdd_formula.SDD).evaluate()
+    expected = {problog.logic.Term("yes"): 1.0}
+    assert res == expected
