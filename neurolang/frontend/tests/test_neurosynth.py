44f91a3d5b9b87ebbc242b825129ad9fbb2c1702
Valentin Iovene <valentin@too.gy>
2020-07-01 11:51:01 +0200

    update test

--- neurolang/frontend/tests/test_neurosynth.py
+++ neurolang/frontend/tests/test_neurosynth.py
@@ -13,5 +13,7 @@ def test_load_study_ids(monkeypatch):
     monkeypatch.setattr(NeuroSynthHandler, "ns_study_ids", mock_study_ids)
     frontend = NeurolangDL()
     symbol = frontend.load_neurosynth_study_ids()
-    study_ids = frontend.symbol_table[symbol].value.to_numpy()
-    assert study_ids == np.array([[1, 2, 3, 4]])
+    np.testing.assert_allclose(
+        frontend[symbol.symbol_name].value.as_numpy_array(),
+        np.array([[1, 2, 3, 4]]),
+    )
