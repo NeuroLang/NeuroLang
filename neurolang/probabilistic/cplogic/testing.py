65dd5089a169a418685015e3d95cbed533fbcdae
Valentin Iovene <valentin@too.gy>
2020-07-17 16:26:29 +0200

    imports

--- neurolang/probabilistic/cplogic/testing.py
+++ neurolang/probabilistic/cplogic/testing.py
@@ -50,9 +50,7 @@ def get_named_relation_tuples(relation):
 def eq_prov_relations(pas1, pas2):
     assert isinstance(pas1, ProvenanceAlgebraSet)
     assert isinstance(pas2, ProvenanceAlgebraSet)
-    assert (
-        pas1.value.projection(*pas1.non_provenance_columns)
-    ) == (
+    assert (pas1.value.projection(*pas1.non_provenance_columns)) == (
         pas2.value.projection(*pas2.non_provenance_columns)
     )
     # ensure the prov col names are different so we can join the sets
