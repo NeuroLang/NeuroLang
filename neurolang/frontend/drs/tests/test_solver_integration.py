27ce723e2ac332a53c30bdea59b9df6803c31dfe
Nikita Zdanovitch <nikita.zdanovitch@inria.fr>
2020-07-28 12:02:39 +0200

    Drs parser (#339)
    * First draft.
    
    * Move parser and base grammar classes to another module.
    
    * Pattern matching over trees.
    
    * Reorganized DRS related code, added DRS builder.
    
    * Add indefinite clauses to english grammar.
    
    * DRS parsing for indefinite noun phrase.
    
    * Add nominative pronouns to english grammar.
    
    * Refactor chart parser to use grammar lexicon and only one lexicon insertion rule. Improve unification upstream and use unified constituents when building tree. Still this fails trying to unify downstream through two or more levels.
    
    * Unify variables down when parsing the tree. Skipped the test.
    
    This seems make no difference because I'm using the unification in the
    wrong direction. Skipped the test because is not a priority now but will
    need to fix later.
    
    * Apposition with variables.
    
    * Reference to variable as noun.
    
    * Test references to variables in drs builder.
    
    * Reference variable nouns in drs builder.
    
    * Add a custom type for the chart for debugging purposes.
    
    * Conditional sentences with "if" and "if then".
    
    * DRS builder cases for conditionals and translation into FOL.
    
    * Changed the proper name rule to hold a constant value. Also added the variable restriction for `Constant = Var` cases in the fol2dl translation.
    
    * Module with an entry point to translate a string in natural language to a datalog program. Simple test example.
    
    * Split a function into a class with methods.
    
    * Split some big methods, use exceptions showing an explicit reason instead of returning None.
    
    * Move some argument variables to the class in the chart parser.
    
    * Split _complete from chart parser.
    
    * Separate even more.
    
    * Inverse wrong condition.
    
    * More informative name, the complexity might be higher than the threshold but is the method is really concrete.

---
+++ neurolang/frontend/drs/tests/test_solver_integration.py
@@ -0,0 +1,95 @@
+from ....expressions import (
+    Symbol,
+    Constant,
+    ExpressionBlock,
+)
+from ....expression_walker import ExpressionBasicEvaluator
+from ....logic import Implication, Conjunction
+from ....datalog.basic_representation import DatalogProgram
+from ....datalog.expressions import TranslateToLogic
+from ....datalog.chase import (
+    ChaseGeneral,
+    ChaseMGUMixin,
+    ChaseNaive,
+)
+from ..translate_to_dl import TranslateToDatalog
+
+
+intersects = Symbol("intersects")
+region = Symbol("region")
+book = Symbol("book")
+likes = Symbol("likes")
+references = Symbol("references")
+x = Symbol("X")
+y = Symbol("Y")
+Jones = Constant("Jones")
+Ulysses = Constant("Ulysses")
+Odyssey = Constant("Odyssey")
+
+
+def test_translate_to_dl():
+    ttdl = TranslateToDatalog()
+    program = ttdl.translate_sentence("if Y intersects X then X intersects Y")
+
+    assert program == ExpressionBlock(
+        (Implication(intersects(x, y), intersects(y, x)),)
+    )
+
+
+def test_translate_to_dl_2():
+    ttdl = TranslateToDatalog()
+    program = ttdl.translate_sentence(
+        "if a region Y intersects a region X then X intersects Y"
+    )
+
+    expected = ExpressionBlock(
+        (
+            Implication(
+                intersects(x, y),
+                Conjunction((intersects(y, x), region(x), region(y),)),
+            ),
+        )
+    )
+    assert program == expected
+
+
+class Datalog(TranslateToLogic, DatalogProgram, ExpressionBasicEvaluator):
+    pass
+
+
+class Chase(ChaseNaive, ChaseMGUMixin, ChaseGeneral):
+    pass
+
+
+def test_solver_integration():
+    """
+    Given an extensional base having the predicates:
+      `likes(x, y), book(x), references(x, y)`
+
+    And the facts:
+      `book(Odyssey), book(Ulysses)`
+
+    Adding the next sentences to the base:
+      `Jones likes a book if it references Odyssey.`
+      `Ulysses references Odyssey.`
+
+    Should answer:
+      `Does Jones like Ulysses`
+    """
+
+    ttdl = TranslateToDatalog()
+    program = ttdl.translate_block(
+        """
+        if a book X references Odyssey then Jones likes X.
+        Ulysses references Odyssey.
+        """
+    )
+
+    dl = Datalog()
+    dl.add_extensional_predicate_from_tuples(book, {(Odyssey,), (Ulysses,)})
+    dl.walk(program)
+
+    dc = Chase(dl)
+    solution = dc.build_chase_solution()
+
+    assert (Jones, Ulysses) in solution[likes].value
