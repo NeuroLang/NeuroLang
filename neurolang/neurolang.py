d9c801b8a4deca8738b3ad660ea169d8ef894da9
Demian Wassermann <demian.wassermann@inria.fr>
2020-10-09 13:54:20 +0200

    fix

--- neurolang/neurolang.py
+++ neurolang/neurolang.py
@@ -1,5 +1,9 @@
 from __future__ import absolute_import, division, print_function
-from importlib.metadata import version, PackageNotFoundError
+
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    from importlib_metadata import version, PackageNotFoundError
 
 from .exceptions import NeuroLangException
 from .expression_pattern_matching import *
