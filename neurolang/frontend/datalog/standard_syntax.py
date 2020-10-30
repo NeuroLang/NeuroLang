728228bd3c9d390058ec45cb6098d3dc78644cca
Gastón Zanitti <gzanitti@gmail.com>
2020-10-30 12:34:33 +0100

    Fix regex used to parse URIs (#531)
    * fix regex uri
    
    * Uri test cases

--- neurolang/frontend/datalog/standard_syntax.py
+++ neurolang/frontend/datalog/standard_syntax.py
@@ -70,7 +70,7 @@ GRAMMAR = u"""
             | ext_identifier ;
 
     identifier = /[a-zA-Z_][a-zA-Z0-9_]*/
-               | '`'@:?"[0-9a-zA-Z/#%:-]+"'`';
+               | '`'@:?"[0-9a-zA-Z/#%._:-]+"'`';
 
     comparison_operator = '==' | '<' | '<=' | '>=' | '>' | '!=' ;
 
