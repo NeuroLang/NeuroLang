96a9d426be536fbdb572df58cdcb9700f37ba287
Valentin Iovene <valentin@too.gy>
2020-11-04 16:27:58 +0100

    Add support for variable equalities and constants in UCQs (#537)
    * wip
    
    * fix test
    
    * doc
    
    * fix tests
    
    * tests + fix
    
    * wip
    
    * all tests passing
    
    * imports / style
    
    * add failing test non-unifiable
    
    * handle FALSE cases in query flattening
    
    * style+refactor
    
    * add tests for rule transformations
    
    * doc
    
    * add tests for maybe_deconjunct
    
    * add tests for probsemiring extended projections
    
    * FreshenFreeVariables walker
    
    * better use pattern matching system
    
    * fix tests
    
    * add test and fix
    
    * much more elegant solution thanks to @demianw
    
    * use already-existing extended proj
    
    * remove TODO message
    
    * isort+black
    
    * fix line too long by using self.
    
    * undo breaking change
    
    * add test for unhandled case
    
    * further testing
    
    * remove .freshened extra security
    
    * attempt to reduce complexity
    
    * fix style
    
    * re-enable now passing test
    
    * handle edge cases using stack
    
    * reset seen_counts dict as suggested by @demianw
    
    * reset seen_counts dict as suggested by @demianw
    
    * reset seen_counts dict everytime an equality was effectively processed
    
    * reset seen_counts dict everytime an equality was effectively processed
    
    * add not-ordered equalities test
    
    * better example
    
    * use pytest.skip() in tests for which the wmc solver is not yet working
    
    * fix codacy
    
    * test commit
    
    * remove file
    
    * refactoring
    
    * fix imports

--- neurolang/probabilistic/dichotomy_theorem_based_solver.py
+++ neurolang/probabilistic/dichotomy_theorem_based_solver.py
@@ -20,13 +20,14 @@ probabilistic databases. VLDB J., 16(4):523–544, 2007.
 '''
 
 import logging
+import typing
 from collections import defaultdict
 
 from ..datalog.expression_processing import flatten_query
 from ..datalog.translate_to_named_ra import TranslateToNamedRA
 from ..expression_walker import ExpressionWalker, add_match
 from ..expressions import Constant, Symbol
-from ..logic import Conjunction, Implication
+from ..logic import Conjunction, Implication, FALSE
 from ..logic.expression_processing import extract_logic_predicates
 from ..utils.orderedset import OrderedSet
 from ..relational_algebra import (
@@ -40,10 +41,12 @@ from ..relational_algebra import (
     RelationalAlgebraPushInSelections,
     RelationalAlgebraStringExpression,
     str2columnstr_constant,
+    NamedRelationalAlgebraFrozenSet,
 )
 from ..relational_algebra_provenance import (
     ProvenanceAlgebraSet,
     RelationalAlgebraProvenanceExpressionSemringSolver,
+    RelationalAlgebraProvenanceCountingSolver,
 )
 from ..utils import log_performance
 from .exceptions import NotHierarchicalQueryException
@@ -211,6 +214,52 @@ class ProbSemiringSolver(RelationalAlgebraProvenanceExpressionSemringSolver):
     def probabilistic_fact_set_invalid(self, prob_fact_set):
         raise NotImplementedError()
 
+    @add_match(ExtendedProjection(ProvenanceAlgebraSet, ...))
+    def extended_projection(self, proj_op):
+        provset = self.walk(proj_op.relation)
+        self._check_prov_col_not_in_proj_list(provset, proj_op.projection_list)
+        self._check_all_non_prov_cols_in_proj_list(
+            provset, proj_op.projection_list
+        )
+        relation = Constant[typing.AbstractSet](provset.relations)
+        prov_col = str2columnstr_constant(provset.provenance_column)
+        new_prov_col = str2columnstr_constant(Symbol.fresh().name)
+        proj_list_with_prov_col = proj_op.projection_list + (
+            ExtendedProjectionListMember(prov_col, new_prov_col),
+        )
+        ra_op = ExtendedProjection(relation, proj_list_with_prov_col)
+        new_relation = self.walk(ra_op)
+        new_provset = ProvenanceAlgebraSet(
+            new_relation.value, new_prov_col.value
+        )
+        return new_provset
+
+    @staticmethod
+    def _check_prov_col_not_in_proj_list(provset, proj_list):
+        if any(
+            member.dst_column.value == provset.provenance_column
+            for member in proj_list
+        ):
+            raise ValueError(
+                "Cannot project on provenance column: "
+                f"{provset.provenance_column}"
+            )
+
+    @staticmethod
+    def _check_all_non_prov_cols_in_proj_list(provset, proj_list):
+        non_prov_cols = set(provset.non_provenance_columns)
+        found_cols = set(
+            member.dst_column.value
+            for member in proj_list
+            if member.dst_column.value in non_prov_cols
+            and member.fun_exp == member.dst_column
+        )
+        if non_prov_cols.symmetric_difference(found_cols):
+            raise ValueError(
+                "All non-provenance columns must be part of the extended "
+                "projection as {c: c} projection list member."
+            )
+
 
 class RAQueryOptimiser(
     EliminateTrivialProjections,
@@ -242,6 +291,15 @@ def solve_succ_query(query, cpl_program):
         LOG, "Preparing query %s", init_args=(query.consequent.functor.name,),
     ):
         flat_query_body = flatten_query(query.antecedent, cpl_program)
+
+    if flat_query_body == FALSE or (
+        isinstance(flat_query_body, Conjunction)
+        and any(conjunct == FALSE for conjunct in flat_query_body.formulas)
+    ):
+        return ProvenanceAlgebraSet(
+            NamedRelationalAlgebraFrozenSet(("_p_",)),
+            ColumnStr("_p_"),
+        )
 
     with log_performance(LOG, "Translation and lifted optimisation"):
         flat_query_body = lift_optimization_for_choice_predicates(
@@ -280,11 +338,11 @@ def solve_succ_query(query, cpl_program):
             )
 
         ra_query = TranslateToNamedRA().walk(shattered_query.antecedent)
+        # finally project on the initial query's head variables
         proj_cols = tuple(
             OrderedSet(
                 str2columnstr_constant(arg.name)
-                for arg in shattered_query.consequent.args
-                if isinstance(arg, Symbol)
+                for arg in query.consequent.args
             )
         )
         ra_query = Projection(ra_query, proj_cols)
@@ -293,8 +351,5 @@ def solve_succ_query(query, cpl_program):
     with log_performance(LOG, "Run RAP query"):
         solver = ProbSemiringSolver(symbol_table)
         prob_set_result = solver.walk(ra_query)
-        prob_set_result = project_on_query_head(
-            shattered_query, prob_set_result
-        )
 
     return prob_set_result
