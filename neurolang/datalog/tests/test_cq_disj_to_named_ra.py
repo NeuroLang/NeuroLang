93ec81965d224480d9174f42ea25786adce06df1
Valentin Iovene <valentin@too.gy>
2020-11-12 14:39:51 +0100

    add tests

--- neurolang/datalog/tests/test_cq_disj_to_named_ra.py
+++ neurolang/datalog/tests/test_cq_disj_to_named_ra.py
@@ -713,3 +713,39 @@ def test_not_ordered_equalites():
         ),
     )
     assert result == expected
+
+
+def test_equality_builtin_application():
+    x = Symbol("x")
+    y = Symbol("y")
+    z = Symbol("z")
+    P = Symbol("P")
+    f = Constant(lambda x: x ** 2)
+    conjunction = Conjunction((P(x), P(y), EQ(z, f(x, y))))
+    result = TranslateToNamedRA().walk(conjunction)
+    expected = ExtendedProjection(
+        NaturalJoin(
+            NameColumns(
+                Projection(P, (C_(ColumnInt(0)),)),
+                (C_(ColumnStr("x")),),
+            ),
+            NameColumns(
+                Projection(P, (C_(ColumnInt(0)),)),
+                (C_(ColumnStr("y")),),
+            ),
+        ),
+        (
+            ExtendedProjectionListMember(
+                str2columnstr_constant("x"),
+                str2columnstr_constant("x"),
+            ),
+            ExtendedProjectionListMember(
+                str2columnstr_constant("y"),
+                str2columnstr_constant("y"),
+            ),
+            ExtendedProjectionListMember(
+                f(C_(ColumnStr("x")), C_(ColumnStr("y"))),
+                str2columnstr_constant("z"),
+            ),
+        ),
+    )
