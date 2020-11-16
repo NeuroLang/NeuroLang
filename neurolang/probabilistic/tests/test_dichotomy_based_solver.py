24c033f3bfb0e483ab94c8d7a7144ef1b074d299
Valentin Iovene <valentin@too.gy>
2020-11-16 10:41:01 +0100

    only allow EQ, add test to prevent other builtins

--- neurolang/probabilistic/tests/test_dichotomy_based_solver.py
+++ neurolang/probabilistic/tests/test_dichotomy_based_solver.py
@@ -1,5 +1,7 @@
+import typing
+
 from ...datalog.expression_processing import EQ
-from ...expressions import Symbol
+from ...expressions import Constant, Symbol
 from ...logic import Conjunction
 from ..dichotomy_theorem_based_solver import is_hierarchical_without_self_joins
 
@@ -36,3 +38,13 @@ def test_not_hierarchical_without_self_joins():
 def test_hierarchical_despite_multiple_equalities():
     q = Conjunction((Q(x), R(y), EQ(y, z), EQ(x, w)))
     assert is_hierarchical_without_self_joins(q)
+
+
+def test_non_hierarchical_builtin():
+    some_builtin = Constant[typing.Callable[[typing.Any, typing.Any], bool]](
+        "some_builtin",
+        verify_type=False,
+        auto_infer_type=False,
+    )
+    q = Conjunction((Q(x), R(y), some_builtin(y, z), some_builtin(x, w)))
+    assert not is_hierarchical_without_self_joins(q)
