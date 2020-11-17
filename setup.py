da817b11b889c9a814c9f7437947b3443513ac55
Demian Wassermann <demian.wassermann@inria.fr>
2020-11-17 16:23:01 +0100

    Fixed setup for distribution

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from setuptools import setup
+from setuptools import find_packages, setup
 
 # Workaround for editable install with pip
 # and versioneer
@@ -11,4 +11,4 @@ import versioneer
 if __name__ == "__main__":
     version = versioneer.get_version(),
     cmdclass = versioneer.get_cmdclass(),
-    setup(use_scm_version=True)
+    setup(use_scm_version=True, packages=find_packages())
