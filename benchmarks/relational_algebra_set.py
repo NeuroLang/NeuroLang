bac0149d90c0274c02ca707a2fb793f2020f7a3c
Jonas Renault <jonas.renault@inria.fr>
2021-01-08 16:17:44 +0100

    Added a testsuite for chain order on natural joins

--- benchmarks/relational_algebra_set.py
+++ benchmarks/relational_algebra_set.py
@@ -37,33 +37,114 @@ class TimeNamedRelationalAlgebraFrozenSets:
         distinct_r: int
             ratio of distinct elements in the set
         """
-        same_cols = [hex(x) for x in range(njoin_columns)]
-        df1 = pd.DataFrame(
+        join_columns = [hex(x) for x in range(njoin_columns)]
+        keys = pd.DataFrame(
             np.random.randint(0, N * distinct_r, size=(N, njoin_columns)),
-            columns=same_cols,
+            columns=join_columns,
         )
         self.sets = []
         for i in range(njoins):
             cols = [hex(ncols * i + x) for x in range(njoin_columns, ncols)]
-            df = pd.DataFrame(
-                np.random.randint(
-                    0, N * distinct_r, size=(N, ncols - njoin_columns)
-                ),
-                columns=cols,
+            df = pd.concat(
+                [
+                    keys,
+                    pd.DataFrame(
+                        np.random.randint(
+                            0, N * distinct_r, size=(N, ncols - njoin_columns)
+                        ),
+                        columns=cols,
+                    ),
+                ],
+                axis=1,
             )
             self.sets.append(
-                NamedRelationalAlgebraFrozenSet(
-                    same_cols + cols, pd.concat([df1, df], axis=1)
-                )
+                NamedRelationalAlgebraFrozenSet(join_columns + cols, df)
             )
-
-    def time_ra_naturaljoin(self, N, ncols, njoin_columns, njoins, distinct_r):
-        reduce(lambda a, b: a.naturaljoin(b), self.sets)
 
     def time_ra_left_naturaljoin(
         self, N, ncols, njoin_columns, njoins, distinct_r
     ):
         reduce(lambda a, b: a.left_naturaljoin(b), self.sets)
+
+
+class TimeChainedNaturalJoins:
+    params = [[10 ** 4, 10 ** 5, 10 ** 6], [10], [3], [8], [0.75]]
+
+    param_names = [
+        "rows",
+        "cols",
+        "number of join columns",
+        "number of chained joins",
+        "ratio of dictinct elements",
+    ]
+
+    timeout = 60 * 3
+
+    def setup(self, N, ncols, njoin_columns, njoins, distinct_r):
+        """
+        Generate NamedRelationalAlgebraFrozenSets to test performance impact
+        of chain order on natural joins. All sets share the same key columns,
+        but the first ones have a large portion of the keys repeated
+        multiple times, while the last ones have only a small subset of
+        distinct keys and are smaller in size.
+        Hence, chaining the joins in reverse order of the sets should be
+        much more efficient than chaining them in the direct order.
+
+        Parameters
+        ----------
+        N : int
+            number of elements per set
+        ncols : int
+            number of columns per set
+        njoin_columns : int
+            number of identical columns for all sets
+        njoins : int
+            number of joins to chain
+        distinct_r: int
+            ratio of distinct elements in the sets
+        """
+        join_columns = [hex(x) for x in range(njoin_columns)]
+        keys = pd.DataFrame(
+            np.random.randint(0, N * distinct_r, size=(N, njoin_columns)),
+            columns=join_columns,
+        )
+        self.sets = []
+        for i in range(njoins):
+            # Take a sample of the default keys.
+            skeys = keys.sample(frac=1 / (i + 1))
+            skeys = pd.DataFrame(
+                np.tile(skeys.to_numpy(), (njoins - i, 1)),
+                columns=join_columns,
+            )
+            # Generate random data for the rest of the set
+            cols = [hex(ncols * i + x) for x in range(njoin_columns, ncols)]
+            df = pd.concat(
+                [
+                    skeys,
+                    pd.DataFrame(
+                        np.random.randint(
+                            0,
+                            N * distinct_r,
+                            size=(skeys.shape[0], ncols - njoin_columns),
+                        ),
+                        columns=cols,
+                    ),
+                ],
+                axis=1,
+            )
+            self.sets.append(
+                NamedRelationalAlgebraFrozenSet(join_columns + cols, df)
+            )
+
+    def time_ra_naturaljoin_hard(
+        self, N, ncols, njoin_columns, njoins, distinct_r
+    ):
+        reduce(lambda a, b: a.naturaljoin(b), self.sets)
+
+    def time_ra_naturaljoin_easy(
+        self, N, ncols, njoin_columns, njoins, distinct_r
+    ):
+        reduce(lambda a, b: a.naturaljoin(b), self.sets[::-1])
 
 
 class TimeRelationalAlgebraFrozenSets:
