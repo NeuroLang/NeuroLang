63f56ef6d41f9344e42ff4f7c1d9893757d75efb
Jonas Renault <jonas.renault@inria.fr>
2021-01-15 16:29:29 +0100

    Added parent_tables reference on RAS to add all indexes on join

--- neurolang/utils/tests/test_sql_relational_algebra_set.py
+++ neurolang/utils/tests/test_sql_relational_algebra_set.py
@@ -1,8 +1,9 @@
 from neurolang.utils.relational_algebra_set.sql import (
     NamedRelationalAlgebraFrozenSet,
+    RelationalAlgebraFrozenSet,
     SQLAEngineFactory,
 )
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Table, MetaData
 from unittest.mock import patch
 import pytest
 
@@ -21,19 +22,83 @@ def mock_sql_engine():
     # meta.drop_all(engine_)
     engine_.dispose()
 
+def get_table_from_engine(table_name):
+    return Table(table_name, MetaData(),autoload=True, autoload_with=SQLAEngineFactory.get_engine())
 
 def test_set_init():
     a = [(i, i * 2) for i in range(5)]
     b = [(i * 2, i * 3) for i in range(5)]
-    c = [(i, i * 2, i * 3) for i in range(5)]
+    ras_a = RelationalAlgebraFrozenSet(a)
+    ras_b = RelationalAlgebraFrozenSet(b)
+    assert ras_a.is_empty() == False
+    assert ras_b.is_empty() == False
 
+
+def test_named_set_init():
+    a = [(i, i * 2) for i in range(5)]
+    b = [(i * 2, i * 3) for i in range(5)]
+    ras_a = NamedRelationalAlgebraFrozenSet(("z", "y"), a)
+    ras_b = NamedRelationalAlgebraFrozenSet(("y", "x"), b)
+
+    assert ras_a.is_empty() == False
+    assert (4, 8) in ras_a
+    assert ras_b.is_empty() == False
+    assert (4, 6) in ras_b
+
+
+def test_try_to_create_index():
+    """
+    try_to_create_index should not create duplicate indexes.
+    """
+    a = [(i, i * 2) for i in range(5)]
+    ras_a = NamedRelationalAlgebraFrozenSet(("z", "y"), a)
+    
+    ras_a._try_to_create_index(["y", "z"])
+    table = get_table_from_engine(ras_a._table_name)
+    assert len(table.indexes) == 1
+    assert set(next(iter(table.indexes)).columns.keys()) == {'y', 'z'}
+
+    ras_a._try_to_create_index(['z', 'y'])
+    table = get_table_from_engine(ras_a._table_name)
+    assert len(table.indexes) == 1
+
+    ras_a._try_to_create_index(['y'])
+    table = get_table_from_engine(ras_a._table_name)
+    assert len(table.indexes) == 2
+
+
+def test_try_to_create_index_on_views():
+    """
+    try_to_create_index should create indexes on parent tables when
+    called on views.
+    """
+    a = [(i, i * 2) for i in range(5)]
+    ras_a = NamedRelationalAlgebraFrozenSet(("z", "y"), a)
+    ras_b = NamedRelationalAlgebraFrozenSet(("v", "u"), a)
+    ab = ras_a.cross_product(ras_b)
+    aba = ab.naturaljoin(ras_a)
+    
+    table = get_table_from_engine(ras_a._table_name)
+    assert len(table.indexes) == 1
+    assert set(next(iter(table.indexes)).columns.keys()) == {'y', 'z'}
+
+    table = get_table_from_engine(ras_b._table_name)
+    assert len(table.indexes) == 0
+
+    abab = aba.naturaljoin(ras_b)
+    table = get_table_from_engine(ras_a._table_name)
+    assert len(table.indexes) == 1
+    table = get_table_from_engine(ras_b._table_name)
+    assert len(table.indexes) == 1
+    assert set(next(iter(table.indexes)).columns.keys()) == {'u', 'v'}
+
+
+def test_natural_join_creates_view():
+    a = [(i, i * 2) for i in range(5)]
+    b = [(i * 2, i * 3) for i in range(5)]
     ras_a = NamedRelationalAlgebraFrozenSet(("z", "y"), a)
     ras_b = NamedRelationalAlgebraFrozenSet(("y", "x"), b)
     ras_c = NamedRelationalAlgebraFrozenSet(("u", "v"), b)
 
-    assert((4, 8) in ras_a)
-
     res = ras_a.naturaljoin(ras_b)
-    res2 = ras_a.naturaljoin(ras_c)
-    res3 = res2.naturaljoin(ras_b)
-
+    assert res._is_view == True
