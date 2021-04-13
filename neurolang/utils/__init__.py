c2876dd7f972c805fa1f244f0a33c354a8d36f44
Demian Wassermann <demian.wassermann@inria.fr>
2021-04-13 23:03:48 +0200

    Added RAP weighted join

--- neurolang/utils/__init__.py
+++ neurolang/utils/__init__.py
@@ -2,12 +2,14 @@ from .orderedset import OrderedSet
 from .relational_algebra_set import (
     NamedRelationalAlgebraFrozenSet,
     RelationalAlgebraFrozenSet,
-    RelationalAlgebraSet
+    RelationalAlgebraSet,
+    RelationalAlgebraStringExpression
 )
 from .various import log_performance
 
 __all__ = [
     'OrderedSet', 'RelationalAlgebraSet',
     'RelationalAlgebraFrozenSet', 'NamedRelationalAlgebraFrozenSet',
+    'RelationalAlgebraStringExpression',
     'log_performance'
 ]
