fc09370a318f7e5aa7ffe56e8ca929dd59235a7f
Akshay Kumar <72260914+AXE02@users.noreply.github.com>
2021-04-23 13:21:48 +0530

    Fixed typos (#614)
    * Update relational_algebra.py
    
    * Update expression_processing.py

--- neurolang/datalog/expression_processing.py
+++ neurolang/datalog/expression_processing.py
@@ -393,7 +393,8 @@ def dependency_matrix(datalog, rules=None, instance=None):
     ------
     SymbolNotFoundError
         If there is a predicate in the antecedent of a rule which
-        is not a constant or an extensiona/intensional predicate.
+        is not a constant or an extension/intensional predicate
+
     """
 
     if rules is None:
