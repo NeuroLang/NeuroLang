f597b3a07d13f7d77c3d072aa8aba8aebac7b5c2
Jonas Renault <jonas.renault@inria.fr>
2021-07-08 16:41:07 +0200

    Refactoring and adding docs

--- neurolang/utils/server/app.py
+++ neurolang/utils/server/app.py
@@ -1,71 +1,186 @@
-from neurolang.utils.server.responses import CustomQueryResultsEncoder, QueryResults
-import threading
-from neurolang.utils.server.engines import init_frontend, load_neurosynth_data
-from threading import BoundedSemaphore
-import tornado.escape
+import json
+import logging
+from neurolang.utils.relational_algebra_set import (
+    NamedRelationalAlgebraFrozenSet,
+)
+import os.path
+from concurrent.futures import Future, ThreadPoolExecutor
+from multiprocessing import Value
+from threading import BoundedSemaphore, get_ident
+from typing import Dict
+from uuid import uuid4
+
 import tornado.ioloop
 import tornado.options
 import tornado.web
 import tornado.websocket
-import os.path
-import logging
-import json
-from uuid import uuid4
-import concurrent.futures
+from neurolang.utils.server.engines import init_frontend, load_neurosynth_data
+from neurolang.utils.server.responses import (
+    CustomQueryResultsEncoder,
+    QueryResults,
+)
 from tornado.options import define, options
-
 
 define("port", default=8888, help="run on the given port", type=int)
 
 LOG = logging.getLogger(__name__)
 LOG.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.ERROR)
+formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+ch.setFormatter(formatter)
+LOG.addHandler(ch)
+
+logger = logging.getLogger("neurolang")
+logger.propagate = False
 
 
-class NeurolangJobManager:
+class NeurolangQueryManager:
+    """
+    Class to manage execution of queries and keep track of results.
+    """
 
     executor = None
     engines = set()
+    created_engines = Value("i", 0)
     engines_sema = None
     results_cache = {}
 
-    def __init__(self, engines) -> None:
-        LOG.debug(
-            f"Initializing NeurolangJobManager with {len(engines)} engines."
-        )
-        for engine in engines:
-            self.engines.add(engine)
-        self.engines_sema = BoundedSemaphore(value=len(engines))
-        self.executor = concurrent.futures.ThreadPoolExecutor(
-            max_workers=len(engines)
-        )
+    def __init__(self, nb_engines: int, engine_create_func: callable) -> None:
+        """
+        By default, the query manager has nb_engines workers, but no engines.
+        It will call the _init_engines to create `nb_engines` engines
+        asynchronously.
 
-    def execute_neurolang_query(self, query):
-        LOG.debug(f"[{threading.get_ident()}] - Executing query...")
+        Parameters
+        ----------
+        nb_engines : int
+            the nb of engines to create. Also the number of workers.
+        engine_create_func : callable
+            a function to create a new engine. Should return a Neurolang instance.
+        """
+        LOG.debug(f"Creating query manager with {nb_engines} engines.")
+        self.engines_sema = BoundedSemaphore(value=0)
+        self.executor = ThreadPoolExecutor(max_workers=nb_engines)
+        self._init_engines(nb_engines, engine_create_func)
+
+    def _init_engines(self, nb_engines: int, engine_create_func: callable):
+        """
+        Dispatch a series of tasks to create `nb_engines` engines using the
+        `engine_create_func` function.
+
+        Parameters
+        ----------
+        nb_engines : int
+            the nb of engines to create. Also the number of workers.
+        engine_create_func : callable
+            a function to create a new engine. Should return a Neurolang instance.
+        """
+        # Dispatch the engine create tasks to the workers
+        for _ in range(nb_engines):
+            future = self.executor.submit(engine_create_func)
+            future.add_done_callback(self._engine_created)
+
+    def _engine_created(self, future: Future):
+        """
+        Callback called when an engine creation task has finished.
+
+        Parameters
+        ----------
+        future : concurrent.futures.Future
+            the future holding the result of the engine creation task.
+        """
+        engine = future.result()
+        with self.created_engines.get_lock():
+            self.engines.add(engine)
+            self.created_engines.value += 1
+            LOG.debug(
+                f"Added a created engine, got {self.created_engines.value}."
+            )
+            # Create semaphore with len(self.engines) tokens
+            self.engines_sema = BoundedSemaphore(
+                value=self.created_engines.value
+            )
+
+    def _execute_neurolang_query(
+        self, query: str
+    ) -> Dict[str, NamedRelationalAlgebraFrozenSet]:
+        """
+        Function executed on a ThreadPoolExecutor worker to execute a query
+        against a neurolang engine.
+
+        Parameters
+        ----------
+        query : str
+            the query to execute
+
+        Returns
+        -------
+        Dict[str, NamedRelationalAlgebraFrozenSet]
+            the result of the query execution
+        """
+        LOG.debug(f"[Thread - {get_ident()}] - Executing query...")
+        LOG.debug(f"[Thread - {get_ident()}] - Query : {query}")
         with self.engines_sema:
             engine = self.engines.pop()
-            LOG.debug(f"[{threading.get_ident()}] - Engine acquired.")
+            LOG.debug(f"[Thread - {get_ident()}] - Engine acquired.")
             try:
                 with engine.scope:
-                    LOG.debug(f"[{threading.get_ident()}] - Solving query...")
+                    LOG.debug(f"[Thread - {get_ident()}] - Solving query...")
                     res = engine.execute_datalog_program(query)
                     if res is None:
                         res = engine.solve_all()
-                    LOG.debug(f"[{threading.get_ident()}] - Query solved.")
+                    else:
+                        res = {"ans": res}
+                    LOG.debug(f"[Thread - {get_ident()}] - Query solved.")
                     return res
+            except Exception as e:
+                LOG.debug(
+                    f"[Thread - {get_ident()}] - Query execution raised {e}."
+                )
+                raise e
             finally:
                 self.engines.add(engine)
-                LOG.debug(f"[{threading.get_ident()}] - Engine released.")
+                LOG.debug(f"[Thread - {get_ident()}] - Engine released.")
 
-    def submit_query(self, uuid, query):
+    def submit_query(self, uuid: str, query: str) -> Future:
+        """
+        Submit the query to one of the available engines / workers.
+
+        Parameters
+        ----------
+        uuid : str
+            the uuid for the query.
+        query : str
+            the datalog query to execute.
+
+        Returns
+        -------
+        Future
+            a future result for the query execution.
+        """
         LOG.debug(f"Submitting query with uuid {uuid} to executor pool.")
-        future_res = self.executor.submit(self.execute_neurolang_query, query)
+        future_res = self.executor.submit(self._execute_neurolang_query, query)
         self.results_cache[uuid] = future_res
+        return future_res
 
-    def get_result(self, uuid):
+    def get_result(self, uuid: str) -> Future:
         return self.results_cache[uuid]
 
 
 class Application(tornado.web.Application):
+    """
+    tornado Application. Defines the routes which the application listens on.
+    Holds the NeurolangQueryManager which manages queries / results.
+
+    Parameters
+    ----------
+    nqm : NeurolangQueryManager
+        the query manager
+    """
+
     def __init__(self, njm):
         self.njm = njm
         uuid_pattern = (
@@ -139,7 +254,7 @@ class JSONRequestHandler(tornado.web.RequestHandler):
 
 class StatusHandler(JSONRequestHandler):
     """
-    Return the status (or the result) of an already running calculation
+    Return the status (or the result) of an already running calculation.
     """
 
     async def get(self, uuid: str):
@@ -166,22 +281,16 @@ class QueryHandler(JSONRequestHandler):
         return self.write_json_reponse({"query": query, "uuid": uuid})
 
 
-def setup_job_manager(nb_engines):
-    engines = []
-    for _ in range(nb_engines):
-        nl = init_frontend()
-        load_neurosynth_data(nl)
-        engines.append(nl)
-
-    njm = NeurolangJobManager(engines)
-    return njm
+def create_neurosynth_engine():
+    nl = init_frontend()
+    load_neurosynth_data(nl)
+    return nl
 
 
 def main():
+    njm = NeurolangQueryManager(2, create_neurosynth_engine)
+
     tornado.options.parse_command_line()
-
-    njm = setup_job_manager(2)
-
     print(f"Tornado application starting on port {options.port}")
     app = Application(njm)
     app.listen(options.port)
--- neurolang/utils/server/engines.py
+++ neurolang/utils/server/engines.py
@@ -9,10 +9,13 @@ from nilearn import datasets, image
 
 def load_neurosynth_data(nl):
     data_dir = Path("neurolang_data")
-    mni_t1 = nib.load(
-        datasets.fetch_icbm152_2009(data_dir=str(data_dir / "icbm"))["t1"]
+    resolution = 2
+    mni_mask = image.resample_img(
+        nib.load(
+            datasets.fetch_icbm152_2009(data_dir=str(data_dir / "icbm"))["gm"]
+        ),
+        np.eye(3) * resolution,
     )
-    mni_t1_4mm = image.resample_img(mni_t1, np.eye(3) * 4)
 
     ns_database_fn, ns_features_fn = datasets.utils._fetch_files(
         data_dir / "neurosynth",
@@ -30,26 +33,81 @@ def load_neurosynth_data(nl):
         ],
     )
 
-    ns_database = pd.read_csv(ns_database_fn, sep="\t")
+    activations = pd.read_csv(ns_database_fn, sep="\t")
+    mni_peaks = activations.loc[activations.space == "MNI"][
+        ["x", "y", "z", "id"]
+    ].rename(columns={"id": "study_id"})
+    non_mni_peaks = activations.loc[activations.space != "MNI"][
+        ["x", "y", "z", "id"]
+    ].rename(columns={"id": "study_id"})
+    proj_mat = np.linalg.pinv(
+        np.array(
+            [
+                [0.9254, 0.0024, -0.0118, -1.0207],
+                [-0.0048, 0.9316, -0.0871, -1.7667],
+                [0.0152, 0.0883, 0.8924, 4.0926],
+                [0.0, 0.0, 0.0, 1.0],
+            ]
+        ).T
+    )
+    projected = np.round(
+        np.dot(
+            np.hstack(
+                (
+                    non_mni_peaks[["x", "y", "z"]].values,
+                    np.ones((len(non_mni_peaks), 1)),
+                )
+            ),
+            proj_mat,
+        )[:, 0:3]
+    )
+    projected_df = pd.DataFrame(
+        np.hstack([projected, non_mni_peaks[["study_id"]].values]),
+        columns=["x", "y", "z", "study_id"],
+    )
+    peak_data = pd.concat([projected_df, mni_peaks]).astype(
+        {"x": int, "y": int, "z": int}
+    )
+    study_ids = peak_data[["study_id"]].drop_duplicates()
+
     ijk_positions = np.round(
         nib.affines.apply_affine(
-            np.linalg.inv(mni_t1_4mm.affine),
-            ns_database[["x", "y", "z"]].values.astype(float),
+            np.linalg.inv(mni_mask.affine),
+            peak_data[["x", "y", "z"]].values.astype(float),
         )
     ).astype(int)
-    ns_database["i"] = ijk_positions[:, 0]
-    ns_database["j"] = ijk_positions[:, 1]
-    ns_database["k"] = ijk_positions[:, 2]
+    peak_data["i"] = ijk_positions[:, 0]
+    peak_data["j"] = ijk_positions[:, 1]
+    peak_data["k"] = ijk_positions[:, 2]
+    peak_data = peak_data[["i", "j", "k", "study_id"]]
 
-    ns_features = pd.read_csv(ns_features_fn, sep="\t")
-    ns_docs = ns_features[["pmid"]].drop_duplicates()
-    ns_terms = pd.melt(
-        ns_features, var_name="term", id_vars="pmid", value_name="TfIdf"
-    ).query("TfIdf > 1e-3")[["pmid", "term"]]
+    features = pd.read_csv(ns_features_fn, sep="\t")
+    features.rename(columns={"pmid": "study_id"}, inplace=True)
 
-    activations = nl.add_tuple_set(ns_database, name="activations")
-    terms = nl.add_tuple_set(ns_terms, name="terms")
-    docs = nl.add_uniform_probabilistic_choice_over_set(ns_docs, name="docs")
+    term_data = pd.melt(
+        features,
+        var_name="term",
+        id_vars="study_id",
+        value_name="tfidf",
+    ).query("tfidf > 1e-3")[["term", "tfidf", "study_id"]]
+
+    nl.add_tuple_set(peak_data, name="PeakReported")
+    nl.add_tuple_set(study_ids, name="Study")
+    nl.add_tuple_set(term_data, name="TermInStudyTFIDF")
+    nl.add_uniform_probabilistic_choice_over_set(
+        study_ids, name="SelectedStudy"
+    )
+    nl.add_tuple_set(
+        np.hstack(
+            np.meshgrid(
+                *(np.arange(0, dim) for dim in mni_mask.get_fdata().shape)
+            )
+        )
+        .swapaxes(0, 1)
+        .reshape(3, -1)
+        .T,
+        name="Voxel",
+    )
 
 
 def init_frontend():
