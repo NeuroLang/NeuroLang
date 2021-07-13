7c748b6d96ca51cee785cca7ef995e6fd35cf4e9
Valentin Iovene <valentin@too.gy>
2021-07-13 10:55:38 +0200

    also fix Disjunction.__repr__

--- neurolang/logic/tests/test_repr.py
+++ neurolang/logic/tests/test_repr.py
@@ -1,6 +1,11 @@
-from .. import Conjunction
+from .. import Conjunction, Disjunction
 
 
 def test_conjunction_repr_ellipsis_pattern():
     pattern = Conjunction(...)
+    repr(pattern)
+
+
+def test_disjunction_repr_ellipsis_pattern():
+    pattern = Disjunction(...)
     repr(pattern)
