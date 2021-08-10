b739b61a558c55fa20bb2498605bb62c09c8de15
Jonas Renault <jonas.renault@inria.fr>
2021-08-10 15:22:56 +0200

    Adding tests for the symbols controller

--- neurolang/utils/server/neurolang-web/symbols/symbols.test.js
+++ neurolang/utils/server/neurolang-web/symbols/symbols.test.js
@@ -5,6 +5,7 @@ import 'datatables.net'
 import dt from 'datatables.net-se'
 import 'semantic-ui-css'
 import { SymbolsController } from './symbols'
+import { API_ROUTE } from '../constants'
 
 const mockShowViewer = jest.fn()
 const mockHideViewer = jest.fn()
@@ -12,6 +13,7 @@ const mockAddImage = jest.fn()
 const mockCanAdd = jest.fn()
 const mockCenter = jest.fn()
 const mockHist = jest.fn()
+const mockJqueryGet = jest.fn()
 
 jest.mock('../papaya/viewer', () => ({
   PapayaViewer: jest.fn().mockImplementation(() => {
@@ -44,7 +46,7 @@ const mockResults = {
         row_type: ["<class 'int'>", "<class 'int'>", "<class 'int'>", "<class 'float'>"],
         columns: ['i', 'j', 'k', 'PROB'],
         size: 27697,
-        values: [[2.0, 77.0, 46.0, 0.0015321803045817697], [4.0, 63.0, 34.0, 0.00023572711035194132], [5.0, 64.0, 38.0, 0.000419401098292473], [8.0, 54.0, 38.0, 0.001412803281281338], [12.0, 61.0, 40.0, 0.0018112978377986602], [12.0, 69.0, 36.0, 0.00045956587380234065], [14.0, 38.0, 40.0, 0.0018165763699549008], [14.0, 44.0, 32.0, 0.0003255290865078548], [14.0, 45.0, 54.0, 0.0008038844221973434], [14.0, 46.0, 32.0, 0.0014595104937104011]]
+        values: [[2.0, 77.0, 46.0, 0.0015321803045817697], [4.0, 63.0, 34.0, 0.00023572711035194132], [5.0, 64.0, 38.0, 0.000419401098292473], [8.0, 54.0, 38.0, 0.001412803281281338], [12.0, 61.0, 40.0, 0.0018112978377986602], [12.0, 69.0, 36.0, 0.00045956587380234065], [14.0, 38.0, 40.0, 0.0018165763699549008]]
       },
       TermInStudyTFIDF: {
         row_type: ["<class 'str'>", "<class 'float'>", "<class 'neurolang.frontend.neurosynth_utils.StudyID'>"],
@@ -81,7 +83,7 @@ function getTestHTML () {
           </div>
           <div class="ui  segment">
             <table class='ui compact striped table nl-symbols-table' id='tabTable' width="100%"></table>
-            <div class="ui message nl-functions-msg" id="queryAlert">
+            <div class="ui message nl-functions-msg">
               <div class="header nl-functions-header">
               </div>
               <p class="nl-functions-message"></p>
@@ -109,23 +111,103 @@ describe('SymbolsController', () => {
     sc = new SymbolsController()
   })
 
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
   it('should create', () => {
     expect(sc).toBeDefined()
   })
 
-  describe('setQueryResults', () => {
+  describe('setRouteEngine', () => {
+    const engine = 'neuroquery'
+
+    beforeEach(() => {
+      $.ajax = mockJqueryGet.mockImplementation(() => {
+        return $.Deferred().resolve(mockResults)
+      })
+    })
+
+    it('should get engine symbols and display them', () => {
+      sc.setRouteEngine(engine)
+      expect($.ajax).toHaveBeenCalledWith({
+        url: `${API_ROUTE.symbols}/${engine}`,
+        type: 'get'
+      })
+      const items = $('.nl-symbols-dropdown .item')
+      expect(items.length).toBe(3)
+    })
+  })
+
+  describe('base symbols', () => {
     let mockFetchTableData
     let mockTableData
+    const mockFunctions = {
+      agg_count: { type: 'typing.Callable[[typing.Iterable], numpy.int64]', doc: 'Aggregate count function', function: true },
+      superior_of: { type: 'typing.Callable[[neurolang.regions.Region, neurolang.regions.Region], bool]', doc: 'Superior_of function', function: true },
+      agg_create_region: { type: 'typing.Callable[[typing.Iterable, typing.Iterable, typing.Iterable], neurolang.regions.ExplicitVBR]', doc: 'Create some brain region', function: true }
+    }
+    let symbols
+
     beforeEach(() => {
-      mockShowViewer.mockClear()
-      mockHideViewer.mockClear()
       mockTableData = {
         draw: 1,
         recordsTotal: 500,
         recordsFiltered: 500,
         data: mockResults.data.results.ActivationGivenTerm.values
       }
-      mockFetchTableData = jest.spyOn(sc, 'fetchNewTableData').mockImplementation((data, callback, settings) => callback(mockTableData))
+      mockFetchTableData = jest.spyOn(sc, 'fetchNewTableData').mockImplementationOnce((data, callback, settings) => callback(mockTableData))
+      symbols = JSON.parse(JSON.stringify(mockResults.data))
+      Object.assign(symbols.results, mockFunctions)
+      sc.symbols = symbols
+      sc._updateSymbolsList()
+    })
+
+    it('should display the base symbols and functions', () => {
+      const items = $('.nl-symbols-dropdown .item')
+      expect(items.length).toBe(6)
+      const headers = $('.nl-symbols-dropdown .menu .header')
+      expect(headers.length).toBe(2)
+      expect(headers.first().text()).toBe('Base symbols')
+      expect(headers.last().text()).toBe('Functions')
+    })
+
+    it('should show the details for the selected symbol or function', () => {
+      // click on the first function item
+      $('.nl-symbols-dropdown .item.function').first().trigger('click')
+
+      expect($('.nl-symbols-table').is(':visible')).toBe(false)
+      expect($('.nl-functions-msg .nl-functions-header').text()).toBe('agg_count')
+      expect($('.nl-functions-msg .nl-functions-message').text()).toBe(`A function of type ${mockFunctions.agg_count.type}`)
+      expect($('.nl-functions-msg .nl-functions-help').text()).toBe(mockFunctions.agg_count.doc)
+
+      // click on last symbol item
+      mockTableData = {
+        draw: 2,
+        recordsTotal: 500,
+        recordsFiltered: 500,
+        data: mockResults.data.results.TermInStudyTFIDF.values
+      }
+      mockFetchTableData.mockImplementationOnce((data, callback, settings) => {
+        mockTableData.draw = data.draw
+        callback(mockTableData)
+      })
+      $('.nl-symbols-dropdown .item.symbol').last().trigger('click')
+      expect($('.nl-symbols-table tr').length).toBe(mockTableData.data.length + 1)
+    })
+  })
+
+  describe('setQueryResults', () => {
+    let mockFetchTableData
+    let mockTableData
+    beforeEach(() => {
+      mockTableData = {
+        draw: 1,
+        recordsTotal: 500,
+        recordsFiltered: 500,
+        data: mockResults.data.results.ActivationGivenTerm.values
+      }
+      mockFetchTableData = jest.spyOn(sc, 'fetchNewTableData').mockImplementationOnce((data, callback, settings) => callback(mockTableData))
     })
 
     it('should add symbols to the symbols list', () => {
@@ -161,10 +243,6 @@ describe('SymbolsController', () => {
           recordsFiltered: 1,
           data: mockResults.data.results.RegionImage.values
         }
-      })
-
-      afterEach(() => {
-        jest.clearAllMocks()
       })
 
       it('should show papaya viewer when tab has VBROverlay type', () => {
@@ -264,6 +342,69 @@ describe('SymbolsController', () => {
         $('.nl-vbr-overlay-hist').last().trigger('click')
         expect(mockHist).toHaveBeenCalledWith('image_1_0')
       })
+    })
+  })
+
+  describe('fetchNewTableData', () => {
+    const engine = 'neuroquery'
+    let data
+    let callback
+
+    beforeEach(() => {
+      data = { draw: 1, order: [], start: 0, length: 25 }
+      callback = jest.fn()
+      $.ajax = mockJqueryGet.mockImplementation(() => {
+        return $.Deferred().resolve(mockResults)
+      })
+      sc.results = mockResults.data
+    })
+
+    it('should send query to status endpoint when active symbol is a query result', () => {
+      const activeSymbol = 'ActivationGivenTerm'
+      sc.activeSymbol = activeSymbol
+      const queryData = {
+        symbol: activeSymbol,
+        start: data.start,
+        length: data.length
+      }
+      sc.fetchNewTableData(data, callback, null)
+      expect($.ajax).toHaveBeenCalledWith({
+        url: `${API_ROUTE.status}/${mockResults.data.uuid}`,
+        type: 'get',
+        data: queryData
+      })
+    })
+
+    it('should send query to symbol endpoint when active symbol is an engine symbol', () => {
+      sc.symbols = sc.results
+      sc.results = undefined
+      sc.engine = engine
+      const activeSymbol = 'TermInStudyTFIDF'
+      sc.activeSymbol = activeSymbol
+      const queryData = {
+        symbol: activeSymbol,
+        start: data.start,
+        length: data.length
+      }
+      sc.fetchNewTableData(data, callback, null)
+      expect($.ajax).toHaveBeenCalledWith({
+        url: `${API_ROUTE.symbols}/${engine}`,
+        type: 'get',
+        data: queryData
+      })
+    })
+
+    it('should call callback with result data', () => {
+      const activeSymbol = 'ActivationGivenTerm'
+      sc.activeSymbol = activeSymbol
+      const resultData = {
+        draw: data.draw,
+        recordsTotal: mockResults.data.results.ActivationGivenTerm.size,
+        recordsFiltered: mockResults.data.results.ActivationGivenTerm.size,
+        data: mockResults.data.results.ActivationGivenTerm.values
+      }
+      sc.fetchNewTableData(data, callback, null)
+      expect(callback).toHaveBeenCalledWith(resultData)
     })
   })
 })
