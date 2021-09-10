865b640a01c2bb6bfa66de52e23b51288a850948
Gaston Zanitti <gzanitti@gmail.com>
2021-09-10 13:11:22 +0200

    walk temporaly removed

--- neurolang/frontend/probabilistic_frontend.py
+++ neurolang/frontend/probabilistic_frontend.py
@@ -178,9 +178,11 @@ class NeurolangPDL(QueryBuilderDatalog):
                 symbol, iterable
             )
         self.program_ir.add_existential_rules(onto.existential_rules)
-        for symbol, expressions in entity_rules.items():
-            for e in expressions:
-                self.program_ir.walk(e)
+        #for symbol, expressions in entity_rules.items():
+        #    for e in expressions:
+        #        self.program_ir.walk(e)
+
+
             #if symbol not in self.symbol_table.keys():
             #    self.symbol_table[symbol] = Union(expressions)
             #else:
