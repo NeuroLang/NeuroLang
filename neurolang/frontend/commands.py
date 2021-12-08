ba42d59ea69426aa6cf1a053bdeda3cf46a8f80a
Jonas Renault <jonas.renault@inria.fr>
2021-12-08 10:47:51 +0100

    Add probabilistic options in load_csv

--- neurolang/frontend/commands.py
+++ neurolang/frontend/commands.py
@@ -21,10 +21,19 @@ class CommandsMixin(PatternWalker):
             - symbol: str, the name of the symbol to load the data into
             - url: str, the url to fetch the data from
         Other keyword arguments are passed to pandas' `read_csv` method with
-        limited support.
+        limited support (only string values).
 
         `.load_csv(Study, "https://github.com/neuroquery/neuroquery_data/raw/master/data/data-neuroquery_version-1_metadata.tsv.gz", sep="\t")`
-        will load NeuroQuery's metadata into the Study relation
+        will load NeuroQuery's metadata table into the Study relation
+
+        Probabilistic choices or facts can be loaded using the `probabilistic`
+        parameter:
+
+        `.load_csv(Study, "...", probabilistic="choice")`
+        loads the data as a probabilistic choice relation
+
+        `.load_csv(Study, "...", probabilistic="facts")`
+        loads the data as a probabilistic fact relation
 
         Raises
         ------
@@ -39,6 +48,7 @@ class CommandsMixin(PatternWalker):
             raise InvalidCommandExpression(
                 "Could not extract Symbol name and url from arguments."
             )
+        probabilistic = command.kwargs.pop("probabilistic", None)
         try:
             data = pd.read_csv(url, **command.kwargs)
         except ParserError as e:
@@ -46,7 +56,13 @@ class CommandsMixin(PatternWalker):
                 f"An error occured while parsing data from {url}: "
             ) from e
         data = data.rename(columns={n: i for i, n in enumerate(data.columns)})
-        self.add_extensional_predicate_from_tuples(symbol, data)
+
+        if probabilistic is None:
+            self.add_extensional_predicate_from_tuples(symbol, data)
+        elif probabilistic.lowercase() == "choice":
+            self.add_probabilistic_choice_from_tuples(symbol, data)
+        else:
+            self.add_probabilistic_facts_from_tuples(symbol, data)
 
     @add_match(Command)
     def unknown_command(self, command):
