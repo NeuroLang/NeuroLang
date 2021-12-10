bb6a8540d080d603f6d92c2c9dfbce9ca5c1faf1
Jonas Renault <jonas.renault@inria.fr>
2021-12-10 15:00:07 +0100

    Refactor load_csv command to work with constants and symbols

--- neurolang/frontend/commands.py
+++ neurolang/frontend/commands.py
@@ -4,11 +4,11 @@ from pandas.errors import ParserError
 from ..exceptions import InvalidCommandExpression, UnsupportedProgramError
 from ..expression_pattern_matching import add_match
 from ..expression_walker import PatternWalker
-from ..expressions import Command, Symbol
+from ..expressions import Command, Constant, Symbol
 
 
 class CommandsMixin(PatternWalker):
-    @add_match(Command("load_csv", ..., ...))
+    @add_match(Command(Symbol("load_csv"), ..., ...))
     def load_csv_command(self, command):
         """
         Process the `.load_csv` command. The load_csv command fetches tabular
@@ -23,49 +23,45 @@ class CommandsMixin(PatternWalker):
         Other keyword arguments are passed to pandas' `read_csv` method with
         limited support (only string values).
 
-        `.load_csv(Study, "https://github.com/neuroquery/neuroquery_data/raw/master/data/data-neuroquery_version-1_metadata.tsv.gz", sep="\t")`
+        `.load_csv(Study, "https://github.com/neuroquery/neuroquery_data/raw/
+        master/data/data-neuroquery_version-1_metadata.tsv.gz", sep="\t")`
         will load NeuroQuery's metadata table into the Study relation
-
-        Probabilistic choices or facts can be loaded using the `probabilistic`
-        parameter:
-
-        `.load_csv(Study, "...", probabilistic="choice")`
-        loads the data as a probabilistic choice relation
-
-        `.load_csv(Study, "...", probabilistic="facts")`
-        loads the data as a probabilistic fact relation
 
         Raises
         ------
         InvalidCommandExpression
-            raised if command args are invalid or if an error occurs while loading the data
+            raised if command args are invalid or if an error occurs while
+            loading the data
         """
         try:
             symbol, url = command.args
             if not isinstance(symbol, Symbol):
                 symbol = Symbol(symbol)
+            url = _unwrap_expr(url)
+            kwargs = {
+                _unwrap_expr(k): _unwrap_expr(v) for k, v in command.kwargs
+            }
         except ValueError:
             raise InvalidCommandExpression(
                 "Could not extract Symbol name and url from arguments."
             )
-        probabilistic = command.kwargs.pop("probabilistic", None)
         try:
-            data = pd.read_csv(url, **command.kwargs)
+            data = pd.read_csv(url, **kwargs)
         except ParserError as e:
             raise InvalidCommandExpression(
                 f"An error occured while parsing data from {url}: "
             ) from e
         data = data.rename(columns={n: i for i, n in enumerate(data.columns)})
-
-        if probabilistic is None:
-            self.add_extensional_predicate_from_tuples(symbol, data)
-        elif probabilistic.lower() == "choice":
-            self.add_probabilistic_choice_from_tuples(symbol, data)
-        else:
-            self.add_probabilistic_facts_from_tuples(symbol, data.to_records(index=False).tolist())
+        self.add_extensional_predicate_from_tuples(symbol, data)
 
     @add_match(Command)
     def unknown_command(self, command):
         raise UnsupportedProgramError(
             f"The command statement {command} is not supported."
         )
+
+
+def _unwrap_expr(expr):
+    if isinstance(expr, Constant):
+        return expr.value
+    return expr.name
--- neurolang/frontend/tests/test_commands.py
+++ neurolang/frontend/tests/test_commands.py
@@ -1,9 +1,12 @@
+from unittest.mock import patch
+
+import pandas as pd
 import pytest
 
 from ...datalog.basic_representation import DatalogProgram
 from ...exceptions import InvalidCommandExpression, UnsupportedProgramError
 from ...expression_walker import ExpressionBasicEvaluator
-from ...expressions import Command, Symbol
+from ...expressions import Command, Constant, Symbol
 from ...probabilistic.cplogic.program import CPLogicMixin
 from ..commands import CommandsMixin
 
@@ -15,46 +18,39 @@ class Datalog(
 
 
 def test_invalid_commands_raise_exceptions():
-    cmd = Command("load_csv", ("https://somerandomurl.csv",), {})
+    cmd = Command(
+        Symbol("load_csv"), (Constant("https://somerandomurl.csv"),), ()
+    )
 
     datalog = Datalog()
     with pytest.raises(InvalidCommandExpression):
         datalog.walk(cmd)
 
-    cmd = Command("print", (Symbol("A"),), {})
+    cmd = Command(Symbol("print"), (Symbol("A"),), ())
     with pytest.raises(UnsupportedProgramError):
         datalog.walk(cmd)
 
 
-def test_load_csv_command_adds_tuple_set():
+@patch("neurolang.frontend.commands.pd.read_csv")
+def test_load_csv_command_adds_tuple_set(mock_pd_readcsv):
+    mock_study_ids = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
+    mock_pd_readcsv.return_value = mock_study_ids
+
+    url = "https://github.com/neuroquery/.../data-neuroquery_version-1_metadata.tsv.gz"
+
     studies = Symbol("Studies")
     cmd = Command(
-        "load_csv",
-        (
-            studies,
-            "https://github.com/neuroquery/neuroquery_data/raw/master/data/data-neuroquery_version-1_metadata.tsv.gz",
-        ),
-        {"sep": "\t"},
+        Symbol("load_csv"),
+        (studies, Constant(url),),
+        ((Symbol("sep"), Constant("\t")),),
     )
 
     datalog = Datalog()
     datalog.walk(cmd)
 
+    mock_pd_readcsv.assert_called_with(url, sep="\t")
     assert studies in datalog.symbol_table
-
-
-def test_load_csv_command_adds_prob_facts():
-    topics = Symbol("Topics")
-    cmd = Command(
-        "load_csv",
-        (
-            topics,
-            "https://github.com/neurosynth/neurosynth-data/raw/master/data-neurosynth_version-7_vocab-LDA50_keys.tsv",
-        ),
-        {"sep": "\t", "index_col": "0", "probabilistic": "facts"},
+    assert (
+        datalog.symbol_table[studies].value
+        == mock_study_ids.to_records(index=False).tolist()
     )
-
-    datalog = Datalog()
-    datalog.walk(cmd)
-
-    assert topics in datalog.symbol_table
