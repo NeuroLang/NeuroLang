cadbd754f4fe7bc3fe70efe1a47264ad8965f275
Demian Wassermann <demian.wassermann@inria.fr>
2022-02-27 09:40:33 -0300

    Refactoring

--- neurolang/probabilistic/ranking.py
+++ neurolang/probabilistic/ranking.py
@@ -33,19 +33,8 @@ def verify_that_the_query_is_ranked(query):
 
 
 def partially_rank_query(query, symbol_table):
-    atoms_to_rank = defaultdict(set)
-    atom_argument_quantity = dict()
-    for atom in extract_logic_atoms(query):
-        if not isinstance(atom.functor, Symbol):
-            continue
-        atom_argument_quantity[atom.functor] = len(atom.args)
-        arg_counts = defaultdict(list)
-        for i, arg in enumerate(atom.args):
-            arg_counts[arg].append(i)
-        arg_counts_tuple = tuple(
-            (v[0], tuple(v[1:])) for v in arg_counts.values()
-        )
-        atoms_to_rank[atom.functor].add(arg_counts_tuple)
+    atoms_to_rank, atom_argument_quantity = \
+        _process_atoms_with_repeatd_arguments(query)
 
     for symbol, instances_to_rank in atoms_to_rank.items():
         if len(instances_to_rank) != 1:
@@ -53,22 +42,9 @@ def partially_rank_query(query, symbol_table):
         arguments_to_rank = instances_to_rank.pop()
         if all(len(repeats) == 0 for _, repeats in arguments_to_rank):
             continue
-        expression = symbol
-        arguments_to_eliminate = set()
-        for arg, repeats in arguments_to_rank:
-            arg = int2columnint_constant(arg)
-            for repeat in repeats:
-                expression = Selection(
-                    expression,
-                    eq_(arg, int2columnint_constant(repeat))
-                )
-            arguments_to_eliminate.update(repeats)
-        attributes = tuple(
-            int2columnint_constant(i)
-            for i in range(atom_argument_quantity[symbol])
-            if i not in arguments_to_eliminate
+        expression, arguments_to_eliminate = _build_ra_expression(
+            atom_argument_quantity, symbol, arguments_to_rank
         )
-        expression = Projection(expression, attributes)
         new_symbol = Symbol.fresh()
         symbol_table[new_symbol] = expression
         replacements = {
@@ -83,3 +59,39 @@ def partially_rank_query(query, symbol_table):
         query = ReplaceExpressionWalker(replacements).walk(query)
     return query
 
+
+def _build_ra_expression(atom_argument_quantity, symbol, arguments_to_rank):
+    expression = symbol
+    arguments_to_eliminate = set()
+    for arg, repeats in arguments_to_rank:
+        arg = int2columnint_constant(arg)
+        for repeat in repeats:
+            expression = Selection(
+                    expression,
+                    eq_(arg, int2columnint_constant(repeat))
+                )
+        arguments_to_eliminate.update(repeats)
+    attributes = tuple(
+            int2columnint_constant(i)
+            for i in range(atom_argument_quantity[symbol])
+            if i not in arguments_to_eliminate
+        )
+    expression = Projection(expression, attributes)
+    return expression, arguments_to_eliminate
+
+
+def _process_atoms_with_repeatd_arguments(query):
+    atoms_to_rank = defaultdict(set)
+    atom_argument_quantity = dict()
+    for atom in extract_logic_atoms(query):
+        if not isinstance(atom.functor, Symbol):
+            continue
+        atom_argument_quantity[atom.functor] = len(atom.args)
+        arg_counts = defaultdict(list)
+        for i, arg in enumerate(atom.args):
+            arg_counts[arg].append(i)
+        arg_counts_tuple = tuple(
+            (v[0], tuple(v[1:])) for v in arg_counts.values()
+        )
+        atoms_to_rank[atom.functor].add(arg_counts_tuple)
+    return atoms_to_rank, atom_argument_quantity
