35d49007e5ac9626997430058e3e6eeaa9a2d3cd
Gaston Zanitti <gzanitti@gmail.com>
2022-03-23 15:55:57 +0100

    Another test. Bug fixed

--- neurolang/frontend/datalog/sugar/tests/test_spatial.py
+++ neurolang/frontend/datalog/sugar/tests/test_spatial.py
@@ -184,5 +184,43 @@ def test_bound_with_symbol_in_table():
     assert len(a3) == 2 and [2, 2, 1] in a3 and [1, 5, 9] in a3
 
 
+def test_bound_with_symbol_in_table_inverted():
+    import pandas as pd
+    from neurolang.frontend import NeurolangPDL
+
+    nl = NeurolangPDL()
+
+    Vol1 = pd.DataFrame([[1, 2, 3], [2, 2, 2], [7, 4, 3]], columns=['x', 'y', 'z'])
+    Vol2 = pd.DataFrame([[1, 5, 9, 6.8], [2, 2, 1, 2]], columns=['x', 'y', 'z', 'dist'])
+
+    Vol1s = nl.add_tuple_set(Vol1.values, name='Vol1s')
+    Vol2s = nl.add_tuple_set(Vol2.values, name='Vol2s')
+
+    with nl.scope as e:
+        e.res[e.x1, e.y1, e.z1] = (
+            Vol1s[e.x2, e.y2, e.z2]
+            & Vol2s(e.x1, e.y1, e.z1, e.dist)
+            & (e.d == e.EUCLIDEAN(e.x1, e.y1, e.z1, e.x2, e.y2, e.z2))
+            & (e.d < e.dist)
+        )
+
+        a1 = nl.query((e.x1, e.y1, e.z1), e.res[e.x1, e.y1, e.z1])
+
+    with nl.scope as e:
+        e.res[e.x1, e.y1, e.z1] = (
+            Vol1s[e.x2, e.y2, e.z2]
+            & Vol2s(e.x1, e.y1, e.z1, e.dist)
+            & (e.d == e.EUCLIDEAN(e.x1, e.y1, e.z1, e.x2, e.y2, e.z2))
+            & (e.d < 2)
+        )
+
+        a2 = nl.query((e.x1, e.y1, e.z1), e.res[e.x1, e.y1, e.z1])
+
+    a1 = a1.as_pandas_dataframe().values
+    a2 = a2.as_pandas_dataframe().values
+    assert len(a1) == 2 and [2, 2, 1] in a1 and [1,5,9] in a1
+    assert len(a2) == 1 and [2, 2, 1] in a2
+
+
 
 
