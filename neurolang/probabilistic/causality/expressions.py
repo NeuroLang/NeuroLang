7b6717b8a70f4c34f8b8eaba82fe6199051f4e66
Gaston Zanitti <gzanitti@gmail.com>
2022-04-07 15:10:32 +0200

    Causality: Basic expression processing

--- neurolang/probabilistic/causality/expressions.py
+++ neurolang/probabilistic/causality/expressions.py
@@ -1,13 +1,11 @@
 from ...expression_pattern_matching import add_match
 from ...expression_walker import ExpressionWalker
-from ...logic import Conjunction
+from ...logic import Conjunction, Implication, NaryLogicOperator, Negation, Union
 from ..exceptions import MalformedCausalOperatorError
 from ..expressions import Condition
-from ...expressions import Symbol, Constant
+from ...expressions import Constant, Symbol
 
-DO = Symbol("DO")
-
-class CausalIntervention(Conjunction):
+class CausalIntervention(NaryLogicOperator):
     def __init__(self, formulas):
         self.formulas = tuple(formulas)
 
@@ -15,15 +13,20 @@ class CausalIntervention(Conjunction):
         for formula in self.formulas:
             if any([not isinstance(arg, Constant) for arg in formula.args]):
                 raise MalformedCausalOperatorError('''The atoms intervened by the
-                operator DO can only contain constants''')
+                DO operator can only contain constants''')
             self._symbols |= formula._symbols
 
     def __repr__(self):
         return f"DO{self.formulas}"
 
 
+class CausalInterventionIdentification(ExpressionWalker):
 
-class CausalInterventionWalker(ExpressionWalker):
+    @add_match(Implication)
+    def process_imp(self, implication):
+        self.walk(implication.antecedent)
+
+        return implication
 
     @add_match(Condition)
     def valid_intervention(self, condition):
@@ -32,9 +35,90 @@ class CausalInterventionWalker(ExpressionWalker):
             for formula in condition.conditioning.formulas
             if isinstance(formula, CausalIntervention)
         ]
-        if len(n_interventions) != 1:
+        if len(n_interventions) > 1:
             raise MalformedCausalOperatorError('''The use of more than one DO operator
             is not allowed. All interventions must be combined in a single DO operator.
             ''')
+        elif len(n_interventions) == 1:
+            self.intervention = n_interventions[0]
 
-        return n_interventions[0]
+class CausalInterventionRewriter(ExpressionWalker):
+
+    def __init__(self, intervention):
+        self.intervention = intervention
+        self.new_facts = set()
+
+        self.intervention_replacement = {}
+        self.new_head_replacement = {}
+
+    def has_causal_intervention(rule):
+        for formula in rule.antecedent.conditioning.formulas:
+            if isinstance(formula, CausalIntervention):
+                return True
+        return False
+
+    @add_match(Implication(..., Condition), has_causal_intervention)
+    def rewrite_do_operator(self, rule):
+        if self.intervention is not None:
+            old_antecedent = rule.antecedent
+            new_conditioning = []
+            for formula in old_antecedent.conditioning.formulas:
+                if isinstance(formula, CausalIntervention):
+                    for ci in formula.formulas:
+                        new_conditioning.append(ci)
+                else:
+                    new_conditioning.append(formula)
+
+            new_rule = Implication(
+                rule.consequent,
+                Condition(
+                    old_antecedent.conditioned,
+                    Conjunction(tuple(new_conditioning))
+                )
+            )
+            return new_rule
+
+    @add_match(Implication, lambda rule: not rule.consequent.functor.is_fresh)
+    def rewrite_implication(self, rule):
+        if self.intervention is not None:
+            head = rule.consequent
+            matched_atom = [atom for atom in self.intervention.formulas if head.functor == atom.functor]
+
+            if len(matched_atom) == 1:
+                matched_functor = matched_atom[0].functor
+                if matched_functor not in self.intervention_replacement.keys():
+                    new_int_functor = Symbol.fresh()
+                    # symbol f1(x)
+                    new_int_atom = new_int_functor(*head.args)
+                    # symbol f1(a)
+                    new_fact = new_int_functor(*matched_atom[0].args)
+                    self.intervention_replacement[matched_functor] = new_int_functor
+                    # added f1(a) as fact
+                    self.new_facts.add(new_fact)
+
+                    # symbol f2(x)
+                    new_head_functor = Symbol.fresh()
+                    new_head = new_head_functor(*head.args)
+                    self.new_head_replacement[matched_functor] = new_head_functor
+
+                    # added rule f2(x) <- f1(x)
+                    f1impf2 = Implication(new_head, new_int_atom)
+                    # added rule f2(x) <- old and not(f1(a))
+                    notf1impf2 = Implication(new_head, Conjunction((rule.antecedent, Negation(new_int_atom))))
+                    rule = Union((f1impf2, notf1impf2))
+                else:
+                    # symbol f1(x)
+                    new_int_functor = self.intervention_replacement[matched_functor]
+                    new_int_atom = new_int_functor(*head.args)
+
+                    # symbol f2(x)
+                    new_head_functor = self.new_head_replacement[matched_functor]
+                    new_head = new_head_functor(*head.args)
+
+                    # rule f2(x) <- f1(x) already exists
+                    # added rule f2(x) <- old and not(f1(a))
+                    rule = Implication(new_head, Conjunction((rule.antecedent, Negation(new_int_atom))))
+
+        return rule
+
+
--- neurolang/probabilistic/causality/tests/test_expression_processing.py
+++ neurolang/probabilistic/causality/tests/test_expression_processing.py
@@ -1,16 +1,26 @@
 import pytest
 
 from ....expressions import Constant, Symbol
-from ....logic import Conjunction
+from ....logic import Conjunction, Implication, Negation, Union
 from ...exceptions import MalformedCausalOperatorError
 from ...expressions import Condition
-from ..expressions import CausalIntervention, CausalInterventionWalker
+from ..expressions import CausalIntervention, CausalInterventionIdentification, CausalInterventionRewriter
+
+P = Symbol("P")
+Q = Symbol("Q")
+R = Symbol("R")
+S = Symbol("S")
+T = Symbol("T")
+U = Symbol("U")
+ans = Symbol("ans")
+
+x = Symbol("x")
+y = Symbol("y")
+z = Symbol("z")
+a = Constant("a")
+b = Constant("b")
 
 def test_symbols_not_allowed():
-    P = Symbol("P")
-    Q = Symbol("Q")
-
-    x = Symbol("x")
     with pytest.raises(MalformedCausalOperatorError, match="The atoms intervened by the*"):
         Condition(
             P(x),
@@ -22,12 +32,6 @@ def test_symbols_not_allowed():
         )
 
 def test_do_instantiation():
-    P = Symbol("P")
-    Q = Symbol("Q")
-
-    x = Symbol("x")
-    a = Constant("a")
-
     imp = Condition(
         P(x),
         Conjunction((
@@ -36,21 +40,13 @@ def test_do_instantiation():
             )),
         ))
     )
-    ciw = CausalInterventionWalker()
+    ciw = CausalInterventionIdentification()
 
-    ci = ciw.walk(imp)
-    assert ci == CausalIntervention((Q(a),))
+    ciw.walk(imp)
+    assert ciw.intervention == CausalIntervention((Q(a),))
 
 
 def test_do_instantiation_more_atoms():
-    P = Symbol("P")
-    Q = Symbol("Q")
-    R = Symbol("R")
-
-    x = Symbol("x")
-    a = Constant("a")
-    b = Constant("b")
-
     imp = Condition(
         P(x),
         Conjunction((
@@ -59,19 +55,12 @@ def test_do_instantiation_more_atoms():
             )),
         ))
     )
-    ciw = CausalInterventionWalker()
+    ciw = CausalInterventionIdentification()
 
     ciw.walk(imp)
-    assert CausalIntervention((Q(a),R(b)))
+    assert ciw.intervention == CausalIntervention((Q(a),R(b)))
 
 def test_multiple_instantiation_exception():
-    P = Symbol("P")
-    Q = Symbol("Q")
-    R = Symbol("R")
-
-    x = Symbol("x")
-    a = Constant("a")
-
     imp = Condition(
         P(x),
         Conjunction((
@@ -83,7 +72,233 @@ def test_multiple_instantiation_exception():
             )),
         ))
     )
-    ciw = CausalInterventionWalker()
+    ciw = CausalInterventionIdentification()
     with pytest.raises(MalformedCausalOperatorError, match="The use of more than one DO operator*"):
         ciw.walk(imp)
 
+def test_nothing_to_rewrite():
+    ciw = CausalInterventionIdentification()
+    imp1 = Implication(Q(x), P(x))
+    imp2 = Implication(
+        ans(x),
+        Condition(
+            R(x),
+            Conjunction((
+                    Q(a),
+            ))
+        )
+    )
+
+    _ = ciw.walk(Union((imp1, imp2)))
+    assert not hasattr(ciw, 'intervention')
+
+
+def test_simple_rewrite():
+    ciw = CausalInterventionIdentification()
+    imp1 = Implication(Q(x), P(x))
+    imp2 = Implication(
+        ans(x),
+        Condition(
+            R(x),
+            Conjunction((
+                CausalIntervention((
+                    Q(a),
+                )),
+            ))
+        )
+    )
+
+    formulas = ciw.walk(Union((imp1, imp2)))
+
+    cir = CausalInterventionRewriter(ciw.intervention)
+    new_formulas = cir.walk(formulas)
+
+    plain_unions = []
+    for f1 in new_formulas.formulas:
+        if isinstance(f1, Union):
+            for f2 in f1.formulas:
+                plain_unions.append(f2)
+        else:
+            plain_unions.append(f1)
+
+    new_formulas = Union(tuple(plain_unions))
+
+    assert len(cir.intervention.formulas) == 1
+    intervention = cir.intervention.formulas[0]
+    new_f1 = cir.intervention_replacement[intervention.functor]
+    new_f2 = cir.new_head_replacement[intervention.functor]
+
+    assert new_f1.is_fresh
+    assert new_f2.is_fresh
+
+    new_f1 = new_f1(x)
+    new_f2 = new_f2(x)
+
+    new_imp1 = Implication(new_f2, new_f1)
+    new_imp2 = Implication(
+        ans(x),
+        Condition(
+            R(x),
+            Conjunction((
+                Q(a),
+            ))
+        )
+    )
+    new_rule = Implication(new_f2, Conjunction((P(x), Negation(new_f1))))
+    new_fact = new_f1.functor(*intervention.args)
+
+    assert len(cir.new_facts) == 1
+    assert new_fact == list(cir.new_facts)[0]
+    assert len(new_formulas.formulas) == 3
+    assert new_imp1 in new_formulas.formulas
+    assert new_imp2 in new_formulas.formulas
+    assert new_rule in new_formulas.formulas
+
+
+def test_rewrite_two_implications():
+    ciw = CausalInterventionIdentification()
+    imp1 = Implication(Q(z), P(z))
+    imp2 = Implication(
+        ans(x),
+        Condition(
+            R(x),
+            Conjunction((
+                CausalIntervention((
+                    Q(a),
+                )),
+            ))
+        )
+    )
+    imp3 = Implication(Q(x), S(x, y))
+
+    formulas = ciw.walk(Union((imp1, imp2, imp3)))
+
+    cir = CausalInterventionRewriter(ciw.intervention)
+    new_formulas = cir.walk(formulas)
+
+    plain_unions = set()
+    for f1 in new_formulas.formulas:
+        if isinstance(f1, Union):
+            for f2 in f1.formulas:
+                plain_unions.add(f2)
+        else:
+            plain_unions.add(f1)
+
+    new_formulas = Union(tuple(plain_unions))
+
+    assert len(cir.intervention.formulas) == 1
+    intervention = cir.intervention.formulas[0]
+    new_f1 = cir.intervention_replacement[intervention.functor]
+    new_f2 = cir.new_head_replacement[intervention.functor]
+
+    assert new_f1.is_fresh
+    assert new_f2.is_fresh
+
+    new_f2_rule_z = new_f2(z)
+    new_f2_rule_x = new_f2(x)
+
+    new_f1_rule_z = new_f1(z)
+    new_f1_rule_x = new_f1(x)
+
+    new_imp1_x = Implication(new_f2_rule_x, new_f1_rule_x)
+    new_imp1_z = Implication(new_f2_rule_z, new_f1_rule_z)
+    new_imp2 = Implication(
+        ans(x),
+        Condition(
+            R(x),
+            Conjunction((
+                Q(a),
+            ))
+        )
+    )
+    new_rule1 = Implication(new_f2_rule_z, Conjunction((P(z), Negation(new_f1_rule_z))))
+    new_rule2 = Implication(new_f2_rule_x, Conjunction((S(x, y), Negation(new_f1_rule_x))))
+    new_fact = new_f1(*intervention.args)
+
+    assert len(cir.new_facts) == 1
+    assert new_fact == list(cir.new_facts)[0]
+    assert len(new_formulas.formulas) == 4
+    assert (new_imp1_x in new_formulas.formulas) or (new_imp1_z in new_formulas.formulas)
+    assert new_imp2 in new_formulas.formulas
+    assert new_rule1 in new_formulas.formulas
+    assert new_rule2 in new_formulas.formulas
+
+def test_rewrite_two_interventions():
+    ciw = CausalInterventionIdentification()
+    imp1 = Implication(Q(z), P(z))
+    imp2 = Implication(
+        ans(x),
+        Condition(
+            T(x),
+            Conjunction((
+                CausalIntervention((
+                    Q(a),R(a, b)
+                )),
+            ))
+        )
+    )
+    imp3 = Implication(R(x, y), S(y, x))
+    imp4 = Implication(T(z), U(y, z))
+
+    formulas = ciw.walk(Union((imp1, imp2, imp3, imp4)))
+
+    cir = CausalInterventionRewriter(ciw.intervention)
+    new_formulas = cir.walk(formulas)
+
+    plain_unions = set()
+    for f1 in new_formulas.formulas:
+        if isinstance(f1, Union):
+            for f2 in f1.formulas:
+                plain_unions.add(f2)
+        else:
+            plain_unions.add(f1)
+
+    new_formulas = Union(tuple(plain_unions))
+
+    assert len(cir.intervention.formulas) == 2
+    intervention_0 = cir.intervention.formulas[0]
+    new_f0s = cir.intervention_replacement[intervention_0.functor]
+    new_f1s = cir.new_head_replacement[intervention_0.functor]
+
+    intervention_1 = cir.intervention.formulas[1]
+    new_f2s = cir.intervention_replacement[intervention_1.functor]
+    new_f3s = cir.new_head_replacement[intervention_1.functor]
+
+    assert new_f0s.is_fresh
+    assert new_f1s.is_fresh
+    assert new_f2s.is_fresh
+    assert new_f3s.is_fresh
+
+    new_f0 = new_f0s(z)
+    new_f1 = new_f1s(z)
+
+    new_f2 = new_f2s(x, y)
+    new_f3 = new_f3s(x, y)
+
+    new_imp_int1 = Implication(new_f1, new_f0)
+    new_imp_int2 = Implication(new_f3, new_f2)
+    new_imp2 = Implication(
+        ans(x),
+        Condition(
+            T(x),
+            Conjunction((
+                Q(a),R(a, b)
+            ))
+        )
+    )
+    new_rule1 = Implication(new_f1, Conjunction((P(z), Negation(new_f0))))
+    new_rule2 = Implication(new_f3, Conjunction((S(y, x), Negation(new_f2))))
+
+    new_fact1 = new_f0s(*intervention_0.args)
+    new_fact2 = new_f2s(*intervention_1.args)
+
+    assert len(cir.new_facts) == 2
+    assert new_fact1 in list(cir.new_facts)
+    assert new_fact2 in list(cir.new_facts)
+    assert len(new_formulas.formulas) == 6
+    assert new_imp_int1 in new_formulas.formulas
+    assert new_imp_int2 in new_formulas.formulas
+    assert new_imp2 in new_formulas.formulas
+    assert new_rule1 in new_formulas.formulas
+    assert new_rule2 in new_formulas.formulas
+    assert imp4 in new_formulas.formulas
