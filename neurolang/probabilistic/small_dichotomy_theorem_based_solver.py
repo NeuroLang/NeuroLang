beed31fd918a727ddc8cc7b896c8f0f4b47b73fe
Demian Wassermann <demian.wassermann@inria.fr>
2022-04-07 14:24:03 +0200

    another small bug

--- neurolang/probabilistic/small_dichotomy_theorem_based_solver.py
+++ neurolang/probabilistic/small_dichotomy_theorem_based_solver.py
@@ -61,7 +61,7 @@ from .probabilistic_ra_utils import (
 from .query_resolution import (
     generate_provenance_query_solver,
     lift_solve_marg_query,
-    reintroduce_unified_head_terms
+    compute_projections_needed_to_reintroduce_head_terms
 )
 from .shattering import shatter_constants
 
@@ -231,7 +231,7 @@ def solve_succ_query(query, cpl_program, run_relational_algebra_solver=True):
         # project on query's head variables
         ra_query = _project_on_query_head(ra_query, shattered_query)
         # re-introduce head variables potentially removed by unification
-        needed_projections = reintroduce_unified_head_terms(
+        needed_projections = compute_projections_needed_to_reintroduce_head_terms(
             ra_query, flat_query, unified_query
         )
 
