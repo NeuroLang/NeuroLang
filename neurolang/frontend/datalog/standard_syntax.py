e9855408af712b4134a061511ec14cf4d10fb323
lalshikh <liza.alshikhley@gmail.com>
2023-05-04 11:22:54 +0200

    [Tatsu2Lark] Arithmetic operation handling

--- neurolang/frontend/datalog/standard_syntax.py
+++ neurolang/frontend/datalog/standard_syntax.py
@@ -2,6 +2,7 @@ from operator import add, eq, ge, gt, le, lt, mul, ne, pow, sub, truediv
 
 import tatsu
 from lark import Lark
+from lark import Transformer
 
 from neurolang.logic import ExistentialPredicate
 
@@ -127,41 +128,59 @@ GRAMMAR_lark = u"""
     // https://stackoverflow.com/questions/74169478/how-to-define-end-of-file-in-a-lark-grammar
     // Proposed solution with newline symbol (for Early parsing algorithm): 
     // https://stackoverflow.com/questions/70966085/how-does-one-match-eol-newline-with-lark
-    start : /expressions $/
-    expressions : "toto"
     
-    lambda_expression = "lambda" arguments ":" argument
-    arguments = "," /.{ argument }+/
-    arguments : type name [function_argument ("," function_argument)*]
-    argument = arithmetic_operation
-             | function_application
-             | "..."
+    start: expressions EOF
+expressions : ([NEWLINE] expression)+
+
+// The initial way of performing the arithmetic operation does nothing as no sign appear in the tree because with Lark, unnamed litterals are skiped from the tree (see doc).
+// -> numbers and signs have to be handled in another way such that the operation signs appear in the tree.
+// Plus, if we add the arithmetic_operation rule in the expression rule, it generates the following error message :
+//GrammarError: Reduce/Reduce collision in Terminal('TRUE') between the following rules: 
+//	- <term : number>
+//	- <expression : number>
+//    collision occurred in state: {
+//	<expression : number * >
+//	<term : number * >
+//    }
+
+//Reduce/Reduce collision in Terminal('__ANON_0') between the following rules: 
+//	- <term : number>
+//	- <expression : number>
+//    collision occurred in state: {
+//	<expression : number * >
+//	<term : number * >
+//    }
+// -> we have to implement numbers and aritmetic operations such that they are easily distinguishable by the parser
+expression : logical_constant | number | arithmetic_operation
+
+
+// arithmetic_operation : term [ ("+" | "-") term ]
+
+arithmetic_operation : term [ term ]
+
+term : number
+
+number : sign? float | sign? integer
+
+// Can't leave the stars. Cause : LexError: Lexer does not allow zero-width terminals. (__ANON_1: '[0-9]*')
+float : "." integer
+      | integer "." integer
+      | integer "."
+integer : /[0-9]+/
+signedint : sign /[0-9]+/
+sign : SIGN
+SIGN : "+" | "-"
+
+logical_constant : FALSE | TRUE
+TRUE : "True" | "\u22A4"
+FALSE : "False" | "\u22A5"
+newline : NEWLINE newline
+NEWLINE : /[(\\u000C)? (\\r)? \\n]+/
+EOF : "\\n"
+
+WHITESPACE : /[\t ]+/
+%ignore WHITESPACE
     
-    literal : number
-            | text
-            | ext_identifier
-    ext_identifier : /@ identifier/
-    identifier : /!reserved_words [a-zA-Z_][a-zA-Z0-9_]*/
-               | /`[0-9a-zA-Z\/#%._:-]+`/
-    reserved_words : exists
-                   | "st"
-                   | "ans"
-    exists : "exists" | "\u2203" | "EXISTS"
-    python_string : /"[^"]*"/
-                  | /'[^']*'/
-    comparison_operator : "==" | "<" | "<=" | ">=" | ">" | "!="
-    text : /"[a-zA-Z0-9 ]*"/
-          | /'[a-zA-Z0-9 ]*'/
-    
-    number : float | integer
-    integer : [ "+" | "-" ] /[0-9]+/
-    float : [ "+" | "-" ] /[0-9]*/"."/[0-9]+/
-          | [ "+" | "-" ] /[0-9]+/"."/[0-9]*/
-    
-    logical_constant : TRUE | FALSE
-    TRUE : "True" | "\u22A4"
-    FALSE : "False" | "\u22A5"
-    newline : /(\\u000C | \\r | \\n)+/
 """
 
 OPERATOR = {
@@ -440,6 +459,259 @@ class DatalogSemantics:
         return ast
 
 
+class DatalogSemantics_lark(Transformer):
+    def __init__(self, locals=None, globals=None):
+        super().__init__()
+
+        if locals is None:
+            locals = {}
+        if globals is None:
+            globals = {}
+
+        self.locals = locals
+        self.globals = globals
+
+    def start(self, ast):
+        return ast
+
+    def expressions(self, ast):
+        if isinstance(ast, Expression):
+            ast = (ast,)
+        return Union(ast)
+
+    def fact(self, ast):
+        return Fact(ast)
+
+    def probabilistic_fact(self, ast):
+        return Implication(
+            ProbabilisticFact(ast[0], ast[2]),
+            Constant(True),
+        )
+
+    def constant_predicate(self, ast):
+        return ast[0](*ast[2])
+
+    def rule(self, ast):
+        head = ast[0]
+        if isinstance(head, Expression) and head.functor == Symbol("ans"):
+            return Query(ast[0], ast[2])
+        else:
+            return Implication(ast[0], ast[2])
+
+    def probabilistic_rule(self, ast):
+        head = ast[0]
+        probability = ast[2]
+        body = ast[4]
+        return Implication(
+            ProbabilisticFact(probability, head),
+            body,
+        )
+
+    def constraint(self, ast):
+        return RightImplication(ast[0], ast[2])
+
+    def statement(self, ast):
+        return Statement(ast[0], ast[2])
+
+    def statement_function(self, ast):
+        return Statement(
+            ast[0],
+            Lambda(ast[2], ast[-1])
+        )
+
+    def body(self, ast):
+        return Conjunction(ast)
+
+    def condition(self, ast):
+
+        conditioned = ast[0]
+        if isinstance(conditioned, list):
+            conditioned = Conjunction(tuple(conditioned))
+
+        condition = ast[2]
+        if isinstance(condition, list):
+            condition = Conjunction(tuple(condition))
+
+        return Condition(conditioned, condition)
+
+    def head_predicate(self, ast):
+        if not isinstance(ast, Expression):
+            if len(ast) == 4:
+                arguments = []
+                for arg in ast[2]:
+                    arguments.append(arg)
+
+                if PROB in arguments:
+                    ix_prob = arguments.index(PROB)
+                    arguments_not_prob = (
+                        arguments[:ix_prob] +
+                        arguments[ix_prob + 1:]
+                    )
+                    prob_arg = PROB(*arguments_not_prob)
+                    arguments = (
+                        arguments[:ix_prob] +
+                        [prob_arg] +
+                        arguments[ix_prob + 1:]
+                    )
+
+                ast = ast[0](*arguments)
+            else:
+                ast = ast[0]()
+        return ast
+
+    def query(self, ast):
+        if len(ast) == 3:
+            # Query head has arguments
+            arguments = ast[1]
+            return Symbol("ans")(*arguments)
+        else:
+            # Query head has no arguments
+            return Symbol("ans")()
+
+    def predicate(self, ast):
+        if not isinstance(ast, Expression):
+            ast = ast[0](*ast[2])
+        return ast
+
+    def negated_predicate(self, ast):
+        return Negation(ast[1])
+
+    def existential_predicate(self, ast):
+        exp = ast[2]
+        if len(exp) == 1:
+            exp = exp[0]
+        else:
+            exp = Conjunction(tuple(exp))
+
+        for arg in ast[0]:
+            exp = ExistentialPredicate(arg, exp)
+        return exp
+
+    def comparison(self, ast):
+        operator = Constant(OPERATOR[ast[1]])
+        return operator(ast[0], ast[2])
+
+    def arguments(self, ast):
+        if isinstance(ast, Expression):
+            return (ast,)
+        return tuple(ast)
+
+    def ext_identifier(self, ast):
+        ast = ast[1]
+        return ExternalSymbol[ast.type](ast.name)
+
+    def lambda_expression(self, ast):
+        return Lambda(ast[1], ast[3])
+
+    def arithmetic_operation(self, ast):
+        if isinstance(ast, Expression):
+            return ast
+
+        if len(ast) == 1:
+            return ast[0]
+
+        op = Constant(OPERATOR[ast[1]])
+
+        return op(*ast[0::2])
+
+    def term(self, ast):
+        if isinstance(ast, Expression):
+            return ast
+        elif len(ast) == 1:
+            return ast[0]
+
+        op = Constant(OPERATOR[ast[1]])
+
+        return op(ast[0], ast[2])
+
+    def factor(self, ast):
+        if isinstance(ast, Expression):
+            return ast
+        elif len(ast) == 1:
+            return ast[0]
+        else:
+            return Constant(pow)(ast[0], ast[2])
+
+    def function_application(self, ast):
+        if not isinstance(ast[0], Expression):
+            f = Symbol(ast[0])
+        else:
+            f = ast[0]
+        return FunctionApplication(f, args=ast[1])
+
+    def signed_int_ext_identifier(self, ast):
+        if isinstance(ast, Expression):
+            return ast
+        else:
+            return Constant(mul)(Constant(-1), ast[1])
+
+    def identifier(self, ast):
+        return Symbol(ast)
+
+    def argument(self, ast):
+        if ast == "...":
+            return Symbol.fresh()
+        else:
+            return ast
+
+    def text(self, ast):
+        return Constant(ast[1])
+
+    def integer(self, ast):
+        return Constant(int("".join(ast)))
+
+    def float(self, ast):
+        return Constant(float("".join(ast)))
+
+    def cmd_identifier(self, ast):
+        return Symbol(ast)
+
+    def pos_args(self, ast):
+        args = [ast[0]]
+        for arg in ast[1]:
+            args.append(arg[1])
+        return tuple(args)
+
+    def keyword_item(self, ast):
+        key = ast[0]
+        return (key, ast[2])
+
+    def pos_item(self, ast):
+        if not isinstance(ast, Expression):
+            return Constant(ast)
+        return ast
+
+    def keyword_args(self, ast):
+        kwargs = [ast[0]]
+        for kwarg in ast[1]:
+            kwargs.append(kwarg[1])
+        return tuple(kwargs)
+
+    def cmd_args(self, ast):
+        if isinstance(ast, list) and len(ast) == 2:
+            args, kwargs = ast
+        elif isinstance(ast[0], tuple):
+            args = ()
+            kwargs = ast
+        else:
+            args = ast
+            kwargs = ()
+        return args, kwargs
+
+    def command(self, ast):
+        if not isinstance(ast, list):
+            cmd = Command(ast, (), ())
+        else:
+            name = ast[0]
+            args, kwargs = ast[1]
+            cmd = Command(name, args, kwargs)
+        return cmd
+
+    def _default(self, ast):
+        return ast
+
+
+
 def parser_tatsu(code, locals=None, globals=None):
     return tatsu.parse(
         COMPILED_GRAMMAR,
@@ -448,6 +720,7 @@ def parser_tatsu(code, locals=None, globals=None):
     )
 
 def parser(code, locals=None, globals=None):
-    return COMPILED_GRAMMAR_lark.parse(
+    lark_tree = COMPILED_GRAMMAR_lark.parse(
         code.strip()
     )
+    return DatalogSemantics_lark().transform(lark_tree)
