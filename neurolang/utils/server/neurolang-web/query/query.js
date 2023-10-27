ace67abda9ce53d32d8a2ed5e5911356fe7e9023
lalshikh <liza.alshikhley@gmail.com>
2023-10-27 14:34:26 +0200

    Updated query.js : adapted the handling of the data returned from the backend.

--- neurolang/utils/server/neurolang-web/query/query.js
+++ neurolang/utils/server/neurolang-web/query/query.js
@@ -13,6 +13,10 @@ import { API_ROUTE } from '../constants'
  */
 export class QueryController {
   constructor () {
+    this.tokens = []
+    this.curSymbols = []
+    this.curFunctions = []
+    this.curCommands = []
     this.engine = 'destrieux'
     /// Initialize the query box with the CodeMirror plugin
     this.queryTextArea = document.querySelector('#queryTextArea')
@@ -54,6 +58,9 @@ export class QueryController {
       this.inputFileReader.trigger('click')
       e.preventDefault()
     })
+
+    this.okButton = $('#okButton')
+    this.okButton.on('click', () => this._handleOkButtonClick());
 
     /// Results Manager
     this.sc = new SymbolsController()
@@ -216,25 +223,49 @@ export class QueryController {
 //    console.log(`activeEngine : ${activeEngine}`)
 //    console.log(`this.sc.engine ${this.sc.engine}`);
     $.post(API_ROUTE.autocompletion, {text: content, engine: this.engine }, data => {
-//        console.log(`data returned : ${data}`);
-//        var t = Object.prototype.toString.call(data);
-//        console.log(`type of data returned : ${t}`)
-//        console.log(`data.tokens returned : ${data.tokens}`);
         // if (data.tokens && data.tokens.length > 0) {
         //     this._showTooltipAtCursor(data.tokens);
         // }
-        console.log(`type of data.tokens avant 1 : ${typeof data.tokens}`)
-        console.log(`type of data.tokens avant 2 : ${Object.prototype.toString.call(data.tokens)}`)
-        console.log(`data.tokens avant : ${data.tokens}`)
-        data.tokens = this._addSymbolsInAutocompletionTokens(data.tokens);
-        console.log(`data.tokens apres : ${data.tokens}`)
-        this._showTooltipAtCursor(data.tokens);
+        console.log(`avant - type of data.tokens : ${Object.prototype.toString.call(data.tokens)}`)
+        console.log(`avant - data.tokens         : ${data.tokens}`)
+        var tokens_to_json = JSON.parse(data.tokens)
+        console.log(`avant - type of tokens_to_json : ${JSON.stringify(tokens_to_json, null, 2)}`)
+        console.log(`avant - tokens_to_json         : ${tokens_to_json}`)
+        //var tmp_tokens = JSON.stringify(data.tokens, null, 2)
+        //console.log(`avant - type of tmp_tokens : ${Object.prototype.toString.call(tmp_tokens)}`)
+        //console.log(`avant - tmp_tokens         : ${tmp_tokens}`)
+        var retrieved_symbols = this._addSymbolsInAutocompletionTokens();
+//        console.log(`retrieved_symbols - type : ${Object.prototype.toString.call(retrieved_symbols)}`)
+//        console.log(`retrieved_symbols - value : ${JSON.stringify(retrieved_symbols, null, 2)}`)
+        //const facets = Object.assign({}, {"tokens" : data.tokens}, retrieved_symbols)
+        var facets = Object.assign({}, tokens_to_json, retrieved_symbols)
+//        console.log(`facets - type : ${Object.prototype.toString.call(facets)}`)
+//        console.log(`facets - value : ${JSON.stringify(facets, null, 2)}`)
+        facets = this._cleanAcceptedTokens(facets)
+
+        this._displayFacets(facets);    // Display the facets based on the tokens
+        //this._showTooltipAtCursor(data.tokens);  // No tokens passed, so it won't show the tooltip but will set up the cursor activity listener to hide facets
     });
   }
 
-  _addSymbolsInAutocompletionTokens(tokens) {
+  _cleanAcceptedTokens(facets_obj) {
+    var keys_to_remove = []
+    var clean_json_object = {}
+
+    for (let key of Object.keys(facets_obj)) {
+        if(facets_obj[key].length != 0) {
+            //keys_to_remove.push(key)
+            clean_json_object[key] = facets_obj[key]
+        }
+    }
+    return clean_json_object
+  }
+
+  //_addSymbolsInAutocompletionTokens(tokens) {
+  _addSymbolsInAutocompletionTokens() {
     console.log(`__in _addSymbolsInAutocompletionTokens()___`)
-    console.log("Displaying tokens:", tokens);
+    //console.log("Displaying tokens:", tokens);
+
     // ajouter les elememts ici
     var curSymbols = Object.keys(window.qc.sc.symbols.results)
     .filter(elt => !window.qc.sc.symbols.results[elt].function && !window.qc.sc.symbols.results[elt].command)
@@ -243,26 +274,168 @@ export class QueryController {
     var curCommands = Object.keys(window.qc.sc.symbols.results)
     .filter(elt => window.qc.sc.symbols.results[elt].command)
 
-    return [].concat(tokens, curSymbols, curFunctions, curCommands)
+    this.curSymbols = curSymbols
+    this.curFunctions = curFunctions
+    this.curCommands = curCommands
+
+    console.log(`Symbols : ${curSymbols}`)
+    console.log(`Symbols type : ${Object.prototype.toString.call(curSymbols)}`)
+    console.log(`Functions : ${curFunctions}`)
+    console.log(`Functions type : ${Object.prototype.toString.call(curFunctions)}`)
+    console.log(`Commands : ${curCommands}`)
+    console.log(`Commands type : ${Object.prototype.toString.call(curCommands)}`)
+
+    //return [].concat(tokens, curSymbols, curFunctions, curCommands)
+
+    return {
+        "symbols" : curSymbols,
+        "functions" : curFunctions,
+        "commands" : curCommands
+    }
   }
+
+  _displayFacets(facets_obj) {
+    const leftFacet = document.getElementById("leftFacet");
+    const rightFacet = document.getElementById("rightFacet");
+    const facetsContainer = document.getElementById("facetsContainer");
+    const okButton = document.getElementById("okButton");
+
+    // Clear previous facet items
+    leftFacet.innerHTML = "";
+    rightFacet.innerHTML = "";
+
+    // Add deselect to the left facet
+    const deselectItem = document.createElement("div");
+    deselectItem.textContent = "deselect";
+    deselectItem.classList.add("facet-item");
+    leftFacet.appendChild(deselectItem);
+
+    // Add facets_obj keys to left facet
+    for (let key of Object.keys(facets_obj)) {
+        const item = document.createElement("div");
+        item.textContent = key;
+        item.classList.add("facet-item");
+        leftFacet.appendChild(item);
+    }
+
+    // Handle left facet item click
+    leftFacet.addEventListener("click", (e) => {
+        const itemContent = e.target.textContent;
+        rightFacet.innerHTML = ""; // Clear previous items
+
+        if (itemContent === "deselect") {
+            for (let key of Object.keys(facets_obj)) {
+                for (let value of facets_obj[key]) {
+                    const item = document.createElement("div");
+                    item.textContent = value;
+                    item.classList.add("facet-item");
+                    rightFacet.appendChild(item);
+                }
+            }
+        } else {
+            for (let value of facets_obj[itemContent]) {
+                const item = document.createElement("div");
+                item.textContent = value;
+                item.classList.add("facet-item");
+                rightFacet.appendChild(item);
+            }
+        }
+    });
+
+    // Handle right facet item click
+    rightFacet.addEventListener('click', this._handleRightFacetClick.bind(this));
+
+    // Handle 'ok' button click
+    //okButton.removeEventListener("click", this._handleOkButtonClick.bind(this))
+    //okButton.addEventListener("click", this._handleOkButtonClick.bind(this));
+
+    // Display the facets container
+    //facetsContainer.removeAttribute("hidden");
+    facetsContainer.style.display = 'flex';         // overrides the 'display: none;' from the CSS.
+
+    // This will always run, whether there are tokens or not
+    this.editor.on("cursorActivity", () => {
+        //const facetsContainer = document.getElementById("facetsContainer");
+        //facetsContainer.setAttribute("hidden", true);
+        facetsContainer.style.display = 'none';     // set the facets container back to be hidden
+    });
+  }
+
+  _handleRightFacetClick(event) {
+    // Check if the clicked element is an item within the right facet
+    if (event.target.classList.contains('facet-item')) {
+        // Deselect previously selected item
+        const previouslySelected = event.currentTarget.querySelector('.selected');
+        if (previouslySelected) {
+            previouslySelected.classList.remove('selected');
+        }
+
+        // Mark the clicked item as selected
+        event.target.classList.add('selected');
+    }
+  }
+
+  _handleOkButtonClick() {
+    console.log(`___in _handleOkButtonClick() -> clicked___`)
+    const rightFacet = document.getElementById("rightFacet");
+    const selectedItem = rightFacet.querySelector(".selected");
+
+    console.log(`right facet : ${rightFacet}`)
+    console.log(`selected item : ${selectedItem}`)
+    console.log(`selected item.textContent : ${selectedItem.textContent}`)
+
+    if (selectedItem && selectedItem.textContent !== "deselect") {
+        console.log(`  selectedItem && selectedItem.textContent !== "deselect"`)
+        //const cursorPos = this.editor.getCursor();
+        //this.editor.replaceRange(selectedItem.textContent, cursorPos);
+
+        const selectedValue = selectedItem.textContent;
+    	const doc = this.editor.getDoc();
+
+    	console.log(`  selectedValue : ${selectedValue}`)
+
+
+    	// Insert the value at the current cursor position.
+    	const cursor = doc.getCursor();
+    	doc.replaceRange(selectedValue, cursor);
+    	//doc.replaceRange("Test insertion", cursor)
+
+    	// Move cursor to end of inserted value
+    	const endPos = { line: cursor.line, ch: cursor.ch + selectedValue.length };
+    	doc.setCursor(endPos);
+
+    }
+
+    //// Hide the facets and 'ok' button
+    //const facetsContainer = document.getElementById("facetsContainer");
+    //facetsContainer.style.display = 'none';     // set the facets container back to be hidden
+  }
+
 
   _showTooltipAtCursor(tokens) {
     console.log(`___in _showTooltipAtCursor()___`)
     console.log("Displaying tokens:", tokens);
-    const cursorPos = this.editor.cursorCoords(true, "page");
-    const tooltip = $('<div class="autocomplete-tooltip"></div>').text(tokens.join(", ")).css({
-        top: cursorPos.top + "px",
-        left: cursorPos.left + "px"
-    }).appendTo(document.body);
 
-    // Close the tooltip on cursor movement or click outside
+    if (tokens.length > 0) {
+        const cursorPos = this.editor.cursorCoords(true, "page");
+        const tooltip = $('<div class="autocomplete-tooltip"></div>').text(tokens.join(", ")).css({
+            top: cursorPos.top + "px",
+            left: cursorPos.left + "px"
+        }).appendTo(document.body);
+
+        // Close the tooltip on cursor movement or click outside
+        this.editor.on("cursorActivity", () => {
+            tooltip.remove();
+        });
+        $(document).on('click', () => {
+            tooltip.remove();
+        });
+    }
+
+    // This will always run, whether there are tokens or not
     this.editor.on("cursorActivity", () => {
-        tooltip.remove();
-    });
-    $(document).on('click', () => {
-        tooltip.remove();
+        const facetsContainer = document.getElementById("facetsContainer");
+        facetsContainer.setAttribute("hidden", true);
     });
   }
-
-
 }
