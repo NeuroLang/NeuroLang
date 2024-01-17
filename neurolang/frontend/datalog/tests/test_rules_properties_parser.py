b2dff1529ca8067ef8fe590ac41e7d1e9607c076
lalshikh <liza.alshikhley@gmail.com>
2024-01-17 13:09:37 +0100

    [FIX] Autocompletion : adding rules properties and pytest test
    Added file : neurolang/frontend/datalog/rules.json
    Modified file : neurolang/frontend/datalog/tests/test_rules_properties_parser.py

--- neurolang/frontend/datalog/tests/test_rules_properties_parser.py
+++ neurolang/frontend/datalog/tests/test_rules_properties_parser.py
@@ -6,22 +6,9 @@ from ..standard_syntax import parse_rules
 def test_rules_properties_parsing():
     res = parse_rules()
     expected = {
-        'expression': {
-            'values': [
-                'rule'
-            ]
-        },
-        'rule': {
-            'values': [
-                '<identifier> ( <arguments> ) :- <condition>',
-                '<identifier> ( <arguments> ) :- <body>',
-                '<query> :- <condition>',
-                '<query> :- <body>'
-            ]
-        },
-        'head': {
-            'values': [
-                '<identifier> ( <arguments> )'
+        "expression": {
+            "values": [
+                "<rule>"
             ]
         }
     }
