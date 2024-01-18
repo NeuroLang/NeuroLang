1a2cdc24920448955496a723ba7a954f64973a2c
lalshikh <liza.alshikhley@gmail.com>
2024-01-18 16:13:12 +0100

    [FEATURE] Autocompletion : rules construction with pattern approach
    Modified files :
    - ../../frontend/datalog/rules.json
    - neurolang-web/autocompletion/autocompletion.js
    - neurolang-web/facets/facets.js

--- neurolang/utils/server/neurolang-web/autocompletion/autocompletion.js
+++ neurolang/utils/server/neurolang-web/autocompletion/autocompletion.js
@@ -26,6 +26,9 @@ export class AutocompletionController {
     this.engine = engine
   }
 
+   /**
+  * Test if, in line line, pos is in a pattern
+  */
   _isCursorInPattern (line, pos) {
     const pattern = /<[a-zA-Z_]+>/g
     const matches = line.match(pattern)
@@ -34,6 +37,7 @@ export class AutocompletionController {
       for (let match of matches) {
         const startIndex = line.indexOf(match)
         const endIndex = startIndex + match.length - 1
+        let l = endIndex - startIndex + 1
 
         if (pos >= startIndex && pos <= endIndex) {
           return {
@@ -42,6 +46,8 @@ export class AutocompletionController {
             end:endIndex
           }
         }
+
+        line = line.substring(0, startIndex) + ' '.repeat(l) + line.substring(startIndex + l)
       }
     }
     return false
@@ -73,24 +79,19 @@ export class AutocompletionController {
     // Split the text into lines
     let lines = allText.split('\n')
 
-    console.log("cursorLineNumber :", cursorLineNumber)
-    console.log("this.editor.getLine(cursorLineNumber) :", this.editor.getLine(cursorLineNumber))
-    console.log("cursorLineContent :", cursorLineContent)
-    console.log("lines[cursorLineNumber] :", lines[cursorLineNumber])
-
     if (cursorLineNumber >= 0 && cursorLineNumber < lines.length) {
       // in the cursor line, get the substring from the line start to the cursor
       const subline = lines[cursorLineNumber].substring(0, cursorLinePosition)
       // in the cursor line, replace the original line by the substring
 
       // The substring contains a pattern or the cursor position is in a pattern
-//      if (  /<[A-Za-z_]+>/.test(subline) || this._isCursorInPattern (lines[cursorLineNumber], cursorLinePosition)) {
-//        lines[cursorLineNumber] = ''
-//      }
+      if (  /<[A-Za-z_]+>/.test(subline) || this._isCursorInPattern (lines[cursorLineNumber], cursorLinePosition)) {
+        lines[cursorLineNumber] = ''
+      }
       // The substring doesn't contain a pattern -> for the cursor line, keep only the substring for autocompletion
-//      else if (lines[cursorLineNumber].trim()) {
-//        lines[cursorLineNumber] = subline
-//      }
+      else if (lines[cursorLineNumber].trim()) {
+        lines[cursorLineNumber] = subline
+      }
     }
 
     // separate the text to get the symbols and the text for autocompletion
@@ -113,16 +114,51 @@ export class AutocompletionController {
 
       // Not empty line
       } else {
-        delete facets.rules
-        const k = Object.keys(facets)[0]
-        // Only one accepted next token
-        if ((Object.keys(facets).length == 1) && (facets[k].length == 1)) {
-          this._writeValueInTextEditor(facets[k][0])
-        // Several accepted tokens
+        const pattern = this._isCursorInPattern(cursorLineContent, cursorLinePosition)
+
+        if (pattern) {
+          this.editor.setSelection({line: cursorLineNumber, ch: pattern.start}, {line: cursorLineNumber, ch: pattern.end+1})
+          const patternContent = pattern.content.slice(1, -1)
+
+          // Pattern is in rule properties
+          if (rules.hasOwnProperty(patternContent)) {
+
+            // Pattern has a value
+            if (rules[patternContent].hasOwnProperty("values")) {
+
+              // Only one possible value/pattern
+              if (rules[patternContent]["values"].length == 1) {
+                this._writeValueInTextEditor(rules[patternContent].values[0])
+
+              // Several possible patterns lines
+              } else {
+                this.fc.createPatternsContainer()
+                this.fc.addPatternsFacetEventListeners(true)
+                this.fc.displayPatternsFacet(rules[patternContent].values)
+              }
+
+            // Pattern does not have value
+            } else if (rules[patternContent].hasOwnProperty("params") && rules[patternContent].hasOwnProperty("unit")) {
+              if (rules[patternContent].params == "number") {
+                this.fc.createInputContainer('Number of ' + rules[patternContent].unit.slice(1, -1) + 's :')
+                this.fc.addInputFacetEventListeners(rules[patternContent].unit, rules[patternContent].sep, true)
+                this.fc.displayInputFacet()
+              }
+            }
+          }
+
         } else {
-          this.fc.createFacets (facets)
-          // Display the facets based on the tokens
-          this.fc.displayFacets(facets)
+          delete facets.rules
+          const k = Object.keys(facets)[0]
+          // Only one accepted next token
+          if ((Object.keys(facets).length == 1) && (facets[k].length == 1)) {
+            this._writeValueInTextEditor(facets[k][0])
+          // Several accepted tokens
+          } else {
+            this.fc.createFacets (facets)
+            // Display the facets based on the tokens
+            this.fc.displayFacets(facets)
+          }
         }
       }
     })
