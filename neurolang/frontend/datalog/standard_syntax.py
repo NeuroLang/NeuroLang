e5eec8b9c837625f8051a919a7a2c103c4281370
lalshikh <131446559+lalshikh@users.noreply.github.com>
2024-03-27 18:05:40 +0100

    Autocompletion rules properties parsing (#804)
    * [FIXED] Docker used in circleCI
    
    --- Summary --------------------------------------------------------------------
    
    A] Adaptation of the circleCI configuration file in order to use the new version
       of Docker.
       -> Modified files :
          - .circleci/config.yml
    
    --- Details --------------------------------------------------------------------
    
    1/ File .circleci/config.yml
    
    Modifications :
    - replaced circleci/python by cimg/python for all the jobs
    
    Additions :
    - new run section to create venv with virtualenv
    
    Deletions :
    - removed the creation of venv with python
    
    * [FEATURE] Autocompletion - backend : rules properties parsing
    
    --- Summary --------------------------------------------------------------------
    
    A] Parsing of the rules properties for the autocompletion functionality
       -> Modified files :
    
          - neurolang/frontend/datalog/standard_syntax.py
    
    --- Details --------------------------------------------------------------------
    
    1/ File neurolang/frontend/datalog/standard_syntax.py
    
    Additions :
    
    - parse_rules() method to parse the rules.json file.
    
    Modifications :
    
    - Reorganised the imports
    - Use of relative imports
    
    * [fix] File standard_syntax.py: removed the lines related to the logging package.

--- neurolang/frontend/datalog/standard_syntax.py
+++ neurolang/frontend/datalog/standard_syntax.py
@@ -1,12 +1,13 @@
-from operator import add, eq, ge, gt, le, lt, mul, ne, pow, sub, truediv
+import json
+import os
 
 from lark import Lark, Transformer
 from lark.exceptions import UnexpectedToken, UnexpectedCharacters, LarkError
-
-from neurolang.logic import ExistentialPredicate
+from operator import add, eq, ge, gt, le, lt, mul, ne, pow, sub, truediv
 
 from ...datalog import Conjunction, Fact, Implication, Negation, Union
 from ...datalog.constraints_representation import RightImplication
+from ...exceptions import UnexpectedTokenError, UnexpectedCharactersError, NeuroLangException
 from ...expressions import (
     Command,
     Constant,
@@ -17,13 +18,14 @@ from ...expressions import (
     Statement,
     Symbol
 )
+from ...logic import ExistentialPredicate
 from ...probabilistic.expressions import (
     PROB,
     Condition,
     ProbabilisticFact
 )
-from ...exceptions import UnexpectedTokenError, UnexpectedCharactersError, NeuroLangException
 from ...utils.interactive_parsing import LarkCompleter
+
 
 GRAMMAR = u"""
 start: expressions
@@ -557,3 +559,17 @@ def parser(code, locals=None, globals=None, interactive=False):
         raise UnexpectedCharactersError(str(e), line=e.line - 1, column=e.column - 1) from e
     except LarkError as e:
         raise NeuroLangException from e
+
+
+def parse_rules():
+    curdir = os.path.dirname(os.path.realpath(__file__))
+    rules_file = os.path.join(
+        curdir,
+        "rules.json"
+    )
+
+    # Opening JSON file
+    with open(rules_file, 'r') as f:
+        # json.load() returns JSON object as a dictionary
+        rules_dico = json.load(f)
+    return rules_dico
